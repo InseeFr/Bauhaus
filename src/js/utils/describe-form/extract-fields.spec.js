82dc4650af2d7b7227db8a58b8c3e14606f9236a
Jean-Baptiste RUDANT <jb@noknot.fr>
2017-07-10 07:26:44 +0200

    Add utility functions to build forms

---
+++ src/js/utils/describe-form/extract-fields.spec.js
@@ -0,0 +1,13 @@
+import buildExtractFields from './extract-fields';
+
+describe('Build extract fields', () => {
+  it('returns a function which takes some data and returns a copy with the requested fields', () => {
+    const extractFields = buildExtractFields(['firstName', 'lastName']);
+    expect(
+      extractFields({ firstName: 'bobby', lastName: 'fischer', hobby: 'chess' })
+    ).toEqual({
+      firstName: 'bobby',
+      lastName: 'fischer',
+    });
+  });
+});
---
+++ src/js/utils/describe-form/index.js
@@ -0,0 +1,9 @@
+import describeHandlers from './describe-handlers';
+import buildExtractFields from './extract-fields';
+
+export default function describeForm(fields) {
+  return {
+    handlersFromSetState: describeHandlers(fields),
+    extractFields: buildExtractFields(fields),
+  };
+}
