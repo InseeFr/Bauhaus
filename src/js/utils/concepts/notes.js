7705669527eab614d302605028c2f2b9baf55dc1
Nicolas Laval <nicolas.laval@insee.fr>
2017-07-19 15:51:37 +0200

    Mark non required fields accordingly

--- src/js/utils/concepts/notes.js
+++ src/js/utils/concepts/notes.js
@@ -2,49 +2,50 @@ import PropTypes from 'prop-types';
 import objectFromKeys from 'js/utils/object-from-keys';
 
 const fieldsToTrack = [
-  'definitionCourteFr',
-  'definitionCourteEn',
-  'definitionFr',
-  'definitionEn',
-  'noteEditorialeFr',
-  'noteEditorialeEn',
-  'changeNoteFr',
-  'changeNoteEn',
+	'definitionCourteFr',
+	'definitionCourteEn',
+	'definitionFr',
+	'definitionEn',
+	'noteEditorialeFr',
+	'noteEditorialeEn',
+	'changeNoteFr',
+	'changeNoteEn',
 ];
 
 const fieldsVersion = [
-  'definitionCourteFrVersion',
-  'definitionCourteEnVersion',
-  'definitionFrVersion',
-  'definitionEnVersion',
-  'noteEditorialeFrVersion',
-  'noteEditorialeEnVersion',
-  'changeNoteFrVersion',
-  'changeNoteEnVersion',
+	'definitionCourteFrVersion',
+	'definitionCourteEnVersion',
+	'definitionFrVersion',
+	'definitionEnVersion',
+	'noteEditorialeFrVersion',
+	'noteEditorialeEnVersion',
+	'changeNoteFrVersion',
+	'changeNoteEnVersion',
 ];
 
 export const fields = [...fieldsToTrack, ...fieldsVersion];
 
 export const processChanges = (oldNotes, newNotes) =>
-  fieldsToTrack.reduce((changes, field) => {
-    changes[`${field}Changed`] = oldNotes[field] !== newNotes[field];
-    return changes;
-  }, {});
+	fieldsToTrack.reduce((changes, field) => {
+		changes[`${field}Changed`] = oldNotes[field] !== newNotes[field];
+		return changes;
+	}, {});
 
 export const areNotesChanged = (oldNotes, newNotes) => {
-  const {
-    isDefinitionCourteFrChanged,
-    isDefinitionFrChanged,
-    isNoteEditorialeFrChanged,
-  } = processChanges(oldNotes, newNotes);
+	const {
+		isDefinitionCourteFrChanged,
+		isDefinitionFrChanged,
+		isNoteEditorialeFrChanged,
+	} = processChanges(oldNotes, newNotes);
 
-  //TODO check what `objectSize` what used for
-  return (
-    isDefinitionCourteFrChanged ||
-    isDefinitionFrChanged ||
-    isNoteEditorialeFrChanged
-  );
+	//TODO check what `objectSize` what used for
+	return (
+		isDefinitionCourteFrChanged ||
+		isDefinitionFrChanged ||
+		isNoteEditorialeFrChanged
+	);
 };
+
 export const propTypes = PropTypes.shape(
-  objectFromKeys(fields, PropTypes.string.isRequired)
+	objectFromKeys(fields, PropTypes.string)
 );
