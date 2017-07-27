99ce6c579115a0ea3ff4b567630b443badef5fd1
Jean-Baptiste RUDANT <jb@noknot.fr>
2017-07-27 16:16:20 +0200

    Fix layout issue when no error message to show
    The position of html elements below was impacted.

--- src/js/components/concept-edition/concept-create-control-layout.js
+++ src/js/components/concept-edition/concept-create-control-layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { dictionary } from 'js/utils/dictionary';
 
-function ConceptCreatecontrolLayout({
+function ConceptCreateControlLayout({
   message,
   redirectCancel,
   handleSave,
@@ -22,12 +22,16 @@ function ConceptCreatecontrolLayout({
           </Link>
         </button>
       </div>
-      {message &&
-        <div className="col-md-8 centered">
-          <div className="alert alert-danger bold" role="alert">
-            {message}
-          </div>
-        </div>}
+      <div className="col-md-8 centered">
+        <div
+          style={{ visibility: message ? 'visible' : 'hidden' }}
+          className="alert alert-danger bold"
+          role="alert">
+          {/* HACK: if no content, the line height is set to 0 and the rest
+              of the page moves a little  */}
+          {message || <span style={{ whiteSpace: 'pre-wrap' }}> </span>}
+        </div>
+      </div>
       <div className="col-md-2 pull-right">
         <button
           type="button"
@@ -45,11 +49,11 @@ function ConceptCreatecontrolLayout({
   );
 }
 
-ConceptCreatecontrolLayout.propTypes = {
+ConceptCreateControlLayout.propTypes = {
   message: PropTypes.string,
   saveEnabled: PropTypes.bool.isRequired,
   redirectCancel: PropTypes.string.isRequired,
   handleSave: PropTypes.func.isRequired,
 };
 
-export default ConceptCreatecontrolLayout;
+export default ConceptCreateControlLayout;
