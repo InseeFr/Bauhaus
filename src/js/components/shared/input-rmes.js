52e1a10d8a37c8475c5d78b5440470485bc02612
Nicolas Laval <nicolas.laval@insee.fr>
2017-07-28 17:55:53 +0200

    Fix Input alignment issue adding 'hiddenStar' attr to InputRmes

--- src/js/components/shared/input-rmes.js
+++ src/js/components/shared/input-rmes.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function InputRmes({ colMd, value, label, flag, star, handleChange }) {
-  return (
-    <div className={`form-group col-md-${colMd}`}>
-      <label>
-        {label} ( {flag} )
-        {/* TODO handle visibility */}
-        {star && <span className="boldRed">*</span>}
-      </label>
-      <input
-        type="text"
-        value={value || ''}
-        className="form-control"
-        onChange={e => handleChange(e.target.value)}
-      />
-    </div>
-  );
+function InputRmes({
+	colMd,
+	value,
+	label,
+	flag,
+	star,
+	hiddenStar,
+	handleChange,
+}) {
+	return (
+		<div className={`form-group col-md-${colMd}`}>
+			<label>
+				{label} ( {flag} )
+				{/* TODO handle visibility */}
+				{star && <span className="boldRed">*</span>}
+				{hiddenStar && <span className="boldWhite">*</span>}
+			</label>
+			<input
+				type="text"
+				value={value || ''}
+				className="form-control"
+				onChange={e => handleChange(e.target.value)}
+			/>
+		</div>
+	);
 }
 
 InputRmes.propTypes = {
-  colMd: PropTypes.number.isRequired,
-  label: PropTypes.string.isRequired,
-  flag: PropTypes.element,
-  star: PropTypes.bool,
-  value: PropTypes.string, //might be undefined
-  handleChange: PropTypes.func.isRequired,
+	colMd: PropTypes.number.isRequired,
+	label: PropTypes.string.isRequired,
+	flag: PropTypes.element,
+	star: PropTypes.bool,
+	value: PropTypes.string, //might be undefined
+	handleChange: PropTypes.func.isRequired,
 };
 
 export default InputRmes;
