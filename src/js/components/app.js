e7df1a48a866161aae874da714c93505ba1fac0e
Nicolas Laval <nicolas.laval@insee.fr>
2017-07-31 09:50:00 +0200

    Pretify App Home

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -7,43 +7,43 @@ import { dictionary } from 'js/utils/dictionary';
 import 'css/app.css';
 
 function App() {
-  return (
-    <div>
-      <div className="centered">
-        <h1>
-          {dictionary.app.title}
-        </h1>
-      </div>
-      <div className="img-block">
-        <h2 className="items">
-          <Link to="/concepts">
-            {dictionary.app.conceptsTitle}
-          </Link>
-        </h2>
-        <h2 className="items">
-          <Link to="/">
-            {dictionary.app.classificationsTitle}
-          </Link>
-        </h2>
-        <h2 className="items">
-          <Link to="/">
-            {dictionary.app.sourcesTitle}
-          </Link>
-        </h2>
-      </div>
-      <div className="img-block">
-        <Link to="/concepts">
-          <img src={conceptsLogo} alt="Concepts" className="img" />
-        </Link>
-        <Link to="/">
-          <img src={nomenclaturesLogo} alt="Nomenclatures" className="img" />
-        </Link>
-        <Link to="/">
-          <img src={sourcesLogo} alt="Sources" className="img" />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="centered page-title">
+				<h1>
+					{dictionary.app.title}
+				</h1>
+			</div>
+			<div className="img-block">
+				<Link to="/concepts">
+					<h2 className="items page-title page-title-link">
+						{dictionary.app.conceptsTitle}
+					</h2>
+				</Link>
+				<Link to="/">
+					<h2 className="items page-title page-title-link">
+						{dictionary.app.classificationsTitle}
+					</h2>
+				</Link>
+				<Link to="/">
+					<h2 className="items page-title page-title-link">
+						{dictionary.app.sourcesTitle}
+					</h2>
+				</Link>
+			</div>
+			<div className="img-block">
+				<Link to="/concepts">
+					<img src={conceptsLogo} alt="Concepts" className="img" />
+				</Link>
+				<Link to="/">
+					<img src={nomenclaturesLogo} alt="Nomenclatures" className="img" />
+				</Link>
+				<Link to="/">
+					<img src={sourcesLogo} alt="Sources" className="img" />
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default App;
