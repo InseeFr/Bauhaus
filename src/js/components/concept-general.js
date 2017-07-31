a8bfa0f9be71c1f82eba69738f8118e5326b03dc
Nicolas Laval <nicolas.laval@insee.fr>
2017-07-31 10:37:18 +0200

    Fix issue to display only filled fields (concept general et links)

--- src/js/components/concept-general.js
+++ src/js/components/concept-general.js
@@ -4,72 +4,95 @@ import Panel from 'js/utils/panel';
 import { dateTimeToDateString } from 'js/utils/utils';
 
 function ConceptGeneral({ attr, english }) {
-  var mapping = {
-    altLabelLg1: dictionary.concept.altLabel + ' (fr)',
-  };
-  if (attr.altLabelLg2) {
-    mapping = {
-      ...mapping,
-      altLabelLg2: dictionary.concept.altLabel + ' (en)',
-    };
-  }
-  mapping = {
-    ...mapping,
-    created: dictionary.concept.created,
-    modified: dictionary.concept.modified,
-    valid: dictionary.concept.valid,
-    conceptVersion: dictionary.concept.conceptVersion,
-    creator: dictionary.concept.creator,
-    contributor: dictionary.concept.contributor,
-    disseminationStatus: dictionary.concept.disseminationStatus.title,
-    additionalMaterial: dictionary.concept.additionalMaterial,
-    isValidated: dictionary.concept.isValidated,
-  };
+	console.log(attr);
+	var mapping = {};
+	if (attr.altLabelLg1) {
+		mapping = {
+			...mapping,
+			altLabelLg1: dictionary.concept.altLabel + ' (fr)',
+		};
+	}
+	if (attr.altLabelLg2) {
+		mapping = {
+			...mapping,
+			altLabelLg2: dictionary.concept.altLabel + ' (en)',
+		};
+	}
+	mapping = {
+		...mapping,
+		created: dictionary.concept.created,
+		modified: dictionary.concept.modified,
+	};
+	if (attr.valid) {
+		mapping = {
+			...mapping,
+			valid: dictionary.concept.valid,
+		};
+	}
+	mapping = {
+		...mapping,
+		conceptVersion: dictionary.concept.conceptVersion,
+		creator: dictionary.concept.creator,
+		contributor: dictionary.concept.contributor,
+		disseminationStatus: dictionary.concept.disseminationStatus.title,
 
-  return (
-    <div className="row">
-      <div className="col-md-12">
-        <Panel title={dictionary.concept.general}>
-          <ul>
-            {Object.keys(mapping).map(fieldName => {
-              if (attr.hasOwnProperty(fieldName)) {
-                if (fieldName === 'altLabelLg2' && !english) {
-                  return null;
-                }
-                if (
-                  fieldName === 'created' ||
-                  fieldName === 'modified' ||
-                  fieldName === 'valid'
-                ) {
-                  return (
-                    <li key={fieldName}>{`${mapping[
-                      fieldName
-                    ]} : ${dateTimeToDateString(attr[fieldName])}`}</li>
-                  );
-                }
-                if (fieldName === 'additionalMaterial') {
-                  return (
-                    <li key={fieldName}>
-                      {`${mapping[fieldName]} : `}
-                      <a href={attr[fieldName]} target="_blank">{`${attr[
-                        fieldName
-                      ]}`}</a>
-                    </li>
-                  );
-                } else {
-                  return (
-                    <li key={fieldName}>{`${mapping[fieldName]} : ${attr[
-                      fieldName
-                    ]}`}</li>
-                  );
-                }
-              } else return null;
-            })}
-          </ul>
-        </Panel>
-      </div>
-    </div>
-  );
+		isValidated: dictionary.concept.isValidated,
+	};
+	if (attr.additionalMaterial) {
+		mapping = {
+			...mapping,
+			additionalMaterial: dictionary.concept.additionalMaterial,
+		};
+	}
+	mapping = {
+		...mapping,
+		isValidated: dictionary.concept.isValidated,
+	};
+
+	return (
+		<div className="row">
+			<div className="col-md-12">
+				<Panel title={dictionary.concept.general}>
+					<ul>
+						{Object.keys(mapping).map(fieldName => {
+							if (attr.hasOwnProperty(fieldName)) {
+								if (fieldName === 'altLabelLg2' && !english) {
+									return null;
+								}
+								if (
+									fieldName === 'created' ||
+									fieldName === 'modified' ||
+									fieldName === 'valid'
+								) {
+									return (
+										<li key={fieldName}>{`${mapping[
+											fieldName
+										]} : ${dateTimeToDateString(attr[fieldName])}`}</li>
+									);
+								}
+								if (fieldName === 'additionalMaterial') {
+									return (
+										<li key={fieldName}>
+											{`${mapping[fieldName]} : `}
+											<a href={attr[fieldName]} target="_blank">{`${attr[
+												fieldName
+											]}`}</a>
+										</li>
+									);
+								} else {
+									return (
+										<li key={fieldName}>{`${mapping[fieldName]} : ${attr[
+											fieldName
+										]}`}</li>
+									);
+								}
+							} else return null;
+						})}
+					</ul>
+				</Panel>
+			</div>
+		</div>
+	);
 }
 
 export default ConceptGeneral;
--- src/js/components/concept-links.js
+++ src/js/components/concept-links.js
@@ -8,175 +8,184 @@ const sortByLabelFr = sortArray('prefLabelLg1');
 const sortByLabelEn = sortArray('prefLabelLg2');
 
 function ConceptLinks({ english, links }) {
-  const cl = english ? 'col-md-6' : 'col-md-12';
+	var nbLinks = 0;
+	const cl = english ? 'col-md-6' : 'col-md-12';
 
-  const conceptParent = [];
-  const conceptEnfants = [];
-  const conceptRef = [];
-  const conceptSucceed = [];
-  const conceptLink = [];
+	const conceptParent = [];
+	const conceptEnfants = [];
+	const conceptRef = [];
+	const conceptSucceed = [];
+	const conceptLink = [];
 
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].typeOfLink === BROADER) {
-      conceptParent.push(links[i]);
-    }
-    if (links[i].typeOfLink === NARROWER) {
-      conceptEnfants.push(links[i]);
-    }
-    if (links[i].typeOfLink === REFERENCES) {
-      conceptRef.push(links[i]);
-    }
-    if (links[i].typeOfLink === SUCCEED) {
-      conceptSucceed.push(links[i]);
-    }
-    if (links[i].typeOfLink === RELATED) {
-      conceptLink.push(links[i]);
-    }
-  }
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].typeOfLink === BROADER) {
+			conceptParent.push(links[i]);
+			nbLinks++;
+		}
+		if (links[i].typeOfLink === NARROWER) {
+			conceptEnfants.push(links[i]);
+			nbLinks++;
+		}
+		if (links[i].typeOfLink === REFERENCES) {
+			conceptRef.push(links[i]);
+			nbLinks++;
+		}
+		if (links[i].typeOfLink === SUCCEED) {
+			conceptSucceed.push(links[i]);
+			nbLinks++;
+		}
+		if (links[i].typeOfLink === RELATED) {
+			conceptLink.push(links[i]);
+			nbLinks++;
+		}
+	}
 
-  const conceptParentListFr = sortByLabelFr(conceptParent).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg1}
-      </Link>
-    </li>
-  );
-  const conceptParentListEn = sortByLabelEn(conceptParent).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg2}
-      </Link>
-    </li>
-  );
-  const conceptEnfantsListFr = sortByLabelFr(conceptEnfants).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg1}
-      </Link>
-    </li>
-  );
-  const conceptEnfantsListEn = sortByLabelEn(conceptEnfants).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg2}
-      </Link>
-    </li>
-  );
-  const conceptRefListFr = sortByLabelFr(conceptRef).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg1}
-      </Link>
-    </li>
-  );
-  const conceptRefListEn = sortByLabelEn(conceptRef).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg2}
-      </Link>
-    </li>
-  );
-  const conceptSucceedListFr = sortByLabelFr(conceptSucceed).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg1}
-      </Link>
-    </li>
-  );
-  const conceptSucceedListEn = sortByLabelEn(conceptSucceed).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg2}
-      </Link>
-    </li>
-  );
-  const conceptLinkListFr = sortByLabelFr(conceptLink).map(item =>
-    <li key={item.id}>
-      <Link to={'/concept/' + item.id}>
-        {item.prefLabelLg1}
-      </Link>
-    </li>
-  );
-  const conceptLinkListEn = sortByLabelEn(conceptLink).map(item =>
-    <li key={item.idLinked}>
-      <Link to={'/concept/' + item.idLinked}>
-        {item.prefLabelLg2}
-      </Link>
-    </li>
-  );
+	const conceptParentListFr = sortByLabelFr(conceptParent).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg1}
+			</Link>
+		</li>
+	);
+	const conceptParentListEn = sortByLabelEn(conceptParent).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg2}
+			</Link>
+		</li>
+	);
+	const conceptEnfantsListFr = sortByLabelFr(conceptEnfants).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg1}
+			</Link>
+		</li>
+	);
+	const conceptEnfantsListEn = sortByLabelEn(conceptEnfants).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg2}
+			</Link>
+		</li>
+	);
+	const conceptRefListFr = sortByLabelFr(conceptRef).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg1}
+			</Link>
+		</li>
+	);
+	const conceptRefListEn = sortByLabelEn(conceptRef).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg2}
+			</Link>
+		</li>
+	);
+	const conceptSucceedListFr = sortByLabelFr(conceptSucceed).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg1}
+			</Link>
+		</li>
+	);
+	const conceptSucceedListEn = sortByLabelEn(conceptSucceed).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg2}
+			</Link>
+		</li>
+	);
+	const conceptLinkListFr = sortByLabelFr(conceptLink).map(item =>
+		<li key={item.id}>
+			<Link to={'/concept/' + item.id}>
+				{item.prefLabelLg1}
+			</Link>
+		</li>
+	);
+	const conceptLinkListEn = sortByLabelEn(conceptLink).map(item =>
+		<li key={item.idLinked}>
+			<Link to={'/concept/' + item.idLinked}>
+				{item.prefLabelLg2}
+			</Link>
+		</li>
+	);
 
-  const isEmpty = array => {
-    if (array.length === 0) return false;
-    else return true;
-  };
+	const isEmpty = array => {
+		if (array.length === 0) return false;
+		else return true;
+	};
 
-  return (
-    <div className="row">
-      <div className={cl}>
-        <Panel title={dictionary.links.title}>
-          <ul>
-            {isEmpty(conceptParent) &&
-              <li>
-                {dictionary.links.narrower} :
-                <ul>{conceptParentListFr}</ul>
-              </li>}
-            {isEmpty(conceptEnfants) &&
-              <li>
-                {dictionary.links.broader} :
-                <ul>{conceptEnfantsListFr}</ul>
-              </li>}
-            {isEmpty(conceptRef) &&
-              <li>
-                {dictionary.links.references} :
-                <ul>{conceptRefListFr}</ul>
-              </li>}
-            {isEmpty(conceptSucceed) &&
-              <li>
-                {dictionary.links.replaces} :
-                <ul>{conceptSucceedListFr}</ul>
-              </li>}
-            {isEmpty(conceptLink) &&
-              <li>
-                {dictionary.links.related} :
-                <ul>{conceptLinkListFr}</ul>
-              </li>}
-          </ul>
-        </Panel>
-      </div>
-      {english &&
-        <div className={cl}>
-          <Panel title={dictionary.links.title}>
-            <ul>
-              {isEmpty(conceptParent) &&
-                <li>
-                  {dictionary.links.narrower} :
-                  <ul>{conceptParentListEn}</ul>
-                </li>}
-              {isEmpty(conceptEnfants) &&
-                <li>
-                  {dictionary.links.broader} :
-                  <ul>{conceptEnfantsListEn}</ul>
-                </li>}
-              {isEmpty(conceptRef) &&
-                <li>
-                  {dictionary.links.references} :
-                  <ul>{conceptRefListEn}</ul>
-                </li>}
-              {isEmpty(conceptSucceed) &&
-                <li>
-                  {dictionary.links.replaces} :
-                  <ul>{conceptSucceedListEn}</ul>
-                </li>}
-              {isEmpty(conceptLink) &&
-                <li>
-                  {dictionary.links.related} :
-                  <ul>{conceptLinkListEn}</ul>
-                </li>}
-            </ul>
-          </Panel>
-        </div>}
-    </div>
-  );
+	// Don't display links panel if there isn't links
+	if (nbLinks === 0) return null;
+
+	return (
+		<div className="row">
+			<div className={cl}>
+				<Panel title={dictionary.links.title}>
+					<ul>
+						{isEmpty(conceptParent) &&
+							<li>
+								{dictionary.links.narrower} :
+								<ul>{conceptParentListFr}</ul>
+							</li>}
+						{isEmpty(conceptEnfants) &&
+							<li>
+								{dictionary.links.broader} :
+								<ul>{conceptEnfantsListFr}</ul>
+							</li>}
+						{isEmpty(conceptRef) &&
+							<li>
+								{dictionary.links.references} :
+								<ul>{conceptRefListFr}</ul>
+							</li>}
+						{isEmpty(conceptSucceed) &&
+							<li>
+								{dictionary.links.replaces} :
+								<ul>{conceptSucceedListFr}</ul>
+							</li>}
+						{isEmpty(conceptLink) &&
+							<li>
+								{dictionary.links.related} :
+								<ul>{conceptLinkListFr}</ul>
+							</li>}
+					</ul>
+				</Panel>
+			</div>
+			{english &&
+				<div className={cl}>
+					<Panel title={dictionary.links.title}>
+						<ul>
+							{isEmpty(conceptParent) &&
+								<li>
+									{dictionary.links.narrower} :
+									<ul>{conceptParentListEn}</ul>
+								</li>}
+							{isEmpty(conceptEnfants) &&
+								<li>
+									{dictionary.links.broader} :
+									<ul>{conceptEnfantsListEn}</ul>
+								</li>}
+							{isEmpty(conceptRef) &&
+								<li>
+									{dictionary.links.references} :
+									<ul>{conceptRefListEn}</ul>
+								</li>}
+							{isEmpty(conceptSucceed) &&
+								<li>
+									{dictionary.links.replaces} :
+									<ul>{conceptSucceedListEn}</ul>
+								</li>}
+							{isEmpty(conceptLink) &&
+								<li>
+									{dictionary.links.related} :
+									<ul>{conceptLinkListEn}</ul>
+								</li>}
+						</ul>
+					</Panel>
+				</div>}
+		</div>
+	);
 }
 
 export default ConceptLinks;
