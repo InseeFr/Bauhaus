8725829257524f0649d27d5e00819fa0cbaa2ada
Jean-Baptiste RUDANT <jb@noknot.fr>
2017-08-01 15:42:54 +0200

    Sort list of concepts to validate

--- src/js/actions/concepts/validate-list.js
+++ src/js/actions/concepts/validate-list.js
@@ -1,9 +1,20 @@
 import api from 'js/remote-api/api';
 import buildAsyncAction from '../utils/build-async-action';
 import * as A from '../constants';
+import { sortArray } from 'js/utils/array-utils';
 
-export default buildAsyncAction(api.getConceptValidateList, [
-  A.LOAD_CONCEPT_VALIDATE_LIST,
-  A.LOAD_CONCEPT_VALIDATE_LIST_SUCCESS,
-  A.LOAD_CONCEPT_VALIDATE_LIST_FAILURE,
-]);
+const sortByLabel = sortArray('label');
+
+export default buildAsyncAction(
+  api.getConceptValidateList,
+  [
+    A.LOAD_CONCEPT_VALIDATE_LIST,
+    A.LOAD_CONCEPT_VALIDATE_LIST_SUCCESS,
+    A.LOAD_CONCEPT_VALIDATE_LIST_FAILURE,
+  ],
+  null, // no payload
+  //process response
+  () => results => {
+    return sortByLabel(results);
+  }
+);
