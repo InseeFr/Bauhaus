cf6151f8cb3c63747be2424cd90f93d7b5759511
Nicolas Laval <nicolas.laval@insee.fr>
2017-08-09 12:36:37 +0200

    Fix issue with changeNote to send to server

--- src/js/utils/concepts/build-payload-creation-update/build-payload-update.js
+++ src/js/utils/concepts/build-payload-creation-update/build-payload-update.js
@@ -1,39 +1,40 @@
 import { processLinks, processGeneral } from './shared';
 
 import {
-  processVersionableChanges,
-  processDatableChanges,
+	processVersionableChanges,
+	keepDatableNotes,
 } from 'js/utils/concepts/notes';
 import { VERSIONING } from 'js/constants';
 
 //only `isValidated` is not sent
 const generalFieldsToKeep = [
-  'prefLabelLg1',
-  'prefLabelLg2',
-  'altLabelLg1',
-  'altLabelLg2',
-  'creator',
-  'created',
-  'contributor',
-  'disseminationStatus',
-  'additionalMaterial',
-  'valid',
+	'prefLabelLg1',
+	'prefLabelLg2',
+	'altLabelLg1',
+	'altLabelLg2',
+	'creator',
+	'created',
+	'contributor',
+	'disseminationStatus',
+	'additionalMaterial',
+	'valid',
 ];
 
 export default function buildPayloadUpdate(versioning, oldConcept, concept) {
-  const { notes: oldNotes } = oldConcept;
-  const { general: rawGeneral, notes, conceptsWithLinks } = concept;
+	const { notes: oldNotes } = oldConcept;
+	const { general: rawGeneral, notes, conceptsWithLinks } = concept;
 
-  const general = processGeneral(rawGeneral, generalFieldsToKeep);
-  const links = processLinks(conceptsWithLinks);
+	const general = processGeneral(rawGeneral, generalFieldsToKeep);
+	const links = processLinks(conceptsWithLinks);
 
-  //TODO check if there is no `isValidated` property within general
-  return {
-    versioning: versioning === VERSIONING ? true : false,
-    ...general, //prefLabelLg1, prefLabelLg2...
-    links,
-    //[{ noteType: 'scopeNoteLg2', content: 'new note'} ...]
-    datableNotes: processDatableChanges(oldNotes, notes),
-    versionableNotes: processVersionableChanges(oldNotes, notes),
-  };
+	//TODO check if there is no `isValidated` property within general
+	return {
+		versioning: versioning === VERSIONING ? true : false,
+		...general, //prefLabelLg1, prefLabelLg2...
+		links,
+		//[{ noteType: 'scopeNoteLg2', content: 'new note'} ...]
+		//datableNotes: processDatableChanges(oldNotes, notes),
+		datableNotes: keepDatableNotes(notes),
+		versionableNotes: processVersionableChanges(oldNotes, notes),
+	};
 }
--- src/js/utils/concepts/notes.js
+++ src/js/utils/concepts/notes.js
@@ -3,12 +3,12 @@ import objectFromKeys from 'js/utils/object-from-keys';
 import { rawHtmlToRmesHtml } from 'js/utils/html';
 
 export const versionableNotes = [
-  'scopeNoteLg1',
-  'scopeNoteLg2',
-  'definitionLg1',
-  'definitionLg2',
-  'editorialNoteLg1',
-  'editorialNoteLg2',
+	'scopeNoteLg1',
+	'scopeNoteLg2',
+	'definitionLg1',
+	'definitionLg2',
+	'editorialNoteLg1',
+	'editorialNoteLg2',
 ];
 
 export const datableNotes = ['changeNoteLg1', 'changeNoteLg2'];
@@ -18,46 +18,60 @@ const allNotes = [...versionableNotes, ...datableNotes];
 export const fields = allNotes;
 
 export const emptyNotes = fields.reduce((notes, typeOfNote) => {
-  notes[typeOfNote] = '';
-  return notes;
+	notes[typeOfNote] = '';
+	return notes;
 }, {});
 
 export const capitalizeFirst = str =>
-  str.charAt(0).toUpperCase() + str.slice(1);
+	str.charAt(0).toUpperCase() + str.slice(1);
 
 export const createNotes = notes => {};
 
 export const processChanges = (oldNotes, notes, fields) =>
-  fields.reduce((changes, noteType) => {
-    const oldContent = oldNotes[noteType];
-    const content = notes[noteType];
-    if (oldContent !== content)
-      changes.push({
-        noteType,
-        //format the note the `rmes` way (with a wrapping div and a
-        //namespace attribte).
-        content: rawHtmlToRmesHtml(content),
-      });
-    return changes;
-  }, []);
+	fields.reduce((changes, noteType) => {
+		const oldContent = oldNotes[noteType];
+		const content = notes[noteType];
+		if (oldContent !== content)
+			changes.push({
+				noteType,
+				//format the note the `rmes` way (with a wrapping div and a
+				//namespace attribte).
+				content: rawHtmlToRmesHtml(content),
+			});
+		return changes;
+	}, []);
+
+export const processNotes = (notesToKeep, fields) =>
+	fields.reduce((notes, noteType) => {
+		const content = notesToKeep[noteType];
+		if (content)
+			notes.push({
+				noteType,
+				//format the note the `rmes` way (with a wrapping div and a
+				//namespace attribte).
+				content: rawHtmlToRmesHtml(content),
+			});
+		return notes;
+	}, []);
 
 export const processVersionableChanges = (oldNotes, notes) =>
-  processChanges(oldNotes, notes, versionableNotes);
+	processChanges(oldNotes, notes, versionableNotes);
 export const processDatableChanges = (oldNotes, notes) =>
-  processChanges(oldNotes, notes, datableNotes);
+	processChanges(oldNotes, notes, datableNotes);
 export const processAllChanges = (oldNotes, notes) =>
-  processChanges(oldNotes, notes, allNotes);
+	processChanges(oldNotes, notes, allNotes);
+export const keepDatableNotes = notes => processNotes(notes, datableNotes);
 
 const versionImpactingNotes = [
-  'scopeNoteLg1',
-  'definitionLg1',
-  'noteEditorialeLg1',
+	'scopeNoteLg1',
+	'definitionLg1',
+	'noteEditorialeLg1',
 ];
 
 export const areNotesImpactingVersionChanged = (oldNotes, notes) => {
-  return processChanges(oldNotes, notes, versionImpactingNotes).length > 0;
+	return processChanges(oldNotes, notes, versionImpactingNotes).length > 0;
 };
 
 export const propTypes = PropTypes.shape(
-  objectFromKeys(fields, PropTypes.string)
+	objectFromKeys(fields, PropTypes.string)
 );
