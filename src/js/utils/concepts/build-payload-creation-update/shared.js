a77e898f5ba2b2b514c72d25e46f7581899d88b6
Nicolas Laval <nicolas.laval@insee.fr>
2017-12-08 11:29:39 +0100

    Fix issue with DatePicker default hour

--- src/js/utils/concepts/build-payload-creation-update/shared.js
+++ src/js/utils/concepts/build-payload-creation-update/shared.js
@@ -3,7 +3,7 @@ import prefixWithHttp from 'js/utils/prefix-with-http';
 import takeKeys from 'js/utils/take-keys';
 
 export function processLinks(conceptsWithLinks) {
-  /*
+	/*
     Links should like [
       {
         typeOfLink: 'broader',
@@ -18,28 +18,32 @@ export function processLinks(conceptsWithLinks) {
     //no entry if there no links of a given type
     }
   */
-  //First we represent the information as an object to allow iterating over
-  //the object keys later
-  const linksObj = conceptsWithLinks.reduce((links, { id, typeOfLink }) => {
-    if (typeOfLink === NONE) return links;
-    if (!links[typeOfLink]) links[typeOfLink] = [id];
-    else links[typeOfLink].push(id);
-    return links;
-  }, {});
-  //We should create an entry for each of the key of `linksOb`
-  return Object.keys(linksObj).reduce((linkArr, typeOfLink) => {
-    const ids = linksObj[typeOfLink];
-    linkArr.push({
-      typeOfLink,
-      ids,
-    });
-    return linkArr;
-  }, []);
+	//First we represent the information as an object to allow iterating over
+	//the object keys later
+	const linksObj = conceptsWithLinks.reduce((links, { id, typeOfLink }) => {
+		if (typeOfLink === NONE) return links;
+		if (!links[typeOfLink]) links[typeOfLink] = [id];
+		else links[typeOfLink].push(id);
+		return links;
+	}, {});
+	//We should create an entry for each of the key of `linksOb`
+	return Object.keys(linksObj).reduce((linkArr, typeOfLink) => {
+		const ids = linksObj[typeOfLink];
+		linkArr.push({
+			typeOfLink,
+			ids,
+		});
+		return linkArr;
+	}, []);
 }
 
 export function processGeneral(general, keys) {
-  const extract = takeKeys(keys);
-  general = extract(general);
-  general.additionalMaterial = prefixWithHttp(general.additionalMaterial);
-  return general;
+	const extract = takeKeys(keys);
+	general = extract(general);
+	general.additionalMaterial = prefixWithHttp(general.additionalMaterial);
+	general.valid = general.valid.replace(
+		/T[0-9]{2}:00:00.000Z/,
+		'T00:00:00.000Z'
+	);
+	return general;
 }
