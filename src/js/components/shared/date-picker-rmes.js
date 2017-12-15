4b10c6c9d4c2085a24a83621e1af92f3ac6177fe
Nicolas Laval <nicolas.laval@insee.fr>
2017-12-15 09:51:53 +0100

    Update dependencies versions

--- src/js/components/shared/date-picker-rmes.js
+++ src/js/components/shared/date-picker-rmes.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import DatePicker from 'react-bootstrap-date-picker';
+import DatePicker from 'react-16-bootstrap-date-picker';
 import { dictionary } from 'js/utils/dictionary';
 
 function DatePickerRmes({ value, onChange, placement }) {
-  const days = dictionary.calendar.days;
-  const months = dictionary.calendar.months;
+	const days = dictionary.calendar.days;
+	const months = dictionary.calendar.months;
 
-  return (
-    <DatePicker
-      className="form-control"
-      value={value}
-      calendarPlacement={placement}
-      onChange={onChange}
-      weekStartsOnMonday={true}
-      dayLabels={days}
-      monthLabels={months}
-    />
-  );
+	return (
+		<DatePicker
+			className="form-control"
+			value={value}
+			calendarPlacement={placement}
+			onChange={onChange}
+			weekStartsOnMonday={true}
+			dayLabels={days}
+			monthLabels={months}
+		/>
+	);
 }
 
 export default DatePickerRmes;
