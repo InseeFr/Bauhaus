2d7df6520129c20a9a42012cf899596ee75ed5f5
Nicolas Laval <nicolas.laval@insee.fr>
2017-12-21 15:10:54 +0100

    Update dashboard reducers

--- src/js/reducers/dashboard/index.js
+++ src/js/reducers/dashboard/index.js
@@ -1,11 +1,21 @@
 import listReducer from '../utils/list-reducer';
 import * as A from 'js/actions/constants';
 
-const collectionDashboardList = listReducer([
-	A.LOAD_COLLECTION_DASHBOARD_LIST,
-	A.LOAD_COLLECTION_DASHBOARD_LIST_SUCCESS,
-	A.LOAD_COLLECTION_DASHBOARD_LIST_FAILURE,
-]);
+export const handleReset = (reducer, actions) => (state, action) => {
+	//we pass the action to the reducer: it should have no effect, we just want
+	//to reinitialize the reducer but we cannot pas `undefined` as an action
+	if (actions.indexOf(action.type) !== -1) return reducer(undefined, action);
+	return reducer(state, action);
+};
+
+const collectionDashboardList = handleReset(
+	listReducer([
+		A.LOAD_COLLECTION_DASHBOARD_LIST,
+		A.LOAD_COLLECTION_DASHBOARD_LIST_SUCCESS,
+		A.LOAD_COLLECTION_DASHBOARD_LIST_FAILURE,
+	]),
+	[A.CREATE_COLLECTION, A.UPDATE_COLLECTION, A.VALIDATE_COLLECTION_LIST]
+);
 
 export default {
 	collectionDashboardList,
