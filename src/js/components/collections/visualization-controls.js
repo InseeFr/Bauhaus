8bdf55b376291515deb01bcd584e4932e361de0f
Nicolas Laval <nicolas.laval@insee.fr>
2018-01-09 09:25:37 +0100

    Fix collection visualization return

--- src/js/components/collections/visualization-controls.js
+++ src/js/components/collections/visualization-controls.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { dictionary } from 'js/utils/dictionary';
 
-const Button = ({ action, label, disabled }) => {
+const Button = ({ action, label }) => {
 	let button;
 	if (typeof action === 'string') {
 		button = (
@@ -30,30 +30,33 @@ Button.propTypes = {
 	label: PropTypes.string.isRequired,
 };
 
-function CollectionVisualizationControls({
-	isValidated,
-	id,
-	handleValidation,
-}) {
-	let btns;
-	const cancel = ['/collections', dictionary.buttons.return];
-	const send = [`/collection/${id}/send`, dictionary.buttons.send];
-	const validate = [handleValidation, dictionary.buttons.validate];
-	const update = [`/collection/${id}/modify`, dictionary.buttons.modify];
+class CollectionVisualizationControls extends Component {
+	render() {
+		const { isValidated, id, handleValidation } = this.props;
 
-	btns = isValidated
-		? [cancel, null, null, null, send, update]
-		: [cancel, null, null, send, update, validate];
+		let btns;
+		const cancel = [
+			() => this.props.history.goBack(),
+			dictionary.buttons.return,
+		];
+		const send = [`/collection/${id}/send`, dictionary.buttons.send];
+		const validate = [handleValidation, dictionary.buttons.validate];
+		const update = [`/collection/${id}/modify`, dictionary.buttons.modify];
 
-	return (
-		<div className="row btn-line">
-			{btns.map((btn, i) => {
-				if (!btn) return <PlaceHolder key={i} />;
-				const [action, label] = btn;
-				return btn && <Button key={label} action={action} label={label} />;
-			})}
-		</div>
-	);
+		btns = isValidated
+			? [cancel, null, null, null, send, update]
+			: [cancel, null, null, send, update, validate];
+
+		return (
+			<div className="row btn-line">
+				{btns.map((btn, i) => {
+					if (!btn) return <PlaceHolder key={i} />;
+					const [action, label] = btn;
+					return btn && <Button key={label} action={action} label={label} />;
+				})}
+			</div>
+		);
+	}
 }
 
 CollectionVisualizationControls.propTypes = {
@@ -62,4 +65,4 @@ CollectionVisualizationControls.propTypes = {
 	handleValidation: PropTypes.func.isRequired,
 };
 
-export default CollectionVisualizationControls;
+export default withRouter(CollectionVisualizationControls);
