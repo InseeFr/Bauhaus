01056556e6b60dafd422d92850ca8dd9bf0b9027
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-15 15:16:17 +0100

    v1.1.2

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ export const baseHostConcepts = `${baseHost}/concepts`;
 export const baseHostOperations = `${baseHost}/operations`;
 
 // App langages
-export const version = '1.1.1';
+export const version = '1.1.2';
 export const lang1 = 'fr';
 export const lang2 = 'en';
 export const authConfig = true;
