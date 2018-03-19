6bde4394f34285923fe9a3e972ab60ea70f7833f
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-19 10:54:55 +0100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Root from 'js/components/Root';
+import Error from 'js/components/shared/error';
 import configureStore from 'js/store/configure-store';
 import Api from 'js/remote-api/api';
 import './css/index.css';
 
-// TODO Loading spinner ??
 Api.getInit()
 	.then(
-		res => {
-			if (res.ok) {
-				return res.json();
-			} else return Promise.reject(res.statusText);
-		},
-		err => Promise.reject(err.toString())
+		res => (res.ok ? res.json() : Promise.reject(res.statusText)),
+		err => {
+			renderApp(Error, {}, { home: true });
+			return Promise.reject(err.toString());
+		}
 	)
-	.then(res => {
-		const initState = res;
-		const renderApp = (Component, initState) => {
-			const { authType: type, ...properties } = initState;
-			const store = configureStore({
-				app: { auth: { type }, properties, secondLang: false },
-			});
-			ReactDOM.render(
-				<Provider store={store}>
-					<Component />
-				</Provider>,
-				document.getElementById('root')
-			);
-		};
-		renderApp(Root, initState);
+	.then(res => renderApp(Root, res));
+
+const renderApp = (Component, initState, props) => {
+	const { authType: type, ...properties } = initState;
+	const store = configureStore({
+		app: { auth: { type }, properties, secondLang: false },
 	});
+	ReactDOM.render(
+		<Provider store={store}>
+			<Component {...props} />
+		</Provider>,
+		document.getElementById('root')
+	);
+};
