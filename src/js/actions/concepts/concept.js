6dbf058dfd86e35c729a7186077dd14bbffe8b13
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-19 17:41:42 +0100

    Update concepts actions

--- src/js/actions/concepts/concept.js
+++ src/js/actions/concepts/concept.js
@@ -1,13 +1,6 @@
 import loadLinks from './links';
-import loadNotes from './notes-version';
 import loadGeneral from './general';
 
 export default id => dispatch => {
-	//handy to return a promise in case we want to chain other actions
-	return Promise.all([
-		dispatch(loadGeneral(id)).then(({ conceptVersion }) =>
-			dispatch(loadNotes(id, conceptVersion))
-		),
-		dispatch(loadLinks(id)),
-	]);
+	return Promise.all([dispatch(loadGeneral(id)), dispatch(loadLinks(id))]);
 };
--- src/js/actions/concepts/general-and-notes.js
+++
@@ -1,7 +0,0 @@
-import loadGeneral from './general';
-import loadNotes from './notes-version';
-
-export default id => dispatch =>
-	dispatch(loadGeneral(id)).then(general => {
-		dispatch(loadNotes(id, general.conceptVersion));
-	});
