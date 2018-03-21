8fb3b58dd0cfd8753ab9fd798ed061a640099393
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-21 16:36:15 +0100

    Add panel-html component tests

---
+++ src/js/components/shared/panel-html.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PanelHtml from './panel-html';
+
+describe('panel-html', () => {
+	it('renders without crashing', () => {
+		shallow(<PanelHtml title="title">{'<div>Text<div>'}</PanelHtml>);
+	});
+
+	it('returns component title', () => {
+		const wrapper = shallow(
+			<PanelHtml title="title">{'<div>Text<div>'}</PanelHtml>
+		);
+		expect(wrapper.find('.panel-title').text()).toEqual('title');
+	});
+
+	it('returns panel body', () => {
+		const wrapper = shallow(
+			<PanelHtml title="title">{'<div>Text<div>'}</PanelHtml>
+		);
+		console.log(wrapper.find('.panel-body').html());
+		expect(wrapper.find('.panel-body').exists()).toBeTruthy();
+	});
+});
