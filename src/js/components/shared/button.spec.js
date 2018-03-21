dda3db242bcd143d26c37d09a60e54ec2ca066fc
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-21 12:03:15 +0100

    Add button component tests

---
+++ src/js/components/shared/button.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import Button from './button';
+
+describe('button', () => {
+	it('renders without crashing', () => {
+		shallow(<Button action="" label="" />);
+	});
+
+	it('contains well-formed Link tag', () => {
+		const wrapper = shallow(<Button action="/home" label="myButton" />);
+		const link = (
+			<Link className="btn btn-primary btn-lg col-md-12" to="/home">
+				myButton
+			</Link>
+		);
+		expect(wrapper.contains(link)).toEqual(true);
+	});
+
+	it('contains well-formed button tag', () => {
+		const onClick = e => '';
+		const wrapper = shallow(<Button action={onClick} label="myButton" />);
+		const button = (
+			<button className="btn btn-primary btn-lg col-md-12" onClick={onClick}>
+				myButton
+			</button>
+		);
+		expect(wrapper.contains(button)).toEqual(true);
+	});
+});
