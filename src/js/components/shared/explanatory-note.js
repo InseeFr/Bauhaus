2251a98fa1b971271dd5e34caeb19205546cd0f9
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-21 13:22:17 +0100

    Add explanatory-note component tests

--- src/js/components/shared/explanatory-note.js
+++ src/js/components/shared/explanatory-note.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PanelHtml from 'js/components/shared/panel-html';
 import flag from 'js/components/shared/flag';
 
-export function ExplanatoryNote({ text, title, lang, alone }) {
+export const ExplanatoryNote = ({ text, title, lang, alone }) => {
 	const cl = alone ? 'col-md-12' : 'col-md-6';
-	if (!{ text }) return null;
+	if (!text) return null;
 	return (
 		<div className={cl}>
 			<PanelHtml title={<NoteFlag text={title} lang={lang} />}>
@@ -12,13 +12,11 @@ export function ExplanatoryNote({ text, title, lang, alone }) {
 			</PanelHtml>
 		</div>
 	);
-}
+};
 
-function NoteFlag({ text, lang }) {
-	return (
-		<div>
-			{text + '  ( '}
-			{flag(lang)} )
-		</div>
-	);
-}
+export const NoteFlag = ({ text, lang }) => (
+	<div>
+		{text + '  ( '}
+		{flag(lang)} )
+	</div>
+);
---
+++ src/js/components/shared/explanatory-note.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExplanatoryNote, NoteFlag } from './explanatory-note';
+
+describe('explanatory-note', () => {
+	it('renders without crashing', () => {
+		shallow(<ExplanatoryNote />);
+	});
+
+	it('renders null component', () => {
+		const wrapper = shallow(<ExplanatoryNote />);
+		expect(wrapper.html()).toBeNull();
+	});
+
+	it('renders not null component', () => {
+		const wrapper = shallow(<ExplanatoryNote text="text" />);
+		expect(wrapper.html()).not.toBeNull();
+	});
+});
+
+describe('note-flag', () => {
+	it('renders without crashing', () => {
+		shallow(<NoteFlag />);
+	});
+});
