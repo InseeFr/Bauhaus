cb441f69fcb39db40b6569929fd56785dfdbeca9
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-03 13:09:40 +0200

    Fix issue with reducer tests

--- src/js/reducers/concepts/by-id/index.spec.js
+++ src/js/reducers/concepts/by-id/index.spec.js
@@ -1,4 +1,5 @@
 import { getConcept } from './';
+import { LOADED } from 'js/constants';
 
 describe('getConcept', () => {
 	test('getConcept selector should extract results', () => {
@@ -13,12 +14,23 @@ describe('getConcept', () => {
 	test('getConcept selector should extract results', () => {
 		const result = getConcept(
 			{
-				general: { id1: { results: 'general id1' } },
-
+				general: {
+					id1: { results: { prefLabelLg1: 'My concept', conceptVersion: '2' } },
+				},
+				notes: {
+					id1: {
+						1: { status: LOADED, results: 'id1 notes v1' },
+						2: { status: LOADED, results: 'id1 notes v2' },
+					},
+				},
 				links: { id1: { results: 'links id1' } },
 			},
 			'id1'
 		);
-		expect(result).toEqual();
+		expect(result).toEqual({
+			general: { prefLabelLg1: 'My concept', conceptVersion: '2' },
+			notes: 'id1 notes v2',
+			links: 'links id1',
+		});
 	});
 });
