2a1f1bf28dcea0e419c32adce32067674aa04129
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-10 15:42:14 +0200

    Update storybook script

--- scripts/storybook.sh
+++ scripts/storybook.sh
@@ -2,22 +2,27 @@
 
 set -e
 
+TEMP_FOLDER="temp"
+UPSTREAM="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+STORY_BOOK_FOLDER="storybook-static"
+MESSAGE="Rebuild storybook for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
+AUTHOR="$USER <>"
+
 function build() {
   yarn build-storybook
 }
 
 function publish() {
-  git remote -v
-  git branch story origin/gh-pages
-  git checkout story
-  git checkout master -- storybook-static
-  git add .
-  git commit -m "Deploy Storybook to GitHub Pages"
-  git push
-  git checkout master
-  git rm -rf storybook-static
-  git commit -m "Delete storybook-static from master"
-  git push
+  pushd "$TEMP_FOLDER"
+  git init
+  git remote add upstream "$UPSTREAM"
+  git fetch --prune upstream
+  git pull upstream/gh-pages cp "$STORY_BOOK_FOLDER" . -r
+  git add --all .
+  if git commit --message "$MESSAGE" --author "$AUTHOR" ; then
+  git push --quiet upstream HEAD:gh-pages
+  fi
+  popd
 }
 
 function main() {
