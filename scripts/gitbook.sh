cc68d60c23722cc7ed4c1959c60bfde41f0c67db
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-12 14:02:27 +0200

    Fix file name inversion

--- scripts/gitbook.sh
+++ scripts/gitbook.sh
@@ -2,27 +2,39 @@
 
 set -e
 
-DOC_FOLDER="built-storybook"
+DOC_FOLDER="docs"
 MAIN_BRANCH="master"
 UPSTREAM="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-MESSAGE="Rebuild storybook for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
+MESSAGE="Rebuild doc for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
 AUTHOR="$USER <>"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];then
-  echo "Storybook won't build on pull request"
+  echo "Documentation won't build on pull request"
   exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" != "$MAIN_BRANCH" ];then
-  echo "Storybook won't build: Not on branch $MAIN_BRANCH"
+  echo "Documentation won't build: Not on branch $MAIN_BRANCH"
   exit 0
 fi
 
-function publish() {
+function setup() {
+  npm install -g gitbook-cli
+}
+
+function build() {
   pushd "$DOC_FOLDER"
+  gitbook install
+  gitbook build
+  popd
+}
+
+function publish() {
+  pushd "$DOC_FOLDER"/_book
   git init
   git remote add upstream "$UPSTREAM"
   git fetch --prune upstream
+  git reset upstream/gh-pages
   git add --all .
   if git commit --message "$MESSAGE" --author "$AUTHOR" ; then
     git push --quiet upstream HEAD:gh-pages
@@ -30,4 +42,8 @@ function publish() {
   popd
 }
 
-publish
+function main() {
+  setup && build && publish
+}
+
+main
--- scripts/storybook.sh
+++ scripts/storybook.sh
@@ -2,39 +2,27 @@
 
 set -e
 
-DOC_FOLDER="docs"
+DOC_FOLDER="built-storybook"
 MAIN_BRANCH="master"
 UPSTREAM="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-MESSAGE="Rebuild doc for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
+MESSAGE="Rebuild storybook for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
 AUTHOR="$USER <>"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];then
-  echo "Documentation won't build on pull request"
+  echo "Storybook won't build on pull request"
   exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" != "$MAIN_BRANCH" ];then
-  echo "Documentation won't build: Not on branch $MAIN_BRANCH"
+  echo "Storybook won't build: Not on branch $MAIN_BRANCH"
   exit 0
 fi
 
-function setup() {
-  npm install -g gitbook-cli
-}
-
-function build() {
-  pushd "$DOC_FOLDER"
-  gitbook install
-  gitbook build
-  popd
-}
-
 function publish() {
-  pushd "$DOC_FOLDER"/_book
+  pushd "$DOC_FOLDER"
   git init
   git remote add upstream "$UPSTREAM"
   git fetch --prune upstream
-  git reset upstream/gh-pages
   git add --all .
   if git commit --message "$MESSAGE" --author "$AUTHOR" ; then
     git push --quiet upstream HEAD:gh-pages
@@ -42,8 +30,4 @@ function publish() {
   popd
 }
 
-function main() {
-  setup && build && publish
-}
-
-main
+publish
