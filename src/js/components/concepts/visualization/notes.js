1ff620181ce8034e0d274b969da892991502b222
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-14 10:02:44 +0200

    Update concept note visualization

--- src/js/components/concepts/visualization/notes.js
+++ src/js/components/concepts/visualization/notes.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NoteVisualization from 'js/components/shared/note-visualization';
-import D from 'js/i18n';
+import { buildNotes } from 'js/utils/concepts/notes';
 
 function ConceptNotes({ secondLang, notes, langs }) {
-	const {
-		scopeNoteLg1,
-		scopeNoteLg2,
-		definitionLg1,
-		definitionLg2,
-		editorialNoteLg1,
-		editorialNoteLg2,
-		changeNoteLg1,
-		changeNoteLg2,
-	} = notes;
-	const params = [
-		{ lg1: scopeNoteLg1, lg2: scopeNoteLg2, title: D.conceptsScopeNote },
-		{ lg1: definitionLg1, lg2: definitionLg2, title: D.conceptsDefinition },
-		{
-			lg1: editorialNoteLg1,
-			lg2: editorialNoteLg2,
-			title: D.conceptsEditorialNote,
-		},
-		{ lg1: changeNoteLg1, lg2: changeNoteLg2, title: D.conceptsChangeNote },
-	];
 	return (
-		<NoteVisualization params={params} langs={langs} secondLang={secondLang} />
+		<NoteVisualization
+			params={buildNotes(notes)}
+			langs={langs}
+			secondLang={secondLang}
+		/>
 	);
 }
 
 ConceptNotes.propTypes = {
 	secondLang: PropTypes.bool.isRequired,
 	notes: PropTypes.object.isRequired,
+	langs: PropTypes.object.isRequired,
 };
 export default ConceptNotes;
