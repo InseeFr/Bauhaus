e9883c4de6402e1f83ff28c939331af74ccd4d4f
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-14 12:07:25 +0200

    Update classification item reducer

--- src/js/reducers/classifications/item/index.js
+++ src/js/reducers/classifications/item/index.js
@@ -1,5 +1,5 @@
 import { getGeneral } from './general';
-import { getNotes } from './notes';
+import { getNotes, getAllNotes } from './notes';
 import { getNarrowers } from './narrowers';
 
 export function getItem(state, classificationId, itemId) {
@@ -22,6 +22,33 @@ export function getItem(state, classificationId, itemId) {
 		general.conceptVersion
 	);
 	if (!notes) return { general, narrowers };
+	return {
+		general,
+		notes,
+		narrowers,
+	};
+}
+
+export function getFullItem(state, classificationId, itemId) {
+	const general = getGeneral(
+		state.classificationItemGeneral,
+		classificationId,
+		itemId
+	);
+	if (!general) return;
+	const narrowers = getNarrowers(
+		state.classificationItemNarrowers,
+		classificationId,
+		itemId
+	);
+	if (!narrowers) return;
+	const notes = getAllNotes(
+		state.classificationItemNotes,
+		classificationId,
+		itemId,
+		general.conceptVersion
+	);
+	if (!notes) return;
 	return {
 		general,
 		notes,
--- src/js/reducers/classifications/item/index.spec.js
+++ src/js/reducers/classifications/item/index.spec.js
@@ -1,85 +1,169 @@
-import { getItem } from './';
+import { getItem, getFullItem } from './';
 import { LOADED } from 'js/constants';
 
-describe('getItem', () => {
-	test('getItem selector should extract results', () => {
-		const result = getItem(
-			{
-				classificationItemGeneral: {},
-				classificationItemNotes: {},
-				classificationItemMembers: {},
-			},
-			'nafr2',
-			'nafr2'
-		);
-		expect(result).toEqual();
-	});
-	test('getConcept selector should extract results', () => {
-		const result = getItem(
-			{
-				classificationItemGeneral: {
-					nafr2: {
-						A: {
-							status: LOADED,
-							results: { prefLabelLg1: 'label', conceptVersion: 2 },
+describe('classification-item-selector', () => {
+	describe('getItem', () => {
+		test('getItem selector should extract results', () => {
+			const result = getItem(
+				{
+					classificationItemGeneral: {},
+					classificationItemNotes: {},
+					classificationItemMembers: {},
+				},
+				'nafr2',
+				'nafr2'
+			);
+			expect(result).toEqual();
+		});
+		test('getItem selector should extract results', () => {
+			const result = getItem(
+				{
+					classificationItemGeneral: {
+						nafr2: {
+							A: {
+								status: LOADED,
+								results: { prefLabelLg1: 'label', conceptVersion: 2 },
+							},
+						},
+					},
+					classificationItemNotes: {
+						nafr2: {
+							A: {
+								1: { status: LOADED, results: 'nafr2 - A - notes v1' },
+								2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+							},
+						},
+					},
+					classificationItemNarrowers: {
+						nafr2: {
+							A: { status: LOADED, results: 'narrowers' },
 						},
 					},
 				},
-				classificationItemNotes: {
-					nafr2: {
-						A: {
-							1: { status: LOADED, results: 'nafr2 - A - notes v1' },
-							2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+				'nafr2',
+				'A'
+			);
+			expect(result).toEqual({
+				general: { prefLabelLg1: 'label', conceptVersion: 2 },
+				notes: 'nafr2 - A - notes v2',
+				narrowers: 'narrowers',
+			});
+		});
+		test('getItem selector should extract results', () => {
+			const result = getItem(
+				{
+					classificationItemGeneral: {
+						nafr2: {
+							A: {
+								status: LOADED,
+								results: { prefLabelLg1: 'label', conceptVersion: 2 },
+							},
+						},
+					},
+					classificationItemNotes: {
+						nafr2: {
+							A: {
+								1: { status: LOADED, results: 'nafr2 - A - notes v1' },
+								2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+							},
+						},
+					},
+					classificationItemNarrowers: {
+						nafr2: {
+							A: { status: LOADED, results: 'narrowers' },
 						},
 					},
 				},
-				classificationItemNarrowers: {
-					nafr2: {
-						A: { status: LOADED, results: 'narrowers' },
-					},
-				},
-			},
-			'nafr2',
-			'A'
-		);
-		expect(result).toEqual({
-			general: { prefLabelLg1: 'label', conceptVersion: 2 },
-			notes: 'nafr2 - A - notes v2',
-			narrowers: 'narrowers',
+				'nafr2',
+				'A'
+			);
+			expect(result).toEqual({
+				general: { prefLabelLg1: 'label', conceptVersion: 2 },
+				notes: 'nafr2 - A - notes v2',
+				narrowers: 'narrowers',
+			});
 		});
 	});
-	test('getConcept selector should extract results', () => {
-		const result = getItem(
-			{
-				classificationItemGeneral: {
-					nafr2: {
-						A: {
-							status: LOADED,
-							results: { prefLabelLg1: 'label', conceptVersion: 2 },
+	describe('getFullItem', () => {
+		test('getFullItem selector should extract results', () => {
+			const result = getFullItem(
+				{
+					classificationItemGeneral: {},
+					classificationItemNotes: {},
+					classificationItemMembers: {},
+				},
+				'nafr2',
+				'nafr2'
+			);
+			expect(result).toEqual();
+		});
+		test('getFullItem selector should extract results', () => {
+			const result = getFullItem(
+				{
+					classificationItemGeneral: {
+						nafr2: {
+							A: {
+								status: LOADED,
+								results: { prefLabelLg1: 'label', conceptVersion: 2 },
+							},
+						},
+					},
+					classificationItemNotes: {
+						nafr2: {
+							A: {
+								1: { status: LOADED, results: 'nafr2 - A - notes v1' },
+								2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+							},
+						},
+					},
+					classificationItemNarrowers: {
+						nafr2: {
+							A: { status: LOADED, results: 'narrowers' },
 						},
 					},
 				},
-				classificationItemNotes: {
-					nafr2: {
-						A: {
-							1: { status: LOADED, results: 'nafr2 - A - notes v1' },
-							2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+				'nafr2',
+				'A'
+			);
+			expect(result).toEqual({
+				general: { prefLabelLg1: 'label', conceptVersion: 2 },
+				notes: { 1: 'nafr2 - A - notes v1', 2: 'nafr2 - A - notes v2' },
+				narrowers: 'narrowers',
+			});
+		});
+		test('getItem selector should extract results', () => {
+			const result = getItem(
+				{
+					classificationItemGeneral: {
+						nafr2: {
+							A: {
+								status: LOADED,
+								results: { prefLabelLg1: 'label', conceptVersion: 2 },
+							},
+						},
+					},
+					classificationItemNotes: {
+						nafr2: {
+							A: {
+								1: { status: LOADED, results: 'nafr2 - A - notes v1' },
+								2: { status: LOADED, results: 'nafr2 - A - notes v2' },
+							},
+						},
+					},
+					classificationItemNarrowers: {
+						nafr2: {
+							A: { status: LOADED, results: 'narrowers' },
 						},
 					},
 				},
-				classificationItemNarrowers: {
-					nafr2: {
-						A: { status: LOADED, results: 'narrowers' },
-					},
-				},
-			},
-			'nafr2',
-			'A'
-		);
-		expect(result).toEqual({
-			general: { prefLabelLg1: 'label', conceptVersion: 2 },
-			notes: 'nafr2 - A - notes v2',
-			narrowers: 'narrowers',
+				'nafr2',
+				'A'
+			);
+			expect(result).toEqual({
+				general: { prefLabelLg1: 'label', conceptVersion: 2 },
+				notes: 'nafr2 - A - notes v2',
+				narrowers: 'narrowers',
+			});
 		});
 	});
 });
