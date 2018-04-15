86cb75a6ebfbe6ecf2f552e69b43a671515e3cde
Nicolas Laval <nicolas.laval@insee.fr>
2018-04-15 14:29:03 +0200

    Add tests to auth utils

---
+++ src/js/utils/auth/open-id-connect-auth/utils.spec.js
@@ -0,0 +1,63 @@
+import * as U from './utils';
+
+const emptyRole = [];
+const items = [{ creator: 'creator' }, { creator: 'creator2' }];
+
+describe('open-id-connect-auth-utils', () => {
+	describe('isAdmin', () => {
+		it('should return true', () => {
+			expect(U.isAdmin(emptyRole)).toBeTruthy();
+		});
+	});
+	describe('isContributor', () => {
+		it('should return true', () => {
+			expect(U.isContributor(emptyRole)).toBeTruthy();
+		});
+	});
+	describe('isConceptCreator', () => {
+		it('should return true', () => {
+			expect(U.isConceptCreator(emptyRole, 'stamp', 'stamp')).toBeTruthy();
+		});
+	});
+	describe('filterConceptsToValidate', () => {
+		it('should return items', () => {
+			expect(U.filterConceptsToValidate(items, emptyRole, 'creator')).toEqual(
+				items
+			);
+		});
+	});
+	describe('isCollectionCreator', () => {
+		it('should return true', () => {
+			expect(U.isCollectionCreator(emptyRole, 'stamp', 'stamp')).toBeTruthy();
+		});
+	});
+	describe('filterCollectionsToValidate', () => {
+		it('should return items', () => {
+			expect(
+				U.filterCollectionsToValidate(items, emptyRole, 'creator')
+			).toEqual(items);
+		});
+	});
+	describe('isAdminOrContributor', () => {
+		it('should return true', () => {
+			expect(U.isAdminOrContributor(emptyRole)).toBeTruthy();
+		});
+	});
+	describe('isAdminOrConceptCreator', () => {
+		it('should return true', () => {
+			expect(U.isAdminOrConceptCreator(emptyRole)).toBeTruthy();
+		});
+	});
+	describe('isAdminOrContributorOrConceptCreator', () => {
+		it('should return true', () => {
+			expect(
+				U.isAdminOrContributorOrConceptCreator(emptyRole, 'stamp', 'stamp')
+			).toBeTruthy();
+		});
+	});
+	describe('isAdminOrCollectionCreator', () => {
+		it('should return true', () => {
+			expect(U.isAdminOrCollectionCreator(emptyRole)).toBeTruthy();
+		});
+	});
+});
