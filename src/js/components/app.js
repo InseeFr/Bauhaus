120bcc40d80504ecb2c7b1259978f263f7ec4ab3
Nicolas Laval <nicolas.laval@insee.fr>
2018-05-16 18:08:32 +0200

    Add classifications link to home

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import conceptsLogo from 'img/concepts.jpg';
-import classificationsLogo from 'img/classifications-temp.png';
+import classificationsLogo from 'img/classifications.jpg';
 import operationsLogo from 'img/operations.jpg';
 import D from 'js/i18n';
 import 'css/app.css';
@@ -18,11 +18,11 @@ function App() {
 						{D.conceptsTitle}
 					</h2>
 				</Link>
-				{/*<Link to="/">*/}
-				<h2 className="items page-title-temp" disabled>
-					{D.classificationsTitle}
-				</h2>
-				{/*</Link>*/}
+				<Link to="/classifications">
+					<h2 className="items page-title page-title-link" disabled>
+						{D.classificationsTitle}
+					</h2>
+				</Link>
 				<Link to="/operations/series">
 					<h2 className="items page-title page-title-link">
 						{D.operationsTitle}
@@ -33,9 +33,13 @@ function App() {
 				<Link to="/concepts">
 					<img src={conceptsLogo} alt="Concepts" className="img" />
 				</Link>
-				{/*<Link to="/">*/}
-				<img src={classificationsLogo} alt="Classifications" className="img" />
-				{/*</Link>*/}
+				<Link to="/classifications">
+					<img
+						src={classificationsLogo}
+						alt="Classifications"
+						className="img"
+					/>
+				</Link>
 				<Link to="/operations/series">
 					<img src={operationsLogo} alt="Operations" className="img" />
 				</Link>
