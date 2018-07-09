786c7d1cf005bdcbc65fea411938a8e19a2abd22
Nicolas Laval <nicolas.laval@insee.fr>
2018-07-09 09:37:21 +0200

    Add classification item links to notes

--- src/js/components/shared/explanatory-note/index.js
+++ src/js/components/shared/explanatory-note/index.js
@@ -5,13 +5,17 @@ import NoteFlag from 'js/components/shared/note-flag';
 export const ExplanatoryNote = ({ text, title, lang, alone, context }) => {
 	const cl = alone ? 'col-md-12' : 'col-md-6';
 	if (!text) return <div className={cl} />;
+	const newText = text.replace(
+		/href="http:\/\/.+?\/codes\/(.+?)\/.+?\/(.+?)"/g,
+		`href="${window.location.origin}/classifications/classification/$1/item/$2"`
+	);
 	return (
 		<div className={cl}>
 			<PanelHtml
 				title={<NoteFlag text={title} lang={lang} />}
 				context={context}
 			>
-				{text}
+				{newText}
 			</PanelHtml>
 		</div>
 	);
