94cb7d952e00b9622a1faf530d3b6ccaa5b1744a
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-07-11 13:52:18 +0200

    feat: Add FLAG when loaded only one item

--- src/js/constants.js
+++ src/js/constants.js
@@ -8,6 +8,7 @@ export const PRODUCTEUR = 'PRODUCTEUR';
 
 export const LOADING = 'LOADING';
 export const LOADED = 'LOADED';
+export const PARTIAL_LOADED = 'PARTIAL_LOADED';
 export const ERROR = 'ERROR';
 
 export const NOT_LOADED = 'NOT_LOADED';
