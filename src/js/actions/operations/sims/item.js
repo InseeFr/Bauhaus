ae80a1dec28d1cc07efbedaaff673b947f7fd5e5
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-10-08 11:16:49 +0200

    feat: add SIMS action

---
+++ src/js/actions/operations/sims/item.js
@@ -0,0 +1,56 @@
+import api from 'js/remote-api/operations-api';
+import * as A from 'js/actions/constants';
+
+export const saveOperation = sims => dispatch => {
+	dispatch({
+		type: A.SAVE_OPERATIONS_SIMS,
+		payload: sims,
+	});
+	return api.putSims(sims).then(
+		results =>
+			dispatch({
+				type: A.SAVE_OPERATIONS_SIMS_SUCCESS,
+				payload: sims,
+			}),
+		err =>
+			dispatch({
+				type: A.SAVE_OPERATIONS_SIMS_FAILURE,
+				payload: { err },
+			})
+	);
+};
+
+export default id => dispatch => {
+	dispatch({
+		type: A.LOAD_OPERATIONS_SIMS,
+		payload: {
+			id,
+		},
+	});
+	return api.getSims(id).then(
+		results => {
+			dispatch({
+				type: A.LOAD_OPERATIONS_SIMS_SUCCESS,
+				payload: {
+					...results,
+					rubrics: results.rubrics.reduce((acc, rubric) => {
+						return {
+							...acc,
+							[rubric.idAttribute.toLowerCase()]: {
+								...rubric,
+								idMas: rubric.idAttribute.toLowerCase(),
+							},
+						};
+					}, {}),
+				},
+			});
+		},
+
+		err => {
+			dispatch({
+				type: A.LOAD_OPERATIONS_SIMS_LIST_FAILURE,
+				payload: { err },
+			});
+		}
+	);
+};
