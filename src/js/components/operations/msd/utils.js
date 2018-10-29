978db8a3e4b9c2f62fda5abdd4818a18ac053992
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-10-29 10:55:42 +0100

    fix: use constant for HELP_COLLAPSED

--- src/js/components/operations/msd/utils.js
+++ src/js/components/operations/msd/utils.js
@@ -1,13 +1,15 @@
+const HELP_COLLAPSED = 'HELP_COLLAPSED';
+
 export function isOpen(id) {
-	const collapsed = JSON.parse(localStorage.getItem('HELP_COLLAPSED')) || {};
+	const collapsed = JSON.parse(localStorage.getItem(HELP_COLLAPSED)) || {};
 	return collapsed[id] || false;
 }
 
 export function toggleOpen(id) {
-	const collapsed = JSON.parse(localStorage.getItem('HELP_COLLAPSED')) || {};
+	const collapsed = JSON.parse(localStorage.getItem(HELP_COLLAPSED)) || {};
 	const previous = collapsed[id] || false;
 	localStorage.setItem(
-		'HELP_COLLAPSED',
+		HELP_COLLAPSED,
 		JSON.stringify({
 			...collapsed,
 			[id]: !previous,
