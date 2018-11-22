4b8071127d897efa6175ab6269a2ae20e3535fe3
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-11-22 09:01:00 +0100

    fix: when we create a SIMS we have to reset the corresponding operation

--- src/js/reducers/operations/current.js
+++ src/js/reducers/operations/current.js
@@ -37,6 +37,8 @@ export const operationsOperationsCurrent = function(state = {}, action) {
 		case A.LOAD_OPERATIONS_OPERATION_SUCCESS:
 		case A.SAVE_OPERATIONS_OPERATION:
 			return action.payload;
+		case A.SAVE_OPERATIONS_SIMS_SUCCESS:
+			return {};
 		default:
 			return state;
 	}
