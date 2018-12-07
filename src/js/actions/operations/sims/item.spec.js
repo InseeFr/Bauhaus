f7c65e79791626503717e5bf13ea39b01cd90d76
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-12-07 14:48:09 +0100

    fix: solve unit tests

--- src/js/actions/operations/sims/item.spec.js
+++ src/js/actions/operations/sims/item.spec.js
@@ -1,19 +1,31 @@
 import get, { saveSims } from './item';
 import * as A from 'js/actions/constants';
 import api from 'js/remote-api/operations-api';
+import { LOADING, NOT_LOADED } from 'js/constants';
 
 const dispatch = jest.fn();
 jest.mock('js/remote-api/operations-api');
 
 describe('SIMS actions', () => {
 	beforeEach(() => dispatch.mockClear());
+	it('should return nothing if the status is LOADING', async () => {
+		const getState = () => {
+			return { operationsSimsCurrentStatus: LOADING };
+		};
+		await get(1)(dispatch, getState);
+
+		expect(dispatch).not.toHaveBeenCalledWith();
+	});
 	describe('get a sims', () => {
-		it('should call dispatch LOAD_OPERATIONS_SIMS_SUCCESS action with the right sims', async () => {
+		it('should call dispatch LOAD_OPERATIONS_SIMS_SUCCESS action with the right sims if the status is not LOADING', async () => {
 			api.getSims = function(id) {
 				return Promise.resolve({ label: 'bbb', id, rubrics: [] });
 			};
+			const getState = () => {
+				return { operationsSimsCurrentStatus: NOT_LOADED };
+			};
 			const id = 1;
-			await get(id)(dispatch);
+			await get(id)(dispatch, getState);
 			expect(dispatch).toHaveBeenCalledWith({
 				type: A.LOAD_OPERATIONS_SIMS,
 				payload: { id },
@@ -23,12 +35,15 @@ describe('SIMS actions', () => {
 				payload: { id, label: 'bbb', rubrics: {} },
 			});
 		});
-		it('should call dispatch LOAD_OPERATIONS_SIMS_LIST_FAILURE action with the error', async () => {
+		it('should call dispatch LOAD_OPERATIONS_SIMS_LIST_FAILURE action with the error if the status is not LOADING', async () => {
 			api.getSims = function(id) {
 				return Promise.reject('error');
 			};
+			const getState = () => {
+				return { operationsSimsCurrentStatus: NOT_LOADED };
+			};
 			const id = 1;
-			await get(id)(dispatch);
+			await get(id)(dispatch, getState);
 			expect(dispatch).toHaveBeenCalledWith({
 				type: A.LOAD_OPERATIONS_SIMS,
 				payload: { id },
