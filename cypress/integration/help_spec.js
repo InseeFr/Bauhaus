ad1ca8eb97514dd40c12cc8c494eb12b0b44637c
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-12-20 11:46:21 +0100

    feat: add cypress test for the help page

---
+++ cypress/integration/help_spec.js
@@ -0,0 +1,34 @@
+describe('Help Page', function() {
+	it(`Should display the Help page and manage the show/hide feature`, function() {
+		cy.server().visit(`/operations/help`);
+
+		cy.get(`.navbar-right li:nth-child(1)`).should('have.class', 'active');
+		cy.get('.sommaire-gauche').should('be.visible');
+		cy.get('.sommaire-droite').should('be.visible');
+
+		cy.get('button[title="open summary"]').click();
+		cy.get('.sommaire-gauche').should('not.be.visible');
+		cy.get('.sommaire-droite').should('be.visible');
+
+		cy.get('.ouverture-sommaire').click();
+		cy.get('.sommaire-gauche').should('be.visible');
+		cy.get('.sommaire-droite').should('be.visible');
+
+		cy.get('button[title="open content"]').click();
+		cy.get('.sommaire-gauche').should('be.visible');
+		cy.get('.sommaire-droite').should('not.be.visible');
+
+		cy.get('.ouverture-contenu').click();
+		cy.get('.sommaire-gauche').should('be.visible');
+		cy.get('.sommaire-droite').should('be.visible');
+	});
+
+	it(`Should manage the collapsible panel feature`, function() {
+		cy.server().visit(`/operations/help`);
+		cy.get('.sommaire li:nth-child(2) .sommaire-item').should('not.be.visible');
+		cy.get('.sommaire li:nth-child(2) .up-down').click();
+		cy.get('.sommaire li:nth-child(2) .sommaire-item').should('be.visible');
+		cy.get('.sommaire li:nth-child(2) .up-down').click();
+		cy.get('.sommaire li:nth-child(2) .sommaire-item').should('not.be.visible');
+	});
+});
