4b03a0f104f18f44bc292ab13c1e7ab486e4ae2a
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-12-21 14:40:47 +0100

    feat: add cypress test for the operation page

---
+++ cypress/integration/operation.spec.js
@@ -0,0 +1,94 @@
+describe('Operation Page', function() {
+	let polyfill;
+
+	before(() => {
+		const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+		cy.request(polyfillUrl).then(response => {
+			polyfill = response.body;
+		});
+	});
+
+	it(`Should go to the operation page and go back`, function() {
+		cy.server().visit(`/operations`);
+		cy.get('.list-group li:nth-child(1) a').click();
+		cy.url().should('include', '/operations/operation');
+		cy.get('.btn-line > div:nth-child(1) button').contains('Retour');
+		cy.get('.btn-line > div:nth-child(1) button').click();
+		cy.url().should('match', /\/operations$/);
+	});
+
+	it(`Should contain the content of an operation`, function() {
+		cy.server()
+			.fixture('operation-with-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.page-title-operations').contains(
+			'Enquête annuelle de production 2013'
+		);
+	});
+	it(`Should have a button to create a SIMS`, function() {
+		cy.server()
+			.fixture('operation-with-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.btn-line').contains('Voir le SIMS');
+	});
+	it(`Should have a button to update a SIMS`, function() {
+		cy.server()
+			.fixture('operation-without-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.btn-line').contains('Créer le SIMS');
+	});
+	it(`Should go to the update page and go back`, function() {
+		cy.server()
+			.fixture('operation-with-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.page-title-operations').contains(
+			'Enquête annuelle de production 2013'
+		);
+		cy.get('a[href="/operations/operation/1/modify"]').click();
+		cy.url().should('match', /\/operations\/operation\/1\/modify$/);
+		cy.get('.btn-line button')
+			.first()
+			.click();
+		cy.url().should('match', /\/operations\/operation\/1$/);
+	});
+});
