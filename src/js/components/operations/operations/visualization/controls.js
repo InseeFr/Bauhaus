f2329dd418a5843014e8ba299c3c6dc743486d1e
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-12-21 15:28:06 +0100

    fix: solve ESLint issue

--- src/js/components/operations/operations/visualization/controls.js
+++ src/js/components/operations/operations/visualization/controls.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Button from 'js/components/shared/button';
 import PlaceHolder from 'js/components/shared/placeholder';
 import { goBack } from 'js/utils/redirection';
 import D from 'js/i18n';
 
-function OperationsControls({ openModal }) {
+function OperationsControls(props) {
+	const { openModal } = props;
 	const cancel = [goBack(props, `/operations/series`), D.btnReturn];
 
 	const exportVarBook = [openModal, D.btnExport];
