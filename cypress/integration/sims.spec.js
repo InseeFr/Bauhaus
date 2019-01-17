2496c14814a41f3dc521fb0093addc161bb7e99f
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-01-17 10:48:40 +0100

    test(sims): Add e2e test for Visualisation page of a SIMS

---
+++ cypress/integration/sims.spec.js
@@ -0,0 +1,64 @@
+describe('SIMS Page', function() {
+	let polyfill;
+
+	before(() => {
+		const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+		cy.request(polyfillUrl).then(response => {
+			polyfill = response.body;
+		});
+	});
+
+	it(`Operation with SIMS`, function() {
+		cy.server()
+			.fixture('operation-with-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+			.fixture('sims')
+			.then(json => {
+				cy.route(
+					'http://localhost:8080/api/operations/metadataReport/1512',
+					json
+				);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.btn-line').contains('Voir le SIMS');
+
+		// Quand on click sur le bouton Voir le SIMS on doit est rediriger vers la page SIMS edit et avoir un bouton duplicate
+		cy.get(':nth-child(3) > .btn').click();
+		cy.url().should('contains', '/sims/1512');
+		cy.get('.btn-line').within(btns => {
+			cy.get('button').should('have.length', 1);
+			cy.get('a').should('have.length', 1);
+		});
+
+		// Quand on click sur le bouton Voir le SIMS on doit est rediriger vers la page SIMS create et ne pas avoir de  bouton duplicate
+
+		// Quand on click sur le bouton Edit on est redireiger vers upadte et ne contiens pas de bouton duplicate
+	});
+	it(`Operation without a SIMS`, function() {
+		cy.server()
+			.fixture('operation-without-sims')
+			.then(json => {
+				cy.route('http://localhost:8080/api/operations/operation/1', json);
+			})
+
+			.visit('/operations/operation/1', {
+				onBeforeLoad(win) {
+					delete win.fetch;
+					win.eval(polyfill);
+					win.fetch = win.unfetch;
+				},
+			});
+		cy.get('.btn-line').contains('Créer le SIMS');
+
+		// Quand on click sur le bouton Voir le SIMS on doit est rediriger vers la page SIMS create et ne pas avoir de  bouton duplicate
+	});
+});
