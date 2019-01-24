c811ec4bceabc8332f17f0e1a1eadea347140066
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-01-24 10:23:09 +0100

    refactor(note-flag): Add PropTypes

--- src/js/components/shared/note-flag.js
+++ src/js/components/shared/note-flag.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import flag from 'js/components/shared/flag';
+import PropTypes from 'prop-types';
 
-export default ({ text, lang }) => (
-	<div>
-		{text + '  ( '}
-		{flag(lang)} )
-	</div>
-);
+function NoteFlag({ text, lang }) {
+	return (
+		<React.Fragment>
+			{text + '  ( '}
+			{flag(lang)} )
+		</React.Fragment>
+	);
+}
+
+NoteFlag.propTypes = {
+	text: PropTypes.string,
+	lang: PropTypes.string,
+};
+
+export default NoteFlag;
