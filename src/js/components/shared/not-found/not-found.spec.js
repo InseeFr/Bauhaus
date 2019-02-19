d2cace4502cfdef854c3d15f3283d24f2ea87ec1
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-02-19 10:02:42 +0100

    refactor(Move component into a specific folder):

--- src/js/components/shared/not-found/not-found.spec.js
+++ src/js/components/shared/not-found/not-found.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NotFound from './not-found';
+import NotFound from '.';
 
 describe('not-found', () => {
 	it('renders without crashing', () => {
