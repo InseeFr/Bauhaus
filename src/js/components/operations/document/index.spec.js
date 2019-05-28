99fcdf94c3070bf9d806e1d1eaeb96717f574932
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-05-28 16:03:25 +0200

    fix: solve unit test for documents

--- src/js/components/operations/document/index.spec.js
+++ src/js/components/operations/document/index.spec.js
@@ -11,18 +11,33 @@ describe('DocumentHomeContainer', () => {
 			documentStoresStatus: NOT_LOADED,
 		});
 	});
-	// it('should return documents if available', () => {
-	// 	const result = mapStateToProps({
-	// 		operationsDocumentsList: {
-	// 			results: ['results'],
-	// 			status: LOADED,
-	// 			err: 'err',
-	// 		},
-	// 	});
-	// 	expect(result).toEqual({
-	// 		documents: ['results'],
-	// 		status: LOADED,
-	// 		err: 'err',
-	// 	});
-	// });
+	it('should return documents if available', () => {
+		const result = mapStateToProps({
+			operationsDocuments: {
+				results: {
+					full: [
+						{ uri: 'uri/1', labelLg1: 'labelLg1' },
+						{ uri: 'uri/2', labelLg2: 'labelLg2' },
+					],
+				},
+				status: LOADED,
+				err: 'err',
+			},
+		});
+		expect(result).toEqual({
+			documentStores: [
+				{
+					id: '1',
+					label: 'labelLg1',
+					uri: 'uri/1',
+				},
+				{
+					id: '2',
+					label: 'labelLg2',
+					uri: 'uri/2',
+				},
+			],
+			documentStoresStatus: LOADED,
+		});
+	});
 });
