d84492d2d8e772e76ab28602bac000b974388d5d
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-07-10 16:21:53 +0200

    fix: add unit test and documentation for the redirection method

--- src/js/utils/redirection.js
+++ src/js/utils/redirection.js
@@ -3,6 +3,16 @@ export const goBack = (props, defaultRedirection) => () =>
 		? props.history.push(defaultRedirection)
 		: props.history.goBack();
 
+/**
+ * This method is used when we have to redirect the user after creating or
+ * updating an object.
+ * If this is a creation, we should replace the current history with the page of the newly created object.
+ * If this is an update, we do a goBack
+ *
+ * @param {{history: any}} props
+ * @param {string} redirectUrl
+ * @param {boolean} shouldReplace
+ */
 export const goBackOrReplace = (props, redirectUrl, shouldReplace = false) => {
 	if (shouldReplace) {
 		props.history.replace(redirectUrl);
--- src/js/utils/redirection.spec.js
+++ src/js/utils/redirection.spec.js
@@ -1,11 +1,21 @@
-import { goBack } from './redirection';
+import { goBack, goBackOrReplace } from './redirection';
 
-const props = { history: { length: 1 } };
+const props = { history: { length: 1, push: jest.fn(), replace: jest.fn() } };
 
 describe('redirection', () => {
 	describe('goBack', () => {
 		it('should return a function to push at default', () => {
 			expect(goBack(props, 'default')).toEqual(expect.any(Function));
+		});
+	});
+	describe('goBackOrReplace', () => {
+		it('should call goBack if it is an update', () => {
+			goBackOrReplace(props, 'default', false);
+			expect(props.history.push).toHaveBeenCalled();
+		});
+		it('should call replace if it is an creation', () => {
+			goBackOrReplace(props, 'default', true);
+			expect(props.history.replace).toHaveBeenCalled();
 		});
 	});
 });
