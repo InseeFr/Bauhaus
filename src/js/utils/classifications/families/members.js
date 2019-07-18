70e2eabe0434310c9ff9f5cb860f8c2dd2c5d962
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-07-18 15:35:49 +0200

    fix: remove useless file

--- src/js/utils/classifications/families/members.js
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types';
-export const propTypes = PropTypes.arrayOf(
-	PropTypes.shape({
-		prefLabelLg1: PropTypes.string.isRequired,
-		prefLabelLg2: PropTypes.string,
-	})
-);
