c3deb524424b3ef2677c339d28e5723f38fc311d
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-08-19 10:48:31 +0200

    fix: remove useless component

--- app/src/js/components/menu/home.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import MenuHome from './home';
-
-describe('menu-home', () => {
-	it('renders without crashing', () => {
-		shallow(<MenuHome permission={{ authType: '', roles: [''] }} />);
-	});
-});
