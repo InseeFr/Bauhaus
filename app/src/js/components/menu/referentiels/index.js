1555e1d5cf3e505124a331bea2c9ceea5f6453db
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-08-21 15:46:23 +0200

    feat: add unit test for the referentiels menu component

--- app/src/js/components/menu/referentiels/index.js
+++ app/src/js/components/menu/referentiels/index.js
@@ -6,39 +6,40 @@ import './referentiels.scss';
 
 function MenuReferentiels() {
 	return (
-		<div>
-			<header>
-				<nav className="navbar navbar-default navbar-default-ref">
-					<div className="container-fluid">
-						<div className="collapse navbar-collapse">
-							<ul className="nav navbar-nav navbar-nav-ref">
-								<li>
-									<Link to="/">
-										<img src={homeLogo} alt="Accueil" className="img-menu" />{' '}
-										{D.home}
-									</Link>
-								</li>
-								{process.env.REACT_APP_APPLICATIONS.split(',').map(appName => {
-									const app = appName.trim();
-									return (
-										<li>
-											<Link to={'/' + app}>
-												<img
-													src={require(`img/${app}.jpg`)}
-													alt={app}
-													className="img-menu"
-												/>{' '}
-												{D[app + 'Title']}
-											</Link>
-										</li>
-									);
-								})}
-							</ul>
-						</div>
+		<header>
+			<nav className="navbar navbar-default navbar-default-ref">
+				<div className="container-fluid">
+					<div className="collapse navbar-collapse">
+						<ul className="nav navbar-nav navbar-nav-ref">
+							<li>
+								<Link to="/">
+									<img src={homeLogo} alt="Accueil" className="img-menu" />{' '}
+									{D.home}
+								</Link>
+							</li>
+							{process.env.REACT_APP_APPLICATIONS.split(',').map(appName => {
+								if (!appName) {
+									return;
+								}
+								const app = appName.trim();
+								return (
+									<li key={appName}>
+										<Link to={'/' + app}>
+											<img
+												src={require(`img/${app}.jpg`)}
+												alt={app}
+												className="img-menu"
+											/>{' '}
+											{D[app + 'Title']}
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
 					</div>
-				</nav>
-			</header>
-		</div>
+				</div>
+			</nav>
+		</header>
 	);
 }
 
--- app/src/js/components/operations/menu/index.js
+++ app/src/js/components/operations/menu/index.js
@@ -135,7 +135,7 @@ class MenuOperations extends Component {
 		if (activePath === '/') return null;
 
 		return (
-			<React.Fragment>
+			<>
 				<header>
 					<nav className="navbar navbar-default-operations">
 						<div className="container-fluid">
@@ -226,7 +226,7 @@ class MenuOperations extends Component {
 					</nav>
 				</header>
 				{menuRef && <MenuReferentiels />}
-			</React.Fragment>
+			</>
 		);
 	}
 }
