77faddc8c193b60510bb0947027a8b078300d955
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-08-28 11:42:26 +0200

    fix: uncomment script

--- scripts/gitbook.sh
+++ scripts/gitbook.sh
@@ -11,15 +11,15 @@ UPSTREAM="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 MESSAGE="Rebuild doc for revision $TRAVIS_COMMIT: $TRAVIS_COMMIT_MESSAGE"
 AUTHOR="$USER <>"
 
-#if [ "$TRAVIS_PULL_REQUEST" != "false" ];then
-#  echo "Documentation won't build on pull request"
-#  exit 0
-#fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];then
+  echo "Documentation won't build on pull request"
+  exit 0
+fi
 
-#if [ "$TRAVIS_BRANCH" != "$MAIN_BRANCH" ];then
-#  echo "Documentation won't build: Not on branch $MAIN_BRANCH"
-#  exit 0
-#fi
+if [ "$TRAVIS_BRANCH" != "$MAIN_BRANCH" ];then
+  echo "Documentation won't build: Not on branch $MAIN_BRANCH"
+  exit 0
+fi
 
 function setup() {
   npm install -g yarn gitbook-cli
@@ -52,14 +52,14 @@ function publish() {
   cp -R "../app/$STORYBOOK_FOLDER/storybook/." storybook/app
   cp -R "../packages/bauhaus-library/$STORYBOOK_FOLDER/storybook/." storybook/lib
 
-  #git init
-  #git remote add upstream "$UPSTREAM"
-  #git fetch --prune upstream
-  #git reset upstream/gh-pages
-  #git add --all .
-  #if git commit --message "$MESSAGE" --author "$AUTHOR" ; then
-  #  git push --quiet upstream HEAD:gh-pages
-  #fi
+  git init
+  git remote add upstream "$UPSTREAM"
+  git fetch --prune upstream
+  git reset upstream/gh-pages
+  git add --all .
+  if git commit --message "$MESSAGE" --author "$AUTHOR" ; then
+    git push --quiet upstream HEAD:gh-pages
+  fi
   popd
 }
 
