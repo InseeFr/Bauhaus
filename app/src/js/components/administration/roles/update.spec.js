8e91490ccbcf8f53111a208272f7b9a2628be7eb
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-09-09 15:16:05 +0200

    feat: add unit test for roles

--- app/src/js/components/administration/roles/update.spec.js
+++ app/src/js/components/administration/roles/update.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Update from './update';
+import Button from 'js/components/shared/button';
+import SelectRmes from 'js/components/shared/select-rmes';
 
 const roles = [{ id: 'id', label: 'label', persons: [] }];
 
@@ -13,5 +15,49 @@ describe('administration-update-roles', () => {
 				handleSave={() => console.log('save')}
 			/>
 		);
+	});
+
+	it('should set options for the select component', () => {
+		const container = shallow(
+			<Update
+				roles={roles}
+				agents={[]}
+				handleSave={() => console.log('save')}
+			/>
+		);
+
+		expect(container.find(SelectRmes).props().options).toEqual([
+			{ value: 'id', label: 'label' },
+		]);
+	});
+
+	it('should set the disabled property to true if there is nothing to add / delete', () => {
+		const container = shallow(
+			<Update
+				roles={roles}
+				agents={[]}
+				handleSave={() => console.log('save')}
+			/>
+		);
+
+		expect(container.find(Button).get(1).props.disabled).toBeTruthy();
+	});
+
+	it('should set the disabled property to false ig there is something to add and/or delete', () => {
+		const container = shallow(
+			<Update
+				roles={roles}
+				agents={[]}
+				handleSave={() => console.log('save')}
+			/>
+		);
+
+		container.setState({
+			toAdd: {
+				'1': '1',
+			},
+		});
+
+		expect(container.find(Button).get(1).props.disabled).toBeFalsy();
 	});
 });
