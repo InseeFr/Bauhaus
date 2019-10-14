697b40468b9c3ea5bc775bc25d7b67d61cb65251
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-10-14 11:43:49 +0200

    fix: remove the deprecated babel/polyfill module

--- app/src/js/components/router/index.js
+++ app/src/js/components/router/index.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
-import 'babel-polyfill';
+
+import 'react-app-polyfill/stable';
 
 class Root extends Component {
 	render() {
 		const { error } = this.props;
-		const footer = `${process.env.REACT_APP_NAME} - ${
-			process.env.REACT_APP_VERSION
-		}`;
+		const footer = `${process.env.REACT_APP_NAME} - ${process.env.REACT_APP_VERSION}`;
 		return (
 			<div>
 				<Router>
