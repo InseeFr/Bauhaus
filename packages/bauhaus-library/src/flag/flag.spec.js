0a6d02775c524e02dd11f3a6cf4e911fb2557591
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-10-31 10:10:22 +0100

    feat: add unit test for the flag component

--- packages/bauhaus-library/src/flag/flag.spec.js
+++ packages/bauhaus-library/src/flag/flag.spec.js
@@ -3,7 +3,13 @@ import { shallow } from 'enzyme';
 import Flag from './';
 
 describe('flag', () => {
-	it('renders without crashing', () => {
-		shallow(<Flag flag={null} />);
+	it('should return null if the flag is not defined', () => {
+		const container = shallow(<Flag flag={null} />);
+		expect(container.html()).toBe(null);
+	});
+	it('should return an image if the flag is defined', () => {
+		const container = shallow(<Flag flag="fr" />);
+		expect(container.find('img').props().src).toBe('fr');
+		expect(container.find('img').props().alt).toBe('flag');
 	});
 });
