6459a9d9405717905a749b22ca6d529dbb61363a
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-11-13 12:36:18 +0100

    fix: add unit test for the select component

--- packages/bauhaus-library/src/select/select.spec.js
+++ packages/bauhaus-library/src/select/select.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Select from './select';
+import { HelpBlock } from 'react-bootstrap';
 
 const options = Array.apply(null, Array(5)).map((a, i) => ({
 	value: `${i + 1}`,
@@ -17,5 +18,28 @@ describe('select', () => {
 				onChange={() => ''}
 			/>
 		);
+	});
+	it('should display a HelpBlock if the helpMsg is defined', () => {
+		const container = shallow(
+			<Select
+				value="value"
+				placeholder="..."
+				options={options}
+				onChange={() => ''}
+				helpMsg="helpMsg"
+			/>
+		);
+		expect(container.find(HelpBlock).length).toBe(1);
+	});
+	it('should not display a HelpBlock if the helpMsg is undefined', () => {
+		const container = shallow(
+			<Select
+				value="value"
+				placeholder="..."
+				options={options}
+				onChange={() => ''}
+			/>
+		);
+		expect(container.find(HelpBlock).length).toBe(0);
 	});
 });
