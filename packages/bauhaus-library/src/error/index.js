a11e85ce1d628387e7eb60098536cd0c21f8d428
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-12-03 16:24:48 +0100

    fix: use context for i18n

--- packages/bauhaus-library/src/error/index.js
+++ packages/bauhaus-library/src/error/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PageTitle from '../page-title';
-import D from 'js/i18n';
+import { I18NContext } from '../context';
 
-export default ({ home }) => (
-	<div>
-		{home && <h1 className="centered page-title">{D.welcome}</h1>}
-		<div className="container centered">
-			<PageTitle title={D.errorTitle} col={6} offset={3} />
-			<p className="col-md-8 col-md-offset-2">{D.errorBody}</p>
+export default ({ home }) => {
+	const D = useContext(I18NContext);
+
+	return (
+		<div>
+			{home && <h1 className="centered page-title">{D.welcome}</h1>}
+			<div className="container centered">
+				<PageTitle title={D.errorTitle} col={6} offset={3} />
+				<p className="col-md-8 col-md-offset-2">{D.errorBody}</p>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
