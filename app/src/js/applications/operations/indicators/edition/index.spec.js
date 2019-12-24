b4805749ee49eb2261ee82eb007fa0467adea599
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-12-24 16:24:52 +0100

    fix: solve unit test

--- app/src/js/applications/operations/indicators/edition/index.spec.js
+++ app/src/js/applications/operations/indicators/edition/index.spec.js
@@ -5,6 +5,7 @@ describe('OperationsIndicatorsEditionContainer', () => {
 	it('should return an empty indicator if the id is not the same', () => {
 		const result = mapStateToProps(
 			{
+				stampList: { results: ['STAMP1'] },
 				app: { lg1: 'lg1', lg2: 'lg2', secondLang: true },
 				operationsIndicatorsCurrent: {
 					id: '2',
@@ -29,6 +30,7 @@ describe('OperationsIndicatorsEditionContainer', () => {
 			operationsAsyncTask: false,
 			organisations: ['results'],
 			series: ['operationsSeriesList'],
+			stamps: ['STAMP1'],
 		});
 	});
 });
