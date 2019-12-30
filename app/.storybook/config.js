bf9acc0153ad8b0f37dd75ce0ee4b8fc6ade10a0
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-12-30 12:48:14 +0100

    fix: solve app storybook

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-import 'app.scss';
 
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 
