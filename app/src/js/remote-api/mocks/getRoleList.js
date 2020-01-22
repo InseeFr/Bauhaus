8164e1460bbe2eebc57a60a2cd116d383909978f
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-01-22 11:25:43 +0100

    feat: mock agent and roles

---
+++ app/src/js/remote-api/mocks/getRoleList.js
@@ -0,0 +1,14 @@
+export default () => {
+	return Promise.resolve([
+		{
+			persons: [
+				{
+					id: '5e2819217a606efe20f8aa44',
+					label: 'Deleon Atkins',
+				},
+			],
+			id: '1',
+			label: 'admin',
+		},
+	]);
+};
