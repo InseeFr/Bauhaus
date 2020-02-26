926ece6e92177936fc7a59bda596de808719f0bf
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-02-26 17:04:39 +0100

    feat: add data to DSD mock

--- app/src/js/remote-api/mocks/dsds/getDSD.js
+++ app/src/js/remote-api/mocks/dsds/getDSD.js
@@ -1,3 +1,9 @@
 export default () => {
-	return Promise.resolve({ labelLg1: 'werwer', id: 'werwer' });
+	return Promise.resolve({
+		labelLg1: 'labelLg1',
+		labelLg2: 'labelLg2',
+		descriptionLg1: 'descriptionLg1',
+		descriptionLg2: 'descriptionLg2',
+		id: 'werwer',
+	});
 };
