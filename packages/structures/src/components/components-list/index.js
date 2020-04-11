b46b686a4f62c5b09fdb9c87803139654c7efbc0
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-04-11 14:38:02 +0200

    feat: create component-list component

---
+++ packages/structures/src/components/components-list/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+	NewButton,
+	SearchableList,
+	PageTitle,
+	VerticalMenu,
+} from '@inseefr/wilco';
+import './component-list.scss';
+
+function ComponentsList({ items, title, childPath }) {
+	return (
+		<div className="container structures-components-list">
+			<div className="row">
+				<VerticalMenu>
+					<NewButton action="/dsds/components/search" wrapper={false} />
+				</VerticalMenu>
+				<div className="col-md-8 centered pull-right">
+					<PageTitle title={title} col={12} offset={0} />
+					<SearchableList
+						items={items}
+						childPath={childPath}
+						label="label"
+						autoFocus={true}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ComponentsList;
