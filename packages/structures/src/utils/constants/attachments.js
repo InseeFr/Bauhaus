de7e530d761b639796b2529090941d3bf2f2438a
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-04-11 14:38:21 +0200

    fix: Observation should come first

--- packages/structures/src/utils/constants/attachments.js
+++ packages/structures/src/utils/constants/attachments.js
@@ -5,7 +5,7 @@ export const OBSERVATION = `${QB}Observation`;
 export const SLICE = `${QB}Slice`;
 
 export const ATTACHMENTS = [
-  { value: DATA_SET, label: 'DataSet' },
-  { value: OBSERVATION, label: 'Observation' },
-  { value: SLICE, label: 'Slice' },
+	{ value: OBSERVATION, label: 'Observation' },
+	{ value: DATA_SET, label: 'DataSet' },
+	{ value: SLICE, label: 'Slice' },
 ];
