f41586a8f2d7b5f73ed889a05b4404cfc62eb6e5
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-04-21 06:04:28 +0200

    feat: migrate utilities to rollup

--- packages/utilities/rollup.config.js
+++ packages/utilities/rollup.config.js
@@ -10,7 +10,7 @@ const { dependencies } = require('./package.json');
 export default {
 	input: 'src/index.js',
 	output: {
-		name: 'bauhaus-structures',
+		name: 'bauhaus-utilities',
 		file: 'dist/index.js',
 		format: 'cjs',
 		strict: false,
