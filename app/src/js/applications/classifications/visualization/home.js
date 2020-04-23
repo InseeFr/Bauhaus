67da9ea2844f3ef8d28fe836ecdd303d76d61ba8
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-04-23 17:19:23 +0200

    fix: convert a component class to a function

--- app/src/js/applications/classifications/visualization/home.js
+++ app/src/js/applications/classifications/visualization/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { PageSubtitle, PageTitle } from '@inseefr/wilco';
 import Controls from './controls';
@@ -8,56 +8,52 @@ import Levels from './levels';
 import D from 'js/i18n';
 import { CheckSecondLang } from 'bauhaus-utilities';
 
-class ClassificationVisualization extends Component {
-	render() {
-		const {
-			classification: { general, levels },
-			classificationId,
-			secondLang,
-			langs,
-		} = this.props;
-		const notes = {
-			scopeNoteLg1: general.scopeNoteLg1,
-			scopeNoteLg2: general.scopeNoteLg2,
-			changeNoteLg1: general.changeNoteLg1,
-			changeNoteLg2: general.changeNoteLg2,
-			descriptionLg1: general.descriptionLg1,
-			descriptionLg2: general.descriptionLg2,
-		};
-		return (
-			<div className="container">
-				<PageTitle title={general.prefLabelLg1} />
-				{general.prefLabelLg2 && (
-					<PageSubtitle subTitle={general.prefLabelLg2} />
-				)}
-				<div className="row">
-					<div className="col-md-12 text-center">
-						<Link
-							to={`/classifications/classification/${classificationId}/items`}
-						>
-							<h3>
-								<span className="glyphicon glyphicon-zoom-in mr-1"></span>
-								{D.classificationAllItemsTitle}
-							</h3>
-						</Link>
-					</div>
+const ClassificationVisualization = props => {
+	const {
+		classification: { general, levels },
+		classificationId,
+		secondLang,
+		langs,
+	} = props;
+	const notes = {
+		scopeNoteLg1: general.scopeNoteLg1,
+		scopeNoteLg2: general.scopeNoteLg2,
+		changeNoteLg1: general.changeNoteLg1,
+		changeNoteLg2: general.changeNoteLg2,
+		descriptionLg1: general.descriptionLg1,
+		descriptionLg2: general.descriptionLg2,
+	};
+	return (
+		<div className="container">
+			<PageTitle title={general.prefLabelLg1} />
+			{general.prefLabelLg2 && <PageSubtitle subTitle={general.prefLabelLg2} />}
+			<div className="row">
+				<div className="col-md-12 text-center">
+					<Link
+						to={`/classifications/classification/${classificationId}/items`}
+					>
+						<h3>
+							<span className="glyphicon glyphicon-zoom-in mr-1"></span>
+							{D.classificationAllItemsTitle}
+						</h3>
+					</Link>
 				</div>
-				<Controls />
-				<CheckSecondLang />
-				<General general={general} secondLang={secondLang} langs={langs} />
-				{notes.scopeNoteLg1 && (
-					<Notes notes={notes} secondLang={secondLang} langs={langs} />
-				)}
-				{levels.length !== 0 && (
-					<Levels
-						levels={levels}
-						classificationId={general.id}
-						secondLang={secondLang}
-					/>
-				)}
 			</div>
-		);
-	}
-}
+			<Controls />
+			<CheckSecondLang />
+			<General general={general} secondLang={secondLang} langs={langs} />
+			{notes.scopeNoteLg1 && (
+				<Notes notes={notes} secondLang={secondLang} langs={langs} />
+			)}
+			{levels.length !== 0 && (
+				<Levels
+					levels={levels}
+					classificationId={general.id}
+					secondLang={secondLang}
+				/>
+			)}
+		</div>
+	);
+};
 
 export default ClassificationVisualization;
