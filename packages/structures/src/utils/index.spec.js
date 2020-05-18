873c22ee62a763011ece57ebb312bd94e924dad6
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-05-18 14:21:09 +0200

    feat: add test for getAllAttchment

--- packages/structures/src/utils/index.spec.js
+++ packages/structures/src/utils/index.spec.js
@@ -1,5 +1,52 @@
-import { validateComponent } from '.';
+import { validateComponent, getAllAttachment } from '.';
 
+describe('getAllAttachment', () => {
+	it('should return all default attachments', () => {
+		const attachments = getAllAttachment([]);
+		expect(attachments).toEqual([
+			{
+				label: 'Observation',
+				value: 'http://purl.org/linked-data/cube#Observation',
+			},
+			{
+				label: 'DataSet',
+				value: 'http://purl.org/linked-data/cube#DataSet',
+			},
+			{
+				label: 'Slice',
+				value: 'http://purl.org/linked-data/cube#Slice',
+			},
+		]);
+	});
+
+	it('should return all default attachments with all measures', () => {
+		const attachments = getAllAttachment([
+			{
+				type: 'http://purl.org/linked-data/cube#measure',
+				id: 1,
+				labelLg1: 'labelLg1',
+			},
+		]);
+		expect(attachments).toEqual([
+			{
+				label: 'Observation',
+				value: 'http://purl.org/linked-data/cube#Observation',
+			},
+			{
+				label: 'DataSet',
+				value: 'http://purl.org/linked-data/cube#DataSet',
+			},
+			{
+				label: 'Slice',
+				value: 'http://purl.org/linked-data/cube#Slice',
+			},
+			{
+				label: 'labelLg1',
+				value: 1,
+			},
+		]);
+	});
+});
 describe('validateComponent', () => {
 	it('should return an error if the identifiant is not defined ', () => {
 		expect(validateComponent({})).toEqual({
