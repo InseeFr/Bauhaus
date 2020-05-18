4621cc2ab04b1add936cd76b72042523ae83aec8
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-05-18 13:56:39 +0200

    feat: add unit test for ComponentSpecificationModal

---
+++ packages/structures/src/components/component-specification-modal/component-specification-modal.spec.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ComponentSpecificationModalBody } from '.';
+import { render, fireEvent } from '@testing-library/react';
+
+describe('<ComponentSpecificationModal />', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+	const specification = {
+		required: true,
+		attachment: ['http://purl.org/linked-data/cube#DataSet'],
+	};
+	const structureComponents = [];
+
+	it('should call the onClose prop', () => {
+		const onClose = jest.fn();
+		const { container } = render(
+			<ComponentSpecificationModalBody
+				specification={specification}
+				structureComponents={structureComponents}
+				onClose={onClose}
+			/>
+		);
+		fireEvent.click(container.querySelector('.modal-header button'));
+		expect(onClose).toHaveBeenCalled();
+	});
+	it('should call the onSave prop', () => {
+		const onSave = jest.fn();
+		const { container } = render(
+			<ComponentSpecificationModalBody
+				specification={specification}
+				structureComponents={structureComponents}
+				onSave={onSave}
+			/>
+		);
+		fireEvent.click(container.querySelector('.modal-footer button'));
+		expect(onSave).toHaveBeenCalled();
+	});
+	it('should display the component specification', () => {
+		const { getAllByText } = render(
+			<ComponentSpecificationModalBody
+				specification={specification}
+				structureComponents={structureComponents}
+			/>
+		);
+		expect(getAllByText('DataSet')).toHaveLength(1);
+	});
+});
--- packages/structures/src/components/component-specification-modal/index.js
+++ packages/structures/src/components/component-specification-modal/index.js
@@ -5,7 +5,7 @@ import { SaveButton } from '@inseefr/wilco';
 import { ComponentSpecificationForm } from '../component-specification-form';
 import './component-specification-modal.scss';
 
-export default ({
+export const ComponentSpecificationModalBody = ({
 	specification: defaultSpecification,
 	structureComponents,
 	onClose,
@@ -16,37 +16,39 @@ export default ({
 	);
 
 	return (
+		<div className="modal-content">
+			<div className="modal-header">
+				<button type="button" className="close" onClick={onClose}>
+					<span aria-hidden="true">&times;</span>
+					<span className="sr-only">{'btnClose'}</span>
+				</button>
+				<h4 className="modal-title">{D.componentSpecificationTitle}</h4>
+			</div>
+
+			<div className="modal-body">
+				<ComponentSpecificationForm
+					onChange={setSpecification}
+					component={specification}
+					structureComponents={structureComponents}
+				/>
+			</div>
+			<div className="modal-footer">
+				<div className="text-right">
+					<SaveButton col={4} offset={8} action={() => onSave(specification)} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default props => {
+	return (
 		<Modal
 			className="Modal__Bootstrap modal-dialog structures structures-specification-modal"
 			isOpen={true}
 			ariaHideApp={false}
 		>
-			<div className="modal-content">
-				<div className="modal-header">
-					<button type="button" className="close" onClick={onClose}>
-						<span aria-hidden="true">&times;</span>
-						<span className="sr-only">{'btnClose'}</span>
-					</button>
-					<h4 className="modal-title">{D.componentSpecificationTitle}</h4>
-				</div>
-
-				<div className="modal-body">
-					<ComponentSpecificationForm
-						onChange={setSpecification}
-						component={specification}
-						structureComponents={structureComponents}
-					/>
-				</div>
-				<div className="modal-footer">
-					<div className="text-right">
-						<SaveButton
-							col={4}
-							offset={8}
-							action={() => onSave(specification)}
-						/>
-					</div>
-				</div>
-			</div>
+			<ComponentSpecificationModalBody {...props} />
 		</Modal>
 	);
 };
