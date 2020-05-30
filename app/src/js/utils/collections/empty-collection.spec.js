af5e01c5164cba746370951631b26f376b573a36
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-05-30 21:07:17 +0200

    feat: add unit test for empty_collection.js

---
+++ app/src/js/utils/collections/empty-collection.spec.js
@@ -0,0 +1,20 @@
+import defaultContributor from './empty-collection';
+describe('defaultContributor', () => {
+	it('should return the defaultContributor', () => {
+		expect(defaultContributor()).toEqual({
+			general: {
+				contributor: undefined,
+				created: '',
+				creator: '',
+				descriptionLg1: '',
+				descriptionLg2: '',
+				id: '',
+				isValidated: '',
+				modified: '',
+				prefLabelLg1: '',
+				prefLabelLg2: '',
+			},
+			members: [],
+		});
+	});
+});
