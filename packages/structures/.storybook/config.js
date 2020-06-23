46c7f01cc7bcdf30d8612024fbc35ddb578db2fb
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-06-23 09:15:43 +0200

    fix: disable storybook

--- packages/structures/.storybook/config.js
+++ packages/structures/.storybook/config.js
@@ -1,11 +1,12 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure } from '@storybook/react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@inseefr/wilco/dist/index.css';
 
 import 'bauhaus-utilities/dist/index.css';
 import './styles.css';
 
-const requireAll = requireContext => requireContext.keys().map(requireContext);
+const requireAll = (requireContext) =>
+	requireContext.keys().map(requireContext);
 
 const loadStories = () =>
 	requireAll(require.context('../src', true, /stories\.jsx?$/));
