3fc941a39418a47b7d7b7162cc6882b4e6bbded5
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-08-21 08:47:22 +0200

    fix: remove warning on unit test

--- app/src/js/applications/concepts/compare/home.spec.js
+++ app/src/js/applications/concepts/compare/home.spec.js
@@ -6,7 +6,11 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 const mockStore = configureStore([]);
-const store = mockStore({});
+const store = mockStore({
+	app: {
+		secondLang: true,
+	},
+});
 
 describe('concepts-compare', () => {
 	it('renders without crashing', () => {
