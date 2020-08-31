6c81033400a4dbd3d748cd96bbb62d4e8f215b56
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-08-31 08:47:26 +0200

    fix: remove warning during unt test

--- app/src/js/applications/classifications/item/home.spec.js
+++ app/src/js/applications/classifications/item/home.spec.js
@@ -7,10 +7,19 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 const mockStore = configureStore([]);
-const store = mockStore({});
+const store = mockStore({
+	app: {
+		secondLang: true,
+	},
+});
 
 const item = {
-	general: { prefLabelLg1: 'Label', classificationId: 'id' },
+	general: {
+		prefLabelLg1: 'Label',
+		classificationId: 'id',
+		itemId: 'itemId',
+		conceptVersion: 'conceptVersion',
+	},
 	notes: {},
 	narrowers: [{ id: '1', label: 'Narrower 1' }],
 };
