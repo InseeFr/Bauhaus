4296bcb0f3c7f00e09aa7f3c8f81db6957b9ea8f
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-10-09 20:46:47 +0100

    feat: review codeslist

--- packages/structures/src/apis/index.js
+++ packages/structures/src/apis/index.js
@@ -1,3 +1,2 @@
 export { default as StructureAPI } from './structure-api';
-export { getConcepts } from './concepts';
 export { getCodeList, getFormattedCodeList } from './code-list';
---
+++ packages/utilities/src/apis/codes-list-api.js
@@ -0,0 +1,7 @@
+import { buildApi } from './build-api';
+
+export const api = {
+	getCodesLists: () => [''],
+};
+
+export default buildApi('codeList', api);
