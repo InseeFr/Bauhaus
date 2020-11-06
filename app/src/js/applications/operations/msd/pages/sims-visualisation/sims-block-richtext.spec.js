edbe9b357cbb89224d372122d10f56fc5e9b88e4
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2020-11-06 13:14:40 +0100

    feat: add unit test for Block Rich Twzt

--- app/src/js/applications/operations/msd/pages/sims-visualisation/sims-block-richtext.spec.js
+++ app/src/js/applications/operations/msd/pages/sims-visualisation/sims-block-richtext.spec.js
@@ -3,6 +3,8 @@ import SimsBlockRichText from "./sims-block-richtext";
 import { render } from '@testing-library/react';
 
 const documents = {
+	labelLg1: "sectionLg1",
+	labelLg2: "sectionLg2",
 	documentsLg1: [{
 		labelLg1: 'Page 1',
 		labelLg2: 'Page 2',
@@ -23,6 +25,15 @@ const documents = {
 	}]
 }
 describe("<SimsBlockRichText />", () => {
+
+	it("should display labelLg1", () => {
+		const { container } = render(<SimsBlockRichText currentSection={documents} isSecondLang={false}/>);
+		expect(container.querySelector("p").innerHTML).toBe("sectionLg1")
+	})
+	it("should display labelLg2", () => {
+		const { container } = render(<SimsBlockRichText currentSection={documents} isSecondLang={true}/>);
+		expect(container.querySelector("p").innerHTML).toBe("sectionLg2")
+	})
 	it("should display link/document Lg1", () => {
 		const { container } = render(<SimsBlockRichText currentSection={documents} isSecondLang={false}/>)
 		expect(container.querySelector(".documentsbloc:nth-child(2) a").innerHTML).toBe('Document 1');
