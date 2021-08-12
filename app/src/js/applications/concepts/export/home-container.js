cd5390d8a73826f81a3800f6abc83281670452a7
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-08-12 11:45:10 +0200

    refactor: rewrite export page for concept

--- app/src/js/applications/concepts/export/home-container.js
+++ app/src/js/applications/concepts/export/home-container.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import * as select from 'js/reducers';
@@ -9,46 +9,42 @@ import exportConceptList from 'js/actions/concepts/export-multi';
 import loadConceptList from 'js/actions/concepts/list';
 import { OK } from 'js/constants';
 
-class ConceptsToExportContainer extends Component {
-	constructor() {
-		super();
-		this.state = {
-			exportRequested: false,
-		};
-		this.handleExportConceptList = (ids, MimeType) => {
-			this.props.exportConceptList(ids, MimeType);
-			this.setState({
-				exportRequested: true,
-			});
-		};
-	}
+const ConceptsToExportContainer = ({
+	concepts,
+	exportStatus,
+	loadConceptList,
+	exportConceptList
+	}) => {
+	const [exportRequested, setExportRequested] = useState(false)
 
-	componentWillMount() {
-		if (!this.props.concepts) this.props.loadConceptList();
-	}
-
-	render() {
-		const { concepts, exportStatus } = this.props;
-		const { exportRequested } = this.state;
-		if (exportRequested) {
-			if (exportStatus === OK) {
-				return <Redirect to="/concepts" />;
-			}
-			return <Loading textType="exporting" />;
+	useEffect(() => {
+		if(!concepts){
+			loadConceptList()
 		}
+	}, [concepts, loadConceptList]);
 
-		if (!concepts) {
-			return <Loading />;
-		}
-		return (
-			<ConceptsToExport
-				concepts={concepts}
-				handleExportConceptList={this.handleExportConceptList}
-			/>
-		);
+	const handleExportConceptList = (ids, MimeType) => {
+		exportConceptList(ids, MimeType);
+		setExportRequested(true);
 	}
+
+	if (exportRequested) {
+		if (exportStatus === OK) {
+			return <Redirect to="/concepts" />;
+		}
+		return <Loading textType="exporting" />;
+	}
+
+	if (!concepts) {
+		return <Loading />;
+	}
+	return (
+		<ConceptsToExport
+			concepts={concepts}
+			handleExportConceptList={handleExportConceptList}
+		/>
+	);
 }
-
 const mapStateToProps = state => ({
 	concepts: select.getConceptList(state),
 	exportStatus: select.getStatus(state, EXPORT_CONCEPT_LIST),
