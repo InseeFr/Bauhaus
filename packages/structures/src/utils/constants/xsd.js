b8eedffbae6288f5c9a453c1f433ddee62eac6de
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-12-27 14:25:43 +0100

    feat: add new Component types

--- packages/structures/src/utils/constants/xsd.js
+++ packages/structures/src/utils/constants/xsd.js
@@ -1,16 +1,20 @@
 import D from '../../i18n/build-dictionary';
-import { XSD, INSEE } from './prefixes';
+import { XSD, INSEE, IGEO } from './prefixes';
 
 export const XSD_DATE = `${XSD}date`;
-export const XSD_FLOAT = `${XSD}float`;
+export const XSD_DATE_TIME = `${XSD}dateTime`;
+export const XSD_FLOAT = `${XSD}double`;
 export const XSD_INT = `${XSD}int`;
 export const XSD_STRING = `${XSD}string`;
 export const XSD_CODE_LIST = `${INSEE}codeList`;
+export const IDEO_TERRITOIRE_STATISTIQUE = `${IGEO}TerritoireStatistique`;
 
 export const XSD_TYPES = [
 	{ value: XSD_DATE, label: D.dateType },
+	{ value: XSD_DATE_TIME, label: D.dateTimeType },
 	{ value: XSD_FLOAT, label: D.floatType },
 	{ value: XSD_INT, label: D.intType },
-	{ value: XSD_STRING, label: D.stringType },
 	{ value: XSD_CODE_LIST, label: D.codesListTitle },
+	{ value: IDEO_TERRITOIRE_STATISTIQUE, label: D.territoireStatistique },
+	{ value: XSD_STRING, label: D.stringType },
 ];
