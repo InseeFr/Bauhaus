178b9d62ecab56c51a20bb89d33eb624d6c86650
BulotF <francois.bulot@insee.fr>
2022-02-23 10:42:26 +0100

    Update controls.js

--- app/src/js/applications/classifications/visualization/controls.js
+++ app/src/js/applications/classifications/visualization/controls.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { goBack, ReturnButton, Button, ActionToolbar } from '@inseefr/wilco';
-import { ValidationButton } from 'bauhaus-utilities';
+import { ValidationButton, Auth } from 'bauhaus-utilities';
 import D from 'js/i18n';
 
 const ClassificationControls = (props) => {
 	const location = props.history.location.pathname;
 	const treeLocation = `${location}/tree`;
-	const { authType } = props.permission;
 
 	return (
 		<ActionToolbar>
