aeccc7df72c692a437b015d8bba912bb36364acd
BulotF <francois.bulot@insee.fr>
2022-03-01 18:02:49 +0100

    Update controls.spec.js

--- app/src/js/applications/classifications/visualization/controls.spec.js
+++ app/src/js/applications/classifications/visualization/controls.spec.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Controls from './controls';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import Controls from './controls';
+
+const store = mockStore({
+	app: {
+		secondLang: true,
+	},
+});
 
 describe('classification-visualization-controls', () => {
 	it('renders without crashing', () => {
 		render(
-			<Controls
-				classification={{}}
-				publish={() => {}}
-				permission={{ authType: '', roles: [''] }}
-			/>,
+			<Provider store={store}>
+				<Controls
+					classification={{}}
+					publish={() => {}}
+					permission={{ authType: '', roles: [''] }}
+				/>
+			</Provider>,
 			{
 				wrapper: MemoryRouter,
 			}
