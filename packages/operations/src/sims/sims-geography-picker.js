8774b44ac30177bbb898dd5f4c3ff050bbf13bee
BulotF <francois.bulot@insee.fr>
2022-03-03 15:58:08 +0100

    Show geography select only when territory is loaded

--- packages/operations/src/sims/sims-geography-picker.js
+++ packages/operations/src/sims/sims-geography-picker.js
@@ -9,7 +9,6 @@ import SimsGeographyField from './sims-geography-field';
 import './sims-geography-picker.scss';
 import { SimsGeographyI18NLabel } from 'bauhaus-operations';
 
-
 const SimsGeographyPicker = ({ onChange, value, loadGeographies }) => {
 	const [territory, setTerritory] = useState();
 	const geographiesOptions = useSelector(Stores.Geographies.getAllOptions);
@@ -19,15 +18,20 @@ const SimsGeographyPicker = ({ onChange, value, loadGeographies }) => {
 	}, []);
 
 	const openViewPanel = useCallback(() => {
-		setTerritory(geographiesOptions?.find(({ value: v }) => v === value)?.geography);
+		setTerritory(
+			geographiesOptions?.find(({ value: v }) => v === value)?.geography
+		);
 		setSlidingModal(true);
 	}, [geographiesOptions, value]);
 
-	const onSave = useCallback((territoryUri) => {
-		setSlidingModal(false);
-		loadGeographies();
-		onChange(territoryUri)
-	}, [loadGeographies, onChange]);
+	const onSave = useCallback(
+		(territoryUri) => {
+			setSlidingModal(false);
+			loadGeographies();
+			onChange(territoryUri);
+		},
+		[loadGeographies, onChange]
+	);
 
 	const onCancel = useCallback(() => {
 		setTerritory(undefined);
@@ -37,44 +41,68 @@ const SimsGeographyPicker = ({ onChange, value, loadGeographies }) => {
 		return <SimsGeographyI18NLabel geography={geography} />;
 	};
 
-	const shouldSeeViewButton = geographiesOptions?.find(({ value: v }) => v === value)?.typeTerritory === "Territoire Statistique";
+	const shouldSeeViewButton =
+		geographiesOptions?.find(({ value: v }) => v === value)?.typeTerritory ===
+		'Territoire Statistique';
 	return (
 		<>
 			<div className="bauhaus-sims-geography-picker">
 				<div className="form-group">
-					<ReactSelect
-						value={geographiesOptions.find(
-							({ value: gValue }) => gValue === value
-						)}
-						filterOption={(option, searchValue) => {
-							const search = searchValue.toLowerCase();
-							const label = option.data.label.toLowerCase();
-							const typeTerritory = option.data.typeTerritory.toLowerCase();
-							return !searchValue
-								|| label.indexOf(search) >= 0
-								|| typeTerritory.indexOf(search) >= 0
-						}}
-						options={geographiesOptions}
-						onChange={(e) => onChange(e ? e.value : '')}
-						placeholder={''}
-						isSearchable={true}
-						noResultsText={D.noResult}
-						isClearable={true}
-						formatOptionLabel={formatOptionLabel}
-					/>
+					{territory && (
+						<ReactSelect
+							value={geographiesOptions.find(
+								({ value: gValue }) => gValue === value
+							)}
+							filterOption={(option, searchValue) => {
+								const search = searchValue.toLowerCase();
+								const label = option.data.label.toLowerCase();
+								const typeTerritory = option.data.typeTerritory.toLowerCase();
+								return (
+									!searchValue ||
+									label.indexOf(search) >= 0 ||
+									typeTerritory.indexOf(search) >= 0
+								);
+							}}
+							options={geographiesOptions}
+							onChange={(e) => onChange(e ? e.value : '')}
+							placeholder={''}
+							isSearchable={true}
+							noResultsText={D.noResult}
+							isClearable={true}
+							formatOptionLabel={formatOptionLabel}
+						/>
+					)}
 				</div>
 
 				<Auth.AuthGuard roles={[Auth.ADMIN]}>
-					<button type="button" className="btn btn-default" onClick={openNewPanel}>
+					<button
+						type="button"
+						className="btn btn-default"
+						onClick={openNewPanel}
+					>
 						{D.btnNew}
 					</button>
 				</Auth.AuthGuard>
-				<button disabled={!shouldSeeViewButton} type="button" className="btn btn-default" onClick={openViewPanel}>
+				<button
+					disabled={!shouldSeeViewButton}
+					type="button"
+					className="btn btn-default"
+					onClick={openViewPanel}
+				>
 					{D.btnSee}
 				</button>
 			</div>
-			<SlidingPanel type={'right'} isOpen={slidingModal} size={60} backdropClicked={() => setSlidingModal(false)}>
-				<SimsGeographyField onCancel={onCancel} onSave={onSave} territory={territory}/>
+			<SlidingPanel
+				type={'right'}
+				isOpen={slidingModal}
+				size={60}
+				backdropClicked={() => setSlidingModal(false)}
+			>
+				<SimsGeographyField
+					onCancel={onCancel}
+					onSave={onSave}
+					territory={territory}
+				/>
 			</SlidingPanel>
 		</>
 	);
@@ -83,5 +111,4 @@ const mapDispatchToProps = {
 	loadGeographies: Stores.Geographies.loadGeographies,
 };
 
-export default connect(undefined, mapDispatchToProps)(SimsGeographyPicker)
-
+export default connect(undefined, mapDispatchToProps)(SimsGeographyPicker);
