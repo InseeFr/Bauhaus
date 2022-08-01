a54694e6a5c12bf8930cad8b3e869145cb13d64a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-08-01 16:50:09 +0200

    feat: rewrite outline-block component

--- app/src/js/applications/operations/msd/outline/outline-block.js
+++ app/src/js/applications/operations/msd/outline/outline-block.js
@@ -1,93 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import { toggleOpen, isOpen } from 'js/applications/operations/msd/utils';
 import PropTypes from 'prop-types';
 import D from 'js/i18n';
-export class OutlineBlock extends Component {
-	static propTypes = {
-		secondary: PropTypes.bool,
-		parent: PropTypes.string,
-		baseUrl: PropTypes.string,
-	};
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			children: Object.keys(this.props.children).reduce((acc, childId) => {
-				return {
-					...acc,
-					[childId]: {
-						...this.props.children[childId],
-						opened: isOpen(childId),
-					},
-				};
-			}, {}),
-		};
-	}
-
-	expandOrCollapseItem = ({ currentTarget: { id } }) => {
-		toggleOpen(id);
-		this.setState(previousState => ({
-			children: {
-				...previousState.children,
-				[id]: {
-					...previousState.children[id],
-					opened: !previousState.children[id].opened,
-				},
+export const OutlineBlock = ({ secondary, parent, baseUrl, disableSectionAnchor = false, children }) => {
+	const [childrenDictionary, setChildrenDictionary] = useState(Object.keys(children).reduce((acc, childId) => {
+		return {
+			...acc,
+			[childId]: {
+				...children[childId],
+				opened: isOpen(childId),
 			},
-		}));
+		};
+	}, {}));
+
+	const expandOrCollapseItem = ({ currentTarget: { id } }) => {
+		toggleOpen(id);
+		setChildrenDictionary({
+			...childrenDictionary,
+			[id]: {
+				...childrenDictionary[id],
+				opened: !childrenDictionary[id].opened,
+			}
+		})
 	};
-	render() {
-		const {
-			secondary,
-			parent,
-			baseUrl = '/operations/help/',
-			disableSectionAnchor = false,
-		} = this.props;
-		const { children } = this.state;
-		if (Object.keys(children).length <= 0) return null;
-		return (
-			<ul className={secondary ? 'msd__item-secondary' : 'msd__item'}>
-				{Object.values(children).map(child => {
-					return (
-						<li key={child.idMas} className="help-item">
-							{Object.keys(child.children).length > 0 && (
-								<button
-									className="msd__item-updown"
-									title={child.opened ? D.hide : D.display}
-									id={child.idMas}
-									onClick={this.expandOrCollapseItem}
-								>
+
+	if (Object.keys(childrenDictionary).length <= 0) return null;
+
+	const childrenArray = Object.values(childrenDictionary);
+
+	return (
+		<ul className={secondary ? 'msd__item-secondary' : 'msd__item'}>
+			{childrenArray.map((child) => {
+				return (
+					<li key={child.idMas} className="help-item">
+						{Object.keys(child.children).length > 0 && (
+							<button
+								className="msd__item-updown"
+								title={child.opened ? D.hide : D.display}
+								id={child.idMas}
+								onClick={expandOrCollapseItem}
+							>
 									<span
 										className={`glyphicon glyphicon-chevron-${
 											child.opened ? 'up' : 'down'
 										}`}
 									/>
-								</button>
-							)}
-							<Link
-								smooth
-								to={`${baseUrl}${disableSectionAnchor ? '' : parent}#${
-									child.idMas
-								}`}
-							>
-								{child.idMas} - {child.masLabelBasedOnCurrentLang}
-							</Link>
-							{child.opened && (
-								<OutlineBlock
-									children={child.children}
-									secondary
-									parent={parent}
-									baseUrl={baseUrl}
-									disableSectionAnchor={disableSectionAnchor}
-								/>
-							)}
-						</li>
-					);
-				})}
-			</ul>
-		);
-	}
+							</button>
+						)}
+						<Link
+							smooth
+							to={`${baseUrl}${disableSectionAnchor ? '' : parent}#${
+								child.idMas
+							}`}
+						>
+							{child.idMas} - {child.masLabelBasedOnCurrentLang}
+						</Link>
+						{child.opened && (
+							<OutlineBlock
+								children={child.children}
+								secondary
+								parent={parent}
+								baseUrl={baseUrl}
+								disableSectionAnchor={disableSectionAnchor}
+							/>
+						)}
+					</li>
+				);
+			})}
+		</ul>
+	);
 }
+
+OutlineBlock.propTypes = {
+	secondary: PropTypes.bool,
+	parent: PropTypes.string,
+	baseUrl: PropTypes.string,
+	disableSectionAnchor: PropTypes.bool,
+	children: PropTypes.object
+};
 
 export default OutlineBlock;
