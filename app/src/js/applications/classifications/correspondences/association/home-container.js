e68ec18e4feee0bd032a8e035eb2effb85a8bbdd
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-09-07 15:50:37 +0200

    feat: rewrite component with Functionnal style

--- app/src/js/applications/classifications/correspondences/association/home-container.js
+++ app/src/js/applications/classifications/correspondences/association/home-container.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Loading, buildExtract } from '@inseefr/wilco';
 import AssociationHome from './home';
@@ -10,24 +10,21 @@ import { Stores } from 'bauhaus-utilities';
 const extractCorrespondenceId = buildExtract('correspondenceId');
 const extractAssociationId = buildExtract('associationId');
 
-class AssociationHomeContainer extends Component {
-	componentWillMount() {
-		const { association, correspondenceId, associationId } = this.props;
+const AssociationHomeContainer = ({ association, secondLang, langs, correspondenceId, associationId, loadCorrespondenceAssociation }) => {
+	useEffect(() => {
 		if (!association) {
-			this.props.loadCorrespondenceAssociation(correspondenceId, associationId);
+			loadCorrespondenceAssociation(correspondenceId, associationId);
 		}
-	}
-	render() {
-		const { association, secondLang, langs } = this.props;
-		if (!association) return <Loading />;
-		return (
-			<AssociationHome
-				association={association}
-				secondLang={secondLang}
-				langs={langs}
-			/>
-		);
-	}
+	}, [association, loadCorrespondenceAssociation, correspondenceId, associationId, ])
+
+	if (!association) return <Loading />;
+	return (
+		<AssociationHome
+			association={association}
+			secondLang={secondLang}
+			langs={langs}
+		/>
+	);
 }
 
 const mapStateToProps = (state, ownProps) => {
