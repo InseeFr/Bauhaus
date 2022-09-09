bf5ea0952da34271c756866bd905e4ad5076738a
Jérôme Bourgeois <89983767+JeromeMBourgeois@users.noreply.github.com>
2022-09-09 08:41:30 +0200

    Revert "Update Classification"

--- app/src/js/applications/classifications/visualization/controls.js
+++ app/src/js/applications/classifications/visualization/controls.js
@@ -3,12 +3,8 @@ import { withRouter } from 'react-router-dom';
 import { goBack, ReturnButton, Button, ActionToolbar } from '@inseefr/wilco';
 import { ValidationButton, Auth } from 'bauhaus-utilities';
 import D from 'js/i18n';
-import PropTypes from 'prop-types';
-
 
 const ClassificationControls = (props) => {
-	const { classification, publish } = props;
-
 	const location = props.history.location.pathname;
 	const treeLocation = `${location}/tree`;
 
@@ -17,24 +13,13 @@ const ClassificationControls = (props) => {
 			<ReturnButton action={goBack(props, `/classifications`)} />
 			<Auth.AuthGuard roles={[Auth.ADMIN]}>
 				<ValidationButton
-					object={classification}
-					callback={publish}
-				/>
-			</Auth.AuthGuard>
-			<Auth.AuthGuard roles={[Auth.ADMIN]}>
-				<Button
-					action={`/classifications/classification/${classification.id}/modify`}
-					label={D.btnUpdate}
+					object={props.classification}
+					callback={props.publish}
 				/>
 			</Auth.AuthGuard>
 			<Button key={D.btnTree} action={treeLocation} label={D.btnTree} col={3} />
 		</ActionToolbar>
 	);
-};
-
-ClassificationControls.propTypes = {
-	classification: PropTypes.object.isRequired,
-	publish: PropTypes.func.isRequired,
 };
 
 export default withRouter(ClassificationControls);
--- app/src/js/applications/classifications/visualization/home-container.js
+++ app/src/js/applications/classifications/visualization/home-container.js
@@ -1,30 +1,56 @@
-import React  from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { PropTypes } from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ClassificationVisualization from './home';
 import { Loading } from '@inseefr/wilco';
 import * as mainSelect from 'js/reducers';
 import { Stores, Auth } from 'bauhaus-utilities';
-import { useClassification, usePublishClassification } from '../hooks';
+import api from 'js/remote-api/classifications-api';
 
-const ClassificationVisualizationContainer = () => {
-
+const ClassificationVisualizationContainer = (props) => {
 	const { id } = useParams();
+	const [loading, setLoading] = useState(true);
 	const langs = useSelector((state) => mainSelect.getLangs(state));
 	const secondLang = useSelector((state) =>
 		Stores.SecondLang.getSecondLang(state)
 	);
+	const [classification, setClassification] = useState([]);
 	const permission = useSelector((state) => Auth.getPermission(state));
+	const [publishing, setPublishing] = useState(false);
+	const [serverSideError, setServerSideError] = useState();
 
-	const { isLoading, classification } = useClassification(id);
-	const { isPublishing, publish, error } = usePublishClassification();
+	const getClassification = (id) => {
+		Promise.all([
+			api.getClassificationGeneral(id),
+			api.getClassificationLevels(id),
+		])
+			.then(([general, levels]) => {
+				setClassification({ general, levels });
+			})
+			.finally(() => setLoading(false));
+	};
 
+	const publish = useCallback(() => {
+		setPublishing(true);
+		api
+			.publishClassification(classification.general)
+			.then(() => {
+				return getClassification(id);
+			})
+			.catch((error) => setServerSideError(error))
+			.finally(() => setPublishing(false));
+	}, [id, classification.general]);
 
-	if (isLoading) {
+	useEffect(() => {
+		getClassification(id);
+	}, [id]);
+
+	if (loading) {
 		return <Loading />;
 	}
 
-	if (isPublishing) return <Loading text="publishing" />;
+	if (publishing) return <Loading text="publishing" />;
 
 	if (!classification) return <Loading />;
 	return (
@@ -34,11 +60,18 @@ const ClassificationVisualizationContainer = () => {
 			secondLang={secondLang}
 			langs={langs}
 			permission={permission}
-			publish={() => publish(id)}
-			serverSideError={error}
+			publish={publish}
+			serverSideError={serverSideError}
 		/>
 	);
 };
 
 export default ClassificationVisualizationContainer;
 
+ClassificationVisualizationContainer.propTypes = {
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			id: PropTypes.string.isRequired,
+		}),
+	}),
+};
