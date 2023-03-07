5cbb551a8e2efab69ab23da5e745af128e927f39
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-03-07 20:42:47 +0100

    feat: rewrite the note compare component

--- app/src/js/applications/shared/note-compare/index.js
+++ app/src/js/applications/shared/note-compare/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import D from 'js/i18n';
 import { ExplanatoryNote } from 'js/applications/shared/explanatory-note';
@@ -7,84 +7,68 @@ import {
 	creatSelectListSelectedLast,
 } from 'js/utils/array-utils';
 
-class CompareNotes extends Component {
-	constructor(props) {
-		super(props);
-		const { version } = props;
-		this.state = { select1: version - 1, select2: version };
+const CompareNotes = ({ secondLang,
+												notes,
+												langs: { lg1, lg2 },
+												version,
+												buildNotes }) => {
+	const [firstChoice, setFirstChoice] = useState(version - 1);
+	const [secondChoice, setSecondChoice] = useState(version)
 
-		this.changeSelect1 = (e) => {
-			this.setState({
-				select1: e.target.value,
-			});
-		};
-		this.changeSelect2 = (e) => {
-			this.setState({
-				select2: e.target.value,
-			});
-		};
-	}
-	render() {
-		const {
-			secondLang,
-			notes,
-			langs: { lg1, lg2 },
-			version,
-			buildNotes,
-		} = this.props;
-		const { select1, select2 } = this.state;
-		const notesVersion1 = buildNotes(notes[select1]);
-		const notesVersion2 = buildNotes(notes[select2]);
+	const changeFirstChoice = (e) => setFirstChoice(e.target.value);
+	const changeSecondChoice = (e) => setSecondChoice(e.target.value);
 
-		return (
-			<div>
-				<div className="row">
-					<div className="col-md-6 text-center">
-						<h3>
-							{' '}
-							{D.version} :{' '}
-							<select
-								value={this.state.select1}
-								onChange={(e) => this.changeSelect1(e)}
-							>
-								{creatSelectList(version)}
-							</select>
-						</h3>
-					</div>
-					<div className="col-md-6 text-center">
-						<h3>
-							{' '}
-							{D.version} :{' '}
-							<select
-								value={this.state.select2}
-								onChange={(e) => this.changeSelect2(e)}
-							>
-								{creatSelectListSelectedLast(version)}
-							</select>
-						</h3>
-					</div>
+	const notesVersion1 = buildNotes(notes[firstChoice]);
+	const notesVersion2 = buildNotes(notes[secondChoice]);
+
+	return (
+		<div>
+			<div className="row">
+				<div className="col-md-6 text-center">
+					<h3>
+						{' '}
+						{D.version} :{' '}
+						<select
+							value={firstChoice}
+							onChange={changeFirstChoice}
+						>
+							{creatSelectList(version)}
+						</select>
+					</h3>
 				</div>
-				{notesVersion2.map((n, i) => (
-					<div className="row" key={`notes-compare-${i}`}>
-						<ExplanatoryNote
-							text={
-								secondLang ? notesVersion1[i]['lg2'] : notesVersion1[i]['lg1']
-							}
-							title={D[notesVersion1[i].title]}
-							lang={secondLang ? lg2 : lg1}
-							alone={false}
-						/>
-						<ExplanatoryNote
-							text={secondLang ? n.lg2 : n.lg1}
-							title={D[n.title]}
-							lang={secondLang ? lg2 : lg1}
-							alone={false}
-						/>
-					</div>
-				))}
+				<div className="col-md-6 text-center">
+					<h3>
+						{' '}
+						{D.version} :{' '}
+						<select
+							value={secondChoice}
+							onChange={changeSecondChoice}
+						>
+							{creatSelectListSelectedLast(version)}
+						</select>
+					</h3>
+				</div>
 			</div>
-		);
-	}
+			{notesVersion2.map((n, i) => (
+				<div className="row" key={`notes-compare-${i}`}>
+					<ExplanatoryNote
+						text={
+							secondLang ? notesVersion1[i]['lg2'] : notesVersion1[i]['lg1']
+						}
+						title={D[notesVersion1[i].title]}
+						lang={secondLang ? lg2 : lg1}
+						alone={false}
+					/>
+					<ExplanatoryNote
+						text={secondLang ? n.lg2 : n.lg1}
+						title={D[n.title]}
+						lang={secondLang ? lg2 : lg1}
+						alone={false}
+					/>
+				</div>
+			))}
+		</div>
+	);
 }
 
 CompareNotes.propTypes = {
