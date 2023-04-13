160452bf2f9ae6ca141923bd3f6106fb4ba3f421
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-04-13 21:26:48 +0200

    feat: review client side validation concept

--- app/src/js/applications/concepts/edition-creation/notes.js
+++ app/src/js/applications/concepts/edition-creation/notes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Tabs, Tab } from 'react-bootstrap';
 import NoteEdition from 'js/applications/shared/note-edition';
@@ -45,83 +45,72 @@ const handleFieldChange = (handleChange, maxLengthScopeNote) =>
 		{}
 	);
 
-class NotesEdition extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeTab: 0,
-		};
-		const { handleChange, maxLengthScopeNote } = this.props;
-		this.handlers = handleFieldChange(handleChange, maxLengthScopeNote);
-		this.selectTab = tabIndex =>
-			this.setState({
-				activeTab: tabIndex,
-			});
-	}
-	render() {
-		const {
-			notes,
-			disseminationStatus,
-			maxLengthScopeNote,
-			langs,
-		} = this.props;
-		const { activeTab } = this.state;
-		return (
-			<ul className="nav nav-tabs nav-justified">
-				<Tabs
-					defaultActiveKey={0}
-					id="kindOfNote"
-					onSelect={this.selectTab}
-					justified
-				>
-					{noteTypes(maxLengthScopeNote).map(
-						(
-							{ rawTitle, noteLg1Name, noteLg2Name, redLg1Empty, maxLength },
-							i
-						) => {
-							const noteLg1 = notes[noteLg1Name];
-							const noteLg2 = notes[noteLg2Name];
-							//note fr empty and we value the `redFrEmptpy` function to know if
-							//given the dissemination status, it should be highlighted or not
-							let noteEdition;
-							const highlight =
-								redLg1Empty &&
-								HTMLUtils.htmlIsEmpty(noteLg1) &&
-								redLg1Empty(disseminationStatus);
-							const title = highlight ? (
-								<div className="red">{D1[rawTitle]}</div>
-							) : (
-								D1[rawTitle]
-							);
-							if (activeTab === i) {
-								noteEdition = (
-									<NoteEdition
-										noteLg1={noteLg1}
-										noteLg2={noteLg2}
-										handleChangeLg1={this.handlers[noteLg1Name]}
-										handleChangeLg2={this.handlers[noteLg2Name]}
-										maxLength={maxLength}
-										langs={langs}
-									/>
-								);
-							}
+const NotesEdition = ({
+	notes,
+	disseminationStatus,
+	maxLengthScopeNote,
+	langs,
+	handleChange,
+}) =>  {
 
-							return (
-								<Tab
-									key={noteLg1Name}
-									eventKey={i}
-									title={title}
-									style={{ marginTop: '20px' }}
-								>
-									{noteEdition}
-								</Tab>
+	const [activeTab, setActiveTab] = useState(0);
+	const handlers = handleFieldChange(handleChange, maxLengthScopeNote);
+
+	return (
+		<ul className="nav nav-tabs nav-justified">
+			<Tabs
+				defaultActiveKey={0}
+				id="kindOfNote"
+				onSelect={setActiveTab}
+				justified
+			>
+				{noteTypes(maxLengthScopeNote).map(
+					(
+						{ rawTitle, noteLg1Name, noteLg2Name, redLg1Empty, maxLength },
+						i
+					) => {
+						const noteLg1 = notes[noteLg1Name];
+						const noteLg2 = notes[noteLg2Name];
+						//note fr empty and we value the `redFrEmptpy` function to know if
+						//given the dissemination status, it should be highlighted or not
+						let noteEdition;
+						const highlight =
+							redLg1Empty &&
+							HTMLUtils.htmlIsEmpty(noteLg1) &&
+							redLg1Empty(disseminationStatus);
+						const title = highlight ? (
+							<div className="red">{D1[rawTitle]}</div>
+						) : (
+							D1[rawTitle]
+						);
+						if (activeTab === i) {
+							noteEdition = (
+								<NoteEdition
+									noteLg1={noteLg1}
+									noteLg2={noteLg2}
+									handleChangeLg1={handlers[noteLg1Name]}
+									handleChangeLg2={handlers[noteLg2Name]}
+									maxLength={maxLength}
+									langs={langs}
+								/>
 							);
 						}
-					)}
-				</Tabs>
-			</ul>
-		);
-	}
+
+						return (
+							<Tab
+								key={noteLg1Name}
+								eventKey={i}
+								title={title}
+								style={{ marginTop: '20px' }}
+							>
+								{noteEdition}
+							</Tab>
+						);
+					}
+				)}
+			</Tabs>
+		</ul>
+	);
 }
 NotesEdition.propTypes = {
 	conceptGeneral: notePropTypes,
