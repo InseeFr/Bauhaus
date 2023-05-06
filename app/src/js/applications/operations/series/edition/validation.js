d799524b12acf9679e22ddbf848e1ec06ec3cc9a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-06 21:31:06 +0200

    feat: add extra mandatory field for series

--- app/src/js/applications/operations/series/edition/validation.js
+++ app/src/js/applications/operations/series/edition/validation.js
@@ -1,30 +1,49 @@
 import D, { D1, D2 } from 'js/i18n';
 
-export function validate({ creators, prefLabelLg1, prefLabelLg2, family }) {
-	const errorMessages = [];
+const listOfExtraMandatoryFields = (process.env.REACT_APP_VALIDATION_OPERATION_SERIES_EXTRA_MANDATORY_FIELDS ?? "").split(',')
+
+const fieldToTitleMapping = {
+	creators: D1.creatorTitle,
+	prefLabelLg1: D1.title,
+	prefLabelLg2: D2.title,
+	family: D1.familyTitle,
+	typeCode: D1.operationType,
+	accrualPeriodicityCode: D1.dataCollectFrequency
+}
+
+export const isMandatoryField = fieldName => listOfExtraMandatoryFields.indexOf(fieldName) >= 0;
+
+export function validate({ creators, prefLabelLg1, prefLabelLg2, family, ...otherFields }) {
+	const fields = {}
+	const errorMessage = [];
 
 	if(!creators || creators.length === 0){
-		errorMessages.push(D.mandatoryProperty(D1.creatorTitle))
+		errorMessage.push(D.mandatoryProperty(fieldToTitleMapping.creators));
+		fields.creators = D.mandatoryProperty(fieldToTitleMapping.creators);
 	}
 
 	if(!prefLabelLg1){
-		errorMessages.push(D.mandatoryProperty(D1.title))
+		errorMessage.push(D.mandatoryProperty(fieldToTitleMapping.prefLabelLg1));
+		fields.prefLabelLg1 = D.mandatoryProperty(fieldToTitleMapping.prefLabelLg1);
 	}
 	if(!prefLabelLg2){
-		errorMessages.push(D.mandatoryProperty(D2.title))
+		errorMessage.push(D.mandatoryProperty(fieldToTitleMapping.prefLabelLg2));
+		fields.prefLabelLg2 = D.mandatoryProperty(fieldToTitleMapping.prefLabelLg2);
 	}
 
 	if (!family) {
-		errorMessages.push(D.mandatoryProperty(D1.familyTitle))
+		errorMessage.push(D.mandatoryProperty(fieldToTitleMapping.family));
+		fields.family = D.mandatoryProperty(fieldToTitleMapping.family);
 	}
 
+	listOfExtraMandatoryFields.forEach(extraMandatoryField => {
+		if(!otherFields[extraMandatoryField]){
+			errorMessage.push(D.mandatoryProperty(fieldToTitleMapping[extraMandatoryField] ?? ""));
+			fields[extraMandatoryField] = D.mandatoryProperty(fieldToTitleMapping[extraMandatoryField] ?? "");
+		}
+	})
 	return {
-		fields: {
-			creators: (!creators || creators.length === 0) ? D.mandatoryProperty(D1.creatorTitle) : '',
-			prefLabelLg1: !prefLabelLg1 ? D.mandatoryProperty(D1.title) : '',
-			prefLabelLg2: !prefLabelLg2 ? D.mandatoryProperty(D2.title) : '',
-			family: !family ? D.mandatoryProperty(D1.familyTitle) : '',
-		},
-		errorMessage: errorMessages,
+		fields,
+		errorMessage,
 	};
 }
