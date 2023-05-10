b28ddadf67befb9636f7de3f6f90cc6c7b9b35b0
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-10 09:32:58 +0200

    Feat/new mandatory field (#398)
    * feat: bump eslint dependencies
    
    * feat: add extra mandatory field for series
    
    * fix: solve unit test
    
    * fix: remove double negation

--- app/src/js/applications/operations/series/edition/validation.spec.js
+++ app/src/js/applications/operations/series/edition/validation.spec.js
@@ -1,30 +1,45 @@
 import { validate } from './validation';
 
-describe('validation', function() {
-	it('should return an error for prefLabelLg1', function() {
-		expect(validate({ prefLabelLg2: 'prefLabelLg2', family: 'family', creators: ['creator'] })).toEqual({
+describe('validation', function () {
+	it('should return an error for prefLabelLg1', function () {
+		expect(
+			validate({
+				prefLabelLg2: 'prefLabelLg2',
+				family: 'family',
+				creators: ['creator'],
+				accrualPeriodicityCode: 'accrualPeriodicityCode',
+				typeCode: 'typeCode',
+			})
+		).toEqual({
 			errorMessage: ['The property <strong>Intitulé</strong> is required.'],
 			fields: {
-				'creators': '',
-				'family': '',
-				'prefLabelLg1': 'The property <strong>Intitulé</strong> is required.',
-				'prefLabelLg2': '',
+				prefLabelLg1: 'The property <strong>Intitulé</strong> is required.',
 			},
 		});
 	});
-	it('should return an error for prefLabelLg2', function() {
-		expect(validate({ prefLabelLg1: 'prefLabelLg1', family: 'family', creators: ['creator'] })).toEqual({
+	it('should return an error for prefLabelLg2', function () {
+		expect(
+			validate({
+				prefLabelLg1: 'prefLabelLg1',
+				family: 'family',
+				creators: ['creator'],
+				accrualPeriodicityCode: 'accrualPeriodicityCode',
+				typeCode: 'typeCode',
+			})
+		).toEqual({
 			errorMessage: ['The property <strong>Title</strong> is required.'],
 			fields: {
-				'creators': '',
-				'family': '',
-				'prefLabelLg1': '',
-				'prefLabelLg2': 'The property <strong>Title</strong> is required.',
+				prefLabelLg2: 'The property <strong>Title</strong> is required.',
 			},
 		});
 	});
-	it('should return an error for prefLabelLg1 and prefLabelLg2', function() {
-		expect(validate({})).toEqual({
+	it('should return an error for prefLabelLg1 and prefLabelLg2', function () {
+		expect(
+			validate({
+				accrualPeriodicityCode: 'accrualPeriodicityCode',
+				typeCode: 'typeCode',
+			})
+		).toEqual({
 			errorMessage: [
 				'The property <strong>Propriétaire</strong> is required.',
 				'The property <strong>Intitulé</strong> is required.',
@@ -32,27 +47,26 @@ describe('validation', function() {
 				'The property <strong>Famille</strong> is required.',
 			],
 			fields: {
-				'creators': 'The property <strong>Propriétaire</strong> is required.',
-				'family': 'The property <strong>Famille</strong> is required.',
-				'prefLabelLg1': 'The property <strong>Intitulé</strong> is required.',
-				'prefLabelLg2': 'The property <strong>Title</strong> is required.',
+				creators: 'The property <strong>Propriétaire</strong> is required.',
+				family: 'The property <strong>Famille</strong> is required.',
+				prefLabelLg1: 'The property <strong>Intitulé</strong> is required.',
+				prefLabelLg2: 'The property <strong>Title</strong> is required.',
 			},
 		});
 	});
-	it('should return no errors', function() {
-		expect(validate({
-			prefLabelLg1: 'prefLabelLg2',
-			prefLabelLg2: 'prefLabelLg2',
-			family: 'family',
-			creators: ['creator'],
-		})).toEqual({
+	it('should return no errors', function () {
+		expect(
+			validate({
+				prefLabelLg1: 'prefLabelLg2',
+				prefLabelLg2: 'prefLabelLg2',
+				family: 'family',
+				creators: ['creator'],
+				accrualPeriodicityCode: 'accrualPeriodicityCode',
+				typeCode: 'typeCode',
+			})
+		).toEqual({
 			errorMessage: [],
-			fields: {
-				'creators': '',
-				'family': '',
-				'prefLabelLg1': '',
-				'prefLabelLg2': '',
-			},
+			fields: {},
 		});
 	});
 });
