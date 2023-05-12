e9b0bcb2d79701c062d6d8f948c3d0046089e1d9
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-12 08:59:13 +0200

    Feat/export concepts (#408)
    * feat: make contributor editable for a structure #362
    
    * Be able to download a list of concepts #392
    
    * feat: add unit test
    
    * feat: rewrite concepts validation page

--- app/src/js/applications/collections/export-buttons/index.js
+++ app/src/js/applications/collections/export-buttons/index.js
@@ -1,42 +1,14 @@
 import D from '../../../i18n/build-dictionary';
 import ExportButton from '../dropdown';
 import React from 'react';
-import api from '../../../remote-api/concepts-collection-api';
-import { getContentDisposition } from '@inseefr/wilco';
-import FileSaver from 'file-saver';
 
-
-export default ({ ids, exporting, onDone }) => {
-	const exportCollection = (type, withConcepts, lang = "lg1") => {
-		exporting(true);
-		let promise;
-		if(ids.length > 1){
-			promise = api.getCollectionExportZipByType(ids, type, lang, withConcepts)
-		} else if(ids.length === 1){
-			promise = api.getCollectionExportByType(ids[0], 'application/vnd.oasis.opendocument.text', type, lang, withConcepts);
-		}
-
-		let fileName;
-		return promise.then(res => {
-			fileName = getContentDisposition(
-				res.headers.get('Content-Disposition')
-			)[1];
-			return res;
-		})
-			.then(res => res.blob())
-			.then(blob => {
-				return FileSaver.saveAs(blob, fileName);
-			})
-			.then(() => onDone ? onDone() : Promise.resolve())
-			.finally(() => exporting(false))
-	}
-
+export default ({ exportHandler }) => {
 	return (
 		<ExportButton actions={[
-			<button type="button" onClick={() => exportCollection('ods', false)}>{D.btnOdsExporter}</button>,
-			<button type="button" onClick={() => exportCollection('odt', false)}>{D.btnOdtLg1Exporter}</button>,
-			<button type="button" onClick={() => exportCollection('odt', false, 'lg2')}>{D.btnOdtLg2Exporter}</button>,
-			<button type="button" onClick={() => exportCollection('odt', true)}>{D.btnCollectionConceptExporter}</button>
+			<button type="button" onClick={() => exportHandler('ods', false)}>{D.btnOdsExporter}</button>,
+			<button type="button" onClick={() => exportHandler('odt', false)}>{D.btnOdtLg1Exporter}</button>,
+			<button type="button" onClick={() => exportHandler('odt', false, 'lg2')}>{D.btnOdtLg2Exporter}</button>,
+			<button type="button" onClick={() => exportHandler('odt', true)}>{D.btnCollectionConceptExporter}</button>
 		]}></ExportButton>
 	)
 }
--- app/src/js/applications/collections/export/home-container.js
+++ app/src/js/applications/collections/export/home-container.js
@@ -1,11 +1,12 @@
+import { Loading, getContentDisposition } from '@inseefr/wilco';
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Loading } from '@inseefr/wilco';
 import { ArrayUtils, useTitle } from 'bauhaus-utilities';
 import D from '../../../i18n/build-dictionary';
 import api from '../../../remote-api/concepts-collection-api';
 import Picker from '../../shared/picker-page';
 import ExportButtons from '../export-buttons';
+import FileSaver from 'file-saver';
 
 const CollectionsToExportContainer = () => {
 	useTitle(D.collectionsTitle, D.exportTitle);
@@ -16,24 +17,60 @@ const CollectionsToExportContainer = () => {
 	const [exporting, setExporting] = useState(false);
 
 	useEffect(() => {
-		api.getCollectionList()
-			.then(body => setCollections(ArrayUtils.sortArrayByLabel(body)))
-			.finally(() => setLoading(false))
-	}, [])
+		api
+			.getCollectionList()
+			.then((body) => setCollections(ArrayUtils.sortArrayByLabel(body)))
+			.finally(() => setLoading(false));
+	}, []);
 
-	if(exporting) return <Loading textType="exporting" />;
+	if (exporting) return <Loading textType="exporting" />;
 	if (loading) return <Loading />;
 
-	console.log(ids)
+	const exportCollection = (type, withConcepts, lang = 'lg1') => {
+		setExporting(true);
+		let promise;
+		if (ids.length > 1) {
+			promise = api.getCollectionExportZipByType(ids, type, lang, withConcepts);
+		} else if (ids.length === 1) {
+			promise = api.getCollectionExportByType(
+				ids[0],
+				'application/vnd.oasis.opendocument.text',
+				type,
+				lang,
+				withConcepts
+			);
+		}
+
+		let fileName;
+		return promise
+			.then((res) => {
+				fileName = getContentDisposition(
+					res.headers.get('Content-Disposition')
+				)[1];
+				return res;
+			})
+			.then((res) => res.blob())
+			.then((blob) => {
+				return FileSaver.saveAs(blob, fileName);
+			})
+			.then(() => history.push('/collections'))
+			.finally(() => setExporting(false));
+	};
 	return (
 		<Picker
 			items={collections}
 			title={D.exportTitle}
 			panelTitle={D.collectionsExportPanelTitle}
 			labelWarning={D.hasNotCollectionToExport}
-			handleAction={value => setIds(value)}
+			handleAction={(value) => setIds(value)}
 			context="collections"
-			ValidationButton={() => <ExportButtons ids={ids} onDone={() => history.push('/collections')} exporting={setExporting}/> }
+			ValidationButton={() => (
+				<ExportButtons
+					ids={ids}
+					exporting={setExporting}
+					exportHandler={exportCollection}
+				/>
+			)}
 		/>
 	);
 };
