d05a5385aaf120a8aaeca72a1254b6547f097331
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-06-25 13:33:29 +0200

    fix: solve unit test

--- app/src/js/applications/collections/edition-creation/home.spec.js
+++ app/src/js/applications/collections/edition-creation/home.spec.js
@@ -4,6 +4,8 @@ import Collection from './home';
 import { empty } from 'js/utils/collections/general';
 import { MemoryRouter } from 'react-router-dom';
 
+jest.mock('./general', () => () => <></>)
+
 describe('collection-edition-creation', () => {
 	it('renders without crashing', () => {
 		render(
