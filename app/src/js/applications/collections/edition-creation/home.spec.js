334ef6f104dc2e87ab864d6e5be3abbf049e129a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-07-05 10:12:49 +0200

    425 (#426)
    * fix: review stamps select input on collections page
    
    * fix: solve unit test
    
    * fix: solve sonar issue
    
    * feat: add unit test
    
    * fix: update sonar config file
    
    * feat: update sonar
    
    * fix: sonar

--- app/src/js/applications/collections/edition-creation/home.spec.js
+++ app/src/js/applications/collections/edition-creation/home.spec.js
@@ -4,6 +4,8 @@ import Collection from './home';
 import { empty } from 'js/utils/collections/general';
 import { MemoryRouter } from 'react-router-dom';
 
+jest.mock('./general', () => () => <></>)
+
 describe('collection-edition-creation', () => {
 	it('renders without crashing', () => {
 		render(
@@ -13,7 +15,6 @@ describe('collection-edition-creation', () => {
 				members={[]}
 				collectionList={[]}
 				conceptList={[]}
-				stampList={[]}
 				save={() => console.log('save')}
 				langs={{ lg1: 'fr', lg2: 'en' }}
 			/>,
