e866da9dd7bdf63fe6663f73bc08d014adc9242d
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-09-04 20:20:42 +0200

    feat: add unit test

--- app/src/js/applications/operations/document/edition/edition.spec.js
+++ app/src/js/applications/operations/document/edition/edition.spec.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import { ConfirmationModal } from './edition';
+import OperationsDocumentationEdition, { ConfirmationModal } from './edition';
 
 describe('ConfirmationModal', () => {
 	it('should display two confirmation buttons', async () => {
 		render(<ConfirmationModal isOpen={true} document={{ sims: [] }} onNo={() => {}} onYes={() => {}}/>)
 		await screen.findByText("Yes")
 		await screen.findByText("No")
+	});
+})
+
+describe('OperationsDocumentationEdition', () => {
+	it('should render without errors', async () => {
+		render(<OperationsDocumentationEdition  document={{ sims: [] }} langOptions={[]} />)
 	});
 })
