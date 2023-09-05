825d17e17a78a885d5db3dc9d6acd015b1b89fdc
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-09-05 11:46:30 +0200

    feat: add unit test

--- app/src/js/applications/operations/document/edition/edition.spec.js
+++ app/src/js/applications/operations/document/edition/edition.spec.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import OperationsDocumentationEdition, { ConfirmationModal } from './edition';
+import { DOCUMENT, LINK } from '../utils';
 
 describe('ConfirmationModal', () => {
 	it('should display two confirmation buttons', async () => {
@@ -11,6 +12,21 @@ describe('ConfirmationModal', () => {
 
 describe('OperationsDocumentationEdition', () => {
 	it('should render without errors', async () => {
-		render(<OperationsDocumentationEdition  document={{ sims: [] }} langOptions={[]} />)
+		render(<OperationsDocumentationEdition  type={DOCUMENT} document={{ url: 'url', sims: [] }} langOptions={[]} />)
 	});
+
+	it('should display a drop zone if the type is a document', async () => {
+		const { container } = render(<OperationsDocumentationEdition  type={DOCUMENT} document={{ sims: [] }} langOptions={[]} />)
+		expect(container.querySelector((".dropzone"))).not.toBeNull();
+	});
+
+	it('should not display a drop zone if the type is a document and contain an url', async () => {
+		const { container } = render(<OperationsDocumentationEdition  type={DOCUMENT} document={{ url: 'url', sims: [] }} langOptions={[]} />)
+		expect(container.querySelector((".dropzone"))).toBeNull();
+		expect(Array.from(container.querySelectorAll(".panel-body"))).toHaveLength(1);
+	})
+	it('should display the link url field', async () => {
+		const { container } = render(<OperationsDocumentationEdition  type={LINK} document={{ url: 'url', sims: [] }} langOptions={[]} />)
+		expect(container.querySelector(("[for='url']"))).not.toBeNull();
+	})
 })
