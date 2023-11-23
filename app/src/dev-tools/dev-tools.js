735bebb054844efd89f73364e912e3100bec4d80
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-11-23 16:50:16 +0100

    feat: add the DEV_TOOLS_ENABLED to the configuration.json file (#518)
    Co-authored-by: Jérôme Bourgeois <89983767+JeromeMBourgeois@users.noreply.github.com>

--- app/src/dev-tools/dev-tools.js
+++ app/src/dev-tools/dev-tools.js
@@ -1,19 +1,32 @@
-import React  from 'react';
+import React from 'react';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import {
-	ADMIN, COLLECTIONS_CREATOR, CONCEPT_CONTRIBUTOR, CONCEPTS_CONTRIBUTOR, CONCEPTS_CREATOR, INDICATOR_CONTRIBUTOR, SERIES_CONTRIBUTOR
+	ADMIN,
+	COLLECTIONS_CREATOR,
+	CONCEPT_CONTRIBUTOR,
+	CONCEPTS_CONTRIBUTOR,
+	CONCEPTS_CREATOR,
+	INDICATOR_CONTRIBUTOR,
+	SERIES_CONTRIBUTOR,
 } from 'bauhaus-utilities/src/auth/roles';
 import Select from 'react-select';
 import D from 'js/i18n';
 import { createRoot } from 'react-dom/client';
 import { CHECK_AUTH } from '../js/store/users';
 
-const options = [ADMIN, CONCEPTS_CREATOR, COLLECTIONS_CREATOR, CONCEPTS_CONTRIBUTOR, CONCEPT_CONTRIBUTOR, INDICATOR_CONTRIBUTOR, SERIES_CONTRIBUTOR].map(role => ({ value: role, label: role }));
+const options = [
+	ADMIN,
+	CONCEPTS_CREATOR,
+	COLLECTIONS_CREATOR,
+	CONCEPTS_CONTRIBUTOR,
+	CONCEPT_CONTRIBUTOR,
+	INDICATOR_CONTRIBUTOR,
+	SERIES_CONTRIBUTOR,
+].map((role) => ({ value: role, label: role }));
 
 function install(store) {
 	import('./dev-tools.scss');
-	console.log(store.getState())
-	window.devToolsEnabled = true
+	window.devToolsEnabled = true;
 	// load local dev tools if it's there
 	// NOTE: this is using some webpack-sepecific features.
 	// if you're not using webpack, you might consider using
@@ -21,35 +34,45 @@ function install(store) {
 	const requireDevToolsLocal = require.context(
 		'./',
 		false,
-		/dev-tools\.local\.js/,
-	)
-	const local = requireDevToolsLocal.keys()[0]
-	let LocalDevTools
+		/dev-tools\.local\.js/
+	);
+	const local = requireDevToolsLocal.keys()[0];
+	let LocalDevTools;
 	if (local) {
-		LocalDevTools = requireDevToolsLocal(local).default
+		LocalDevTools = requireDevToolsLocal(local).default;
 	}
-	LocalDevTools = LocalDevTools || (() => null)
+	LocalDevTools = LocalDevTools || (() => null);
 
 	function AdminTool() {
-		const rolesAndStamps = useSelector(state => state.app.auth.user);
-		const dispatch = useDispatch()
+		const rolesAndStamps = useSelector((state) => state.app.auth.user);
+		const dispatch = useDispatch();
 
-		const changeRoles = value => {
-			dispatch({ type: CHECK_AUTH, payload: {
+		const changeRoles = (value) => {
+			dispatch({
+				type: CHECK_AUTH,
+				payload: {
 					...rolesAndStamps,
-					roles: value.map(({ value }) => value)
-				}})
-		}
+					roles: value.map(({ value }) => value),
+				},
+			});
+		};
 
-		const value = options.filter(({ value }) => rolesAndStamps.roles.indexOf(value) >= 0);
+		const value = options.filter(
+			({ value }) => rolesAndStamps.roles.indexOf(value) >= 0
+		);
 		return (
 			<div>
-				<label style={{ width: '100%'}}>
+				<label style={{ width: '100%' }}>
 					{D.pickedRolePlaceholder}
-					<Select options={options} value={value} multi={true} onChange={value => changeRoles(value)}/>
+					<Select
+						options={options}
+						value={value}
+						multi={true}
+						onChange={(value) => changeRoles(value)}
+					/>
 				</label>
 			</div>
-		)
+		);
 	}
 
 	function DevTools() {
@@ -63,15 +86,14 @@ function install(store) {
 					</div>
 				</div>
 			</Provider>
-
-		)
+		);
 	}
 
 	// add dev tools UI to the page
-	const devToolsRoot = document.createElement('div')
-	document.body.appendChild(devToolsRoot)
+	const devToolsRoot = document.createElement('div');
+	document.body.appendChild(devToolsRoot);
 	const root = createRoot(devToolsRoot);
-	root.render(<DevTools />, devToolsRoot)
+	root.render(<DevTools />, devToolsRoot);
 }
 
-export {install}
+export { install };
--- app/src/dev-tools/load.js
+++ app/src/dev-tools/load.js
@@ -1,7 +1,7 @@
-function load (store, callback){
-	if (process.env.REACT_APP_DEV_TOOLS_ENABLED === 'true') {
+function load(store, configuration, callback) {
+	if (configuration.DEV_TOOLS_ENABLED === true) {
 		import('./dev-tools')
-			.then(mod => mod.install(store))
+			.then((mod) => mod.install(store))
 			.finally(callback);
 	} else {
 		callback();
