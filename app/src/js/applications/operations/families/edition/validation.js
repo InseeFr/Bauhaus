618f2983b1a72411191cf2b3a885a633e681d1e8
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-05 15:28:13 +0100

    move code in a global reusable file

--- app/src/js/applications/operations/families/edition/validation.js
+++ app/src/js/applications/operations/families/edition/validation.js
@@ -1,41 +1,6 @@
 import D, { D1, D2 } from 'js/i18n';
+import { formatValidation } from 'js/utils/validation';
 import { z } from 'zod';
-
-const formatValidation = (ZodObject) => (values) => {
-	const ZodError = ZodObject.safeParse(values);
-
-	const defaultFields = Object.keys(ZodObject.shape).reduce(
-		(acc, key) => ({
-			...acc,
-			[key]: '',
-		}),
-		{}
-	);
-
-	if (ZodError.success) {
-		return {
-			fields: defaultFields,
-			errorMessage: [],
-		};
-	}
-
-	const fields = {
-		...defaultFields,
-		...ZodError.error.issues.reduce(
-		(acc, error) => ({
-			...acc,
-			[error.path]: error.message,
-		}),
-		{}
-	)};
-
-	const errorMessage = ZodError.error.issues.map((error) => error.message);
-
-	return {
-		fields,
-		errorMessage,
-	};
-};
 
 const Family = z.object({
 	prefLabelLg1: z.string().min(1, {message: D.mandatoryProperty(D1.title)}),
---
+++ app/src/js/utils/validation.js
@@ -0,0 +1,37 @@
+
+export const formatValidation = (ZodObject) => (values) => {
+	const ZodError = ZodObject.safeParse(values);
+
+	const defaultFields = Object.keys(ZodObject.shape).reduce(
+		(acc, key) => ({
+			...acc,
+			[key]: '',
+		}),
+		{}
+	);
+
+	if (ZodError.success) {
+		return {
+			fields: defaultFields,
+			errorMessage: [],
+		};
+	}
+
+	const fields = {
+		...defaultFields,
+		...ZodError.error.issues.reduce(
+            (acc, error) => ({
+                ...acc,
+                [error.path]: error.message,
+            }),
+		    {}
+        )
+    };
+
+	const errorMessage = ZodError.error.issues.map((error) => error.message);
+
+	return {
+		fields,
+		errorMessage,
+	};
+};
