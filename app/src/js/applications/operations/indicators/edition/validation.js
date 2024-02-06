74b8746955750c5d598fdb579747fdc5544c0463
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-06 15:58:45 +0100

    solve zod checking if object is defined

--- app/src/js/applications/operations/indicators/edition/validation.js
+++ app/src/js/applications/operations/indicators/edition/validation.js
@@ -5,7 +5,7 @@ import { z } from 'zod';
 const Indicator = z.object({
 	prefLabelLg1: z.string().min(1, {message: D.mandatoryProperty(D1.title)}),
 	prefLabelLg2: z.string().min(1, {message: D.mandatoryProperty(D2.title)}),
-	creators: z.string().array().nonempty({message: D.mandatoryProperty(D.creatorTitle)}),
+	creators: z.string({required_error: D.mandatoryProperty(D.creatorTitle)}).array().nonempty({message: D.mandatoryProperty(D.creatorTitle)}),
 });
 
 export const validate = formatValidation(Indicator)
--- app/src/js/applications/operations/indicators/edition/validation.spec.js
+++ app/src/js/applications/operations/indicators/edition/validation.spec.js
@@ -2,22 +2,22 @@ import { validate } from './validation';
 
 describe('validate', () => {
 	it('should return an error if the prefLabelLg1 is not defined', () => {
-		expect(validate({ prefLabelLg2: 'prefLabelLg2', creators: ['c'] })).toEqual({
+		expect(validate({ prefLabelLg1: '', prefLabelLg2: 'prefLabelLg2', creators: ['c'] })).toEqual({
 			errorMessage: ['The property <strong>Intitulé</strong> is required.'],
 			fields: {
-				creators: "",
+				creators: '',
 				prefLabelLg1: 'The property <strong>Intitulé</strong> is required.',
 				prefLabelLg2: '',
 			},
 		});
 	});
 	it('should return an error if the prefLabelLg2 is not defined', () => {
-		expect(validate({ prefLabelLg1: 'prefLabelLg1', creators: ['c'] })).toEqual({
+		expect(validate({ prefLabelLg1: 'prefLabelLg1', prefLabelLg2: '', creators: ['c'] })).toEqual({
 			errorMessage: ['The property <strong>Title</strong> is required.'],
 			fields: {
-				creators: "",
-				prefLabelLg1: "",
-				prefLabelLg2: "The property <strong>Title</strong> is required.",
+				creators: '',
+				prefLabelLg1: '',
+				prefLabelLg2: 'The property <strong>Title</strong> is required.',
 			},
 		});
 	});
@@ -25,9 +25,9 @@ describe('validate', () => {
 		expect(validate({ prefLabelLg1: 'prefLabelLg1',  prefLabelLg2: 'prefLabelLg2' })).toEqual({
 			errorMessage: ['The property <strong>Owner</strong> is required.'],
 			fields: {
-				creators: "The property <strong>Owner</strong> is required.",
-				prefLabelLg1: "",
-				prefLabelLg2: "",
+				creators: 'The property <strong>Owner</strong> is required.',
+				prefLabelLg1: '',
+				prefLabelLg2: '',
 			},
 		});
 	});
@@ -35,21 +35,21 @@ describe('validate', () => {
 		expect(validate({ prefLabelLg1: 'prefLabelLg1',  prefLabelLg2: 'prefLabelLg2', creators: [] })).toEqual({
 			errorMessage: ['The property <strong>Owner</strong> is required.'],
 			fields: {
-				creators: "The property <strong>Owner</strong> is required.",
-				prefLabelLg1: "",
-				prefLabelLg2: "",
+				creators: 'The property <strong>Owner</strong> is required.',
+				prefLabelLg1: '',
+				prefLabelLg2: '',
 			},
 		});
 	});
 	it('should return nothing if there is no errors', () => {
 		expect(
-			validate({ creators: ['a'], prefLabelLg1: 'prefLabelLg1', prefLabelLg2: 'prefLabelLg2' })
+			validate({ creators: ['c'], prefLabelLg1: 'prefLabelLg1', prefLabelLg2: 'prefLabelLg2' })
 		).toEqual({
 			errorMessage: [],
 			fields: {
-				creators: "",
-				prefLabelLg1: "",
-				prefLabelLg2: "",
+				creators: '',
+				prefLabelLg1: '',
+				prefLabelLg2: '',
 			},
 		});
 	});
