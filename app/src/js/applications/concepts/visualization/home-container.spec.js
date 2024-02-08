865514f86a0cc8035557efe966c02422617e3bf4
Emmanuel <demey.emmanuel@gmail.com>
2024-02-08 09:15:45 +0100

    fix: solve unit test

--- app/src/js/applications/concepts/visualization/home-container.spec.js
+++ app/src/js/applications/concepts/visualization/home-container.spec.js
@@ -1,9 +1,10 @@
-import { findByTestId, fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ConceptVisualizationContainer from './home-container';
 import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import api from '../../../remote-api/concepts-api';
+import * as R from 'bauhaus-utilities/src/auth/roles';
 
 jest.mock('../../../remote-api/concepts-api');
 
@@ -13,28 +14,28 @@ const store = mockStore({
 		auth: {
 			type: 'type',
 			user: {
-				roles: [],
-				stamp: 'stamp'
-			}
+				roles: [R.ADMIN],
+				stamp: 'stamp',
+			},
 		},
 		lg1: 'fr',
-		lg2: 'en'
-	}
+		lg2: 'en',
+	},
 });
 
 describe('ConceptVisualizationContainer', () => {
 	it('should display the component', async () => {
 		api.getConceptGeneral = jest.fn().mockImplementation(() => {
 			return Promise.resolve({
-				conceptVersion: 1
-			})
-		})
+				conceptVersion: 1,
+			});
+		});
 		api.getNoteVersionList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 		api.getConceptLinkList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 		render(
 			<Provider store={store}>
 				<MemoryRouter>
@@ -44,25 +45,24 @@ describe('ConceptVisualizationContainer', () => {
 		);
 
 		expect(api.getConceptGeneral).toHaveBeenCalled();
-
-	})
+	});
 
 	it('should publish the component', async () => {
 		api.getConceptGeneral = jest.fn().mockImplementation(() => {
 			return Promise.resolve({
-				conceptVersion: 1
-			})
-		})
+				conceptVersion: 1,
+			});
+		});
 		api.getNoteVersionList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 		api.getConceptLinkList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 
 		api.putConceptValidList = jest.fn().mockImplementation(() => {
-			return Promise.resolve()
-		})
+			return Promise.resolve();
+		});
 
 		render(
 			<Provider store={store}>
@@ -70,30 +70,30 @@ describe('ConceptVisualizationContainer', () => {
 					<ConceptVisualizationContainer />
 				</MemoryRouter>
 			</Provider>
-		)
+		);
 
-		const publish = await screen.findByText("Publish")
+		const publish = await screen.findByText('Publish');
 		fireEvent.click(publish);
 
 		expect(api.putConceptValidList).toHaveBeenCalled();
-	})
+	});
 
 	it('should delete the component', async () => {
 		api.getConceptGeneral = jest.fn().mockImplementation(() => {
 			return Promise.resolve({
-				conceptVersion: 1
-			})
-		})
+				conceptVersion: 1,
+			});
+		});
 		api.getNoteVersionList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 		api.getConceptLinkList = jest.fn().mockImplementation(() => {
-			return Promise.resolve([])
-		})
+			return Promise.resolve([]);
+		});
 
 		api.deleteConcept = jest.fn().mockImplementation(() => {
-			return Promise.resolve()
-		})
+			return Promise.resolve();
+		});
 
 		render(
 			<Provider store={store}>
@@ -101,15 +101,14 @@ describe('ConceptVisualizationContainer', () => {
 					<ConceptVisualizationContainer />
 				</MemoryRouter>
 			</Provider>
-		)
+		);
 
-		const deleteButton = await screen.findByText("Delete")
+		const deleteButton = await screen.findByText('Delete');
 		fireEvent.click(deleteButton);
 
-		const yesButton = await screen.findByText("Yes")
+		const yesButton = await screen.findByText('Yes');
 		fireEvent.click(yesButton);
 
-
 		expect(api.deleteConcept).toHaveBeenCalled();
-	})
-})
+	});
+});
