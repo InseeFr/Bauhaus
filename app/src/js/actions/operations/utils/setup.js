43ee2fd30ee466140a58472466ca5a19e7e9753e
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-09 10:35:12 +0100

    undelete files

--- app/src/js/actions/operations/utils/setup.js
+++ app/src/js/actions/operations/utils/setup.js
@@ -1,11 +1,37 @@
 import apiOrganisations from 'js/remote-api/organisations-api';
+import { CL_SOURCE_CATEGORY, CL_FREQ } from 'js/actions/constants/codeList';
 import loadStampList from 'js/actions/stamp';
 import * as A from 'js/actions/constants';
+import { CodesList } from 'bauhaus-utilities';
 
 export const loadSetup = () => (dispatch) => {
+	loadCodesList([CL_SOURCE_CATEGORY, CL_FREQ], dispatch);
 	loadOrganisations(dispatch);
 	loadStampList()(dispatch);
 };
+
+export function loadCodesList(notations, dispatch) {
+	notations.forEach((notation) => {
+		return Promise.all([
+			CodesList.getCodesList(notation),
+			CodesList.getCodesListCodes(notation, 1, 0),
+		]).then(
+			([codesList, codes]) =>
+				dispatch({
+					type: A.LOAD_OPERATIONS_CODES_LIST_SUCCESS,
+					payload: {
+						codes: codes.items ?? [],
+						...codesList,
+					},
+				}),
+			(err) =>
+				dispatch({
+					type: A.LOAD_OPERATIONS_CODES_LIST_FAILURE,
+					payload: { err },
+				})
+		);
+	});
+}
 
 export function loadOrganisations(dispatch) {
 	return apiOrganisations.getOrganisations().then(
