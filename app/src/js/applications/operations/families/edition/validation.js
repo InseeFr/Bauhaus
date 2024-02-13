d3eb41d1e5774853e113a83824e9ebee194b0284
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-13 14:16:03 +0100

    zod on families

--- app/src/js/applications/operations/families/edition/validation.js
+++ app/src/js/applications/operations/families/edition/validation.js
@@ -1,19 +1,28 @@
-import D, {D1, D2} from 'js/i18n';
+import D, { D1, D2 } from 'js/i18n';
+import { formatValidation } from 'js/utils/validation';
+import { z } from 'zod';
 
-export function validate({ prefLabelLg1, prefLabelLg2 }) {
+// export function validate({ prefLabelLg1, prefLabelLg2 }) {
 
-	const errorMessages = [];
-	if(!prefLabelLg1){
-		errorMessages.push(D.mandatoryProperty(D1.title))
-	}
-	if(!prefLabelLg2){
-		errorMessages.push(D.mandatoryProperty(D2.title))
-	}
-	return {
-		fields: {
-			prefLabelLg1: !prefLabelLg1 ? D.mandatoryProperty(D1.title) : '',
-			prefLabelLg2: !prefLabelLg2 ? D.mandatoryProperty(D2.title) : '',
-		},
-		errorMessage: errorMessages
-	};
-}
+// 	const errorMessages = [];
+// 	if(!prefLabelLg1){
+// 		errorMessages.push(D.mandatoryProperty(D1.title))
+// 	}
+// 	if(!prefLabelLg2){
+// 		errorMessages.push(D.mandatoryProperty(D2.title))
+// 	}
+// 	return {
+// 		fields: {
+// 			prefLabelLg1: !prefLabelLg1 ? D.mandatoryProperty(D1.title) : '',
+// 			prefLabelLg2: !prefLabelLg2 ? D.mandatoryProperty(D2.title) : '',
+// 		},
+// 		errorMessage: errorMessages
+// 	};
+// }
+
+const Family = z.object({
+	prefLabelLg1: z.string().min(1, {message: D.mandatoryProperty(D1.title)}),
+	prefLabelLg2: z.string().min(1, {message: D.mandatoryProperty(D2.title)}),
+});
+
+export const validate = formatValidation(Family)
