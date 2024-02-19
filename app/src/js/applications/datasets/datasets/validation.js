ecfa10a3d2db6a9db41590288e7f4d1676e63a43
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-19 09:50:22 +0100

    reshape

--- app/src/js/applications/datasets/datasets/validation.js
+++ app/src/js/applications/datasets/datasets/validation.js
@@ -3,20 +3,12 @@ import { formatValidation } from 'js/utils/validation';
 import { z } from 'zod';
 
 const Dataset = z.object({
-	labelLg1: z.string({
-        required_error: D.mandatoryProperty(D1.title)
-    }).min(1, {
-        message: D.mandatoryProperty(D1.title)
-    }),
-	labelLg2: z.string({
-        required_error: D.mandatoryProperty(D2.title)
-    }).min(1, {
-        message: D.mandatoryProperty(D2.title)
-    }),
-	creator: z.string({required_error: D.mandatoryProperty(D1.creatorTitle)}),
-	contributor: z.string({required_error: D.mandatoryProperty(D1.contributorTitle)}),
-	disseminationStatus: z.string({required_error: D.mandatoryProperty(D1.disseminationStatusTitle)}),
-	idSerie: z.string({required_error: D.mandatoryProperty(D1.generatedBy)}),
+	labelLg1: z.string({ required_error: D.mandatoryProperty(D1.title) }).min(1, { message: D.mandatoryProperty(D1.title) }),
+	labelLg2: z.string({ required_error: D.mandatoryProperty(D2.title) }).min(1, { message: D.mandatoryProperty(D2.title) }),
+	creator: z.string({ required_error: D.mandatoryProperty(D1.creatorTitle) }),
+	contributor: z.string({ required_error: D.mandatoryProperty(D1.contributorTitle) }),
+	disseminationStatus: z.string({ required_error: D.mandatoryProperty(D1.disseminationStatusTitle) }),
+	idSerie: z.string({ required_error: D.mandatoryProperty(D1.generatedBy) }),
 });
 
 export const validate = ({catalogRecord, ...otherFields}) => formatValidation(Dataset)({creator: catalogRecord?.creator, contributor: catalogRecord?.contributor, ...otherFields});
