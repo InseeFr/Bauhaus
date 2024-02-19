1933d07c828db435fe5d00e92e094af5e7af9b0e
Emmanuel <demey.emmanuel@gmail.com>
2024-02-19 10:38:07 +0100

    feat: display codelist detail menu only for admin

--- packages/codelists/src/menu/index.js
+++ packages/codelists/src/menu/index.js
@@ -2,21 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Menu } from '@inseefr/wilco';
 import D from '../i18n/build-dictionary';
+import { Auth } from 'bauhaus-utilities';
+import { useSelector } from 'react-redux';
+
 const defaultAttrs = { 'aria-current': 'page' };
 
 const MenuCodelists = ({ location }) => {
+	const permission = useSelector((state) => Auth.getPermission(state));
+
 	const activePath = location.pathname;
 	if (activePath === '/') return null;
 
 	const paths = [
-		{
-			path: '/codelists-partial',
-			pathKey: 'codelists-partial',
-			className: null,
-			attrs: null,
-			label: D.codelistsPartialTitle,
-			order: 2,
-		},
 		{
 			path: '/codelists',
 			pathKey: 'codelists',
@@ -26,6 +23,17 @@ const MenuCodelists = ({ location }) => {
 			order: 1,
 		},
 	];
+
+	if (permission.roles.includes(Auth.ADMIN)) {
+		paths.push({
+			path: '/codelists-partial',
+			pathKey: 'codelists-partial',
+			className: null,
+			attrs: null,
+			label: D.codelistsPartialTitle,
+			order: 2,
+		});
+	}
 
 	const currentPath = paths.find((path) => {
 		return location.pathname.includes(path.pathKey);
