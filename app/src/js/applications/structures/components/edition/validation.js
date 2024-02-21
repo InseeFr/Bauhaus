072d8d5ef63025e93da98edb4d20d3df2d8b3eff
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-02-21 11:46:48 +0100

    minor fix

--- app/src/js/applications/structures/components/edition/validation.js
+++ app/src/js/applications/structures/components/edition/validation.js
@@ -2,33 +2,6 @@ import D, { D1, D2 } from 'js/i18n';
 import { formatValidation } from 'js/utils/validation';
 import { z } from 'zod';
 
-export const validate2 = (component) => {
-    const errorMessage = [];
-    const fields = {};
-
-    if  (!component.identifiant)  {
-        errorMessage.push(D.mandatoryProperty(D.idTitle));
-        fields.identifiant = D.mandatoryProperty(D.idTitle);
-    }
-    if  (!component.labelLg1)  {
-        errorMessage.push(D.mandatoryProperty(D1.labelTitle));
-        fields.labelLg1 = D.mandatoryProperty(D1.labelTitle);
-    }
-    if  (!component.labelLg2)  {
-        errorMessage.push(D.mandatoryProperty(D2.labelTitle));
-        fields.labelLg2 = D.mandatoryProperty(D2.labelTitle);
-    }
-    if  (!component.type)  {
-        errorMessage.push(D.mandatoryProperty(D1.type));
-        fields.type = D.mandatoryProperty(D.type);
-    }
-
-    return {
-        errorMessage,
-        fields,
-    };
-};
-
 const Component = z.object({
     identifiant: z.string({ required_error: D.mandatoryProperty(D.idTitle) }).min(1, { message: D.mandatoryProperty(D.idTitle) }),
     labelLg1: z.string({ required_error: D.mandatoryProperty(D1.labelTitle) }).min(1, { message: D.mandatoryProperty(D1.labelTitle) }),
