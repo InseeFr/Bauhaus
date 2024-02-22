fed52922de2309512582fc79d27af93e6628ab55
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-02-22 16:43:28 +0100

    feat: improve select component (#648)

--- packages/utilities/src/components/select-rmes/index.js
+++ packages/utilities/src/components/select-rmes/index.js
@@ -6,58 +6,33 @@ import 'react-select/dist/react-select.css';
 import './select-rmes.scss';
 
 function SelectRmes({
-	value,
-	placeholder,
-	options,
 	onChange,
 	unclearable,
-	searchable,
 	multi,
-	disabled,
+	...props
 }) {
-	const isClearable = unclearable ? false : true;
 	const onChangeSelect = multi
 		? e => onChange(e)
 		: e => onChange(e ? e.value : '');
 	return (
 		<Select
-			name="hello"
-			value={value}
-			placeholder={placeholder}
-			options={options}
+			{...props}
 			onChange={onChangeSelect}
-			clearable={isClearable}
-			searchable={searchable}
+			clearable={!unclearable}
 			noResultsText={D.noResult}
-			multi={multi}
-			disabled={disabled}
 		/>
 	);
 }
 
 SelectRmes.defaultProps = {
 	multi: false,
-	clearable: false,
 	searchable: true,
 	disabled: false,
-	name: '',
 };
 
 SelectRmes.propTypes = {
-	value: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
-	placeholder: PropTypes.string.isRequired,
-	options: PropTypes.arrayOf(
-		PropTypes.shape({
-			value: PropTypes.string.isRequired,
-			label: PropTypes.string.isRequired,
-		})
-	).isRequired,
 	onChange: PropTypes.func.isRequired,
-	clearable: PropTypes.bool,
-	searchable: PropTypes.bool,
-	creatable: PropTypes.bool,
-	disabled: PropTypes.bool,
-	name: PropTypes.string,
+	unclearable: PropTypes.bool,
 };
 
 export default SelectRmes;
