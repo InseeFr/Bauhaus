3c8c07ca34f1c6e68061d1d741c304280f604336
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-02-28 19:37:14 +0100

    feat: be able to use [ ] inside a rich text input (#667)

---
+++ packages/utilities/src/utils/draftjs/mdToDraftjs.spec.js
@@ -0,0 +1,48 @@
+import { mdToDraftjs } from './mdToDraftjs';
+
+describe('mdToDraftjs', () => {
+	it('should manage [ ] without content without styles', () => {
+		expect(mdToDraftjs('content [1]')).toEqual({
+				'blocks': [
+					{
+						'depth': 0,
+						'entityRanges': [],
+						'inlineStyleRanges': [],
+						'text': 'content [1]',
+						'type': 'unstyled',
+					},
+				],
+				'entityMap': {
+					'data': '',
+					'mutability': '',
+					'type': '',
+				},
+			},
+		);
+	});
+
+	it('should manage [ ] without content with styles', () => {
+		expect(mdToDraftjs('content [*1*]')).toEqual({
+				'blocks': [
+					{
+						'depth': 0,
+						'entityRanges': [],
+						'inlineStyleRanges':  [{
+							"length": 1,
+			        "offset": 9,
+			        "style": "ITALIC",
+						},
+		       ],
+						'text': 'content [1]',
+						'type': 'unstyled',
+					},
+				],
+				'entityMap': {
+					'data': '',
+					'mutability': '',
+					'type': '',
+				},
+			},
+		);
+	});
+});
