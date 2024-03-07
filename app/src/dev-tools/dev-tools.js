c0837171fdc0fdd87f66c171916bc24f2b5706b0
Pierre Vasseur <132562606+PierreVasseur@users.noreply.github.com>
2024-03-07 08:16:01 +0100

    add missing roles in toolbox (#687)

--- app/src/dev-tools/dev-tools.js
+++ app/src/dev-tools/dev-tools.js
@@ -1,18 +1,38 @@
-import React  from 'react';
+import React from 'react';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import {
-	ADMIN, COLLECTIONS_CREATOR, CONCEPT_CONTRIBUTOR, CONCEPTS_CONTRIBUTOR, CONCEPTS_CREATOR, INDICATOR_CONTRIBUTOR, SERIES_CONTRIBUTOR
+	ADMIN,
+	COLLECTIONS_CREATOR,
+	CONCEPT_CONTRIBUTOR,
+	CONCEPTS_CONTRIBUTOR,
+	CONCEPTS_CREATOR,
+	INDICATOR_CONTRIBUTOR,
+	SERIES_CONTRIBUTOR,
+	DATASET_CONTRIBUTOR,
+	CODELIST_CONTRIBUTOR,
+	STRUCTURE_CONTRIBUTOR,
 } from 'bauhaus-utilities/src/auth/roles';
 import Select from 'react-select';
 import D from 'js/i18n';
 import { createRoot } from 'react-dom/client';
 import { CHECK_AUTH } from '../js/store/users';
 
-const options = [ADMIN, CONCEPTS_CREATOR, COLLECTIONS_CREATOR, CONCEPTS_CONTRIBUTOR, CONCEPT_CONTRIBUTOR, INDICATOR_CONTRIBUTOR, SERIES_CONTRIBUTOR].map(role => ({ value: role, label: role }));
+const options = [
+	ADMIN,
+	CONCEPTS_CREATOR,
+	COLLECTIONS_CREATOR,
+	CONCEPTS_CONTRIBUTOR,
+	CONCEPT_CONTRIBUTOR,
+	INDICATOR_CONTRIBUTOR,
+	SERIES_CONTRIBUTOR,
+	DATASET_CONTRIBUTOR,
+	CODELIST_CONTRIBUTOR,
+	STRUCTURE_CONTRIBUTOR,
+].map((role) => ({ value: role, label: role }));
 
 function install(store) {
 	import('./dev-tools.scss');
-	window.devToolsEnabled = true
+	window.devToolsEnabled = true;
 	// load local dev tools if it's there
 	// NOTE: this is using some webpack-sepecific features.
 	// if you're not using webpack, you might consider using
@@ -20,35 +40,45 @@ function install(store) {
 	const requireDevToolsLocal = require.context(
 		'./',
 		false,
-		/dev-tools\.local\.js/,
-	)
-	const local = requireDevToolsLocal.keys()[0]
-	let LocalDevTools
+		/dev-tools\.local\.js/
+	);
+	const local = requireDevToolsLocal.keys()[0];
+	let LocalDevTools;
 	if (local) {
-		LocalDevTools = requireDevToolsLocal(local).default
+		LocalDevTools = requireDevToolsLocal(local).default;
 	}
-	LocalDevTools = LocalDevTools || (() => null)
+	LocalDevTools = LocalDevTools || (() => null);
 
 	function AdminTool() {
-		const rolesAndStamps = useSelector(state => state.app.auth.user);
-		const dispatch = useDispatch()
+		const rolesAndStamps = useSelector((state) => state.app.auth.user);
+		const dispatch = useDispatch();
 
-		const changeRoles = value => {
-			dispatch({ type: CHECK_AUTH, payload: {
+		const changeRoles = (value) => {
+			dispatch({
+				type: CHECK_AUTH,
+				payload: {
 					...rolesAndStamps,
-					roles: value.map(({ value }) => value)
-				}})
-		}
+					roles: value.map(({ value }) => value),
+				},
+			});
+		};
 
-		const value = options.filter(({ value }) => rolesAndStamps.roles.indexOf(value) >= 0);
+		const value = options.filter(
+			({ value }) => rolesAndStamps.roles.indexOf(value) >= 0
+		);
 		return (
 			<div>
-				<label style={{ width: '100%'}}>
+				<label style={{ width: '100%' }}>
 					{D.pickedRolePlaceholder}
-					<Select options={options} value={value} multi={true} onChange={value => changeRoles(value)}/>
+					<Select
+						options={options}
+						value={value}
+						multi={true}
+						onChange={(value) => changeRoles(value)}
+					/>
 				</label>
 			</div>
-		)
+		);
 	}
 
 	function DevTools() {
@@ -62,15 +92,14 @@ function install(store) {
 					</div>
 				</div>
 			</Provider>
-
-		)
+		);
 	}
 
 	// add dev tools UI to the page
-	const devToolsRoot = document.createElement('div')
-	document.body.appendChild(devToolsRoot)
+	const devToolsRoot = document.createElement('div');
+	document.body.appendChild(devToolsRoot);
 	const root = createRoot(devToolsRoot);
-	root.render(<DevTools />)
+	root.render(<DevTools />);
 }
 
-export {install}
+export { install };
