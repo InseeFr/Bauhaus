651ec8f2792e8941405bc30787d1de423cde18aa
Emmanuel <demey.emmanuel@gmail.com>
2024-05-08 20:42:43 +0200

    fix: rewrite Tabs Component

--- app/src/js/applications/shared/tabs/index.js
+++ app/src/js/applications/shared/tabs/index.js
@@ -1,34 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 import './tabs.scss';
 
-class TabsRmes extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { activeTab: 1 };
-		this.selectTab = tabIndex =>
-			this.setState({
-				activeTab: tabIndex,
-			});
-	}
-
-	render() {
-		const { tabs } = this.props;
-		return (
-			<Tabs
-				defaultActiveKey={1}
-				id="informationToManage"
-				onSelect={this.selectTab}
-				justified
-			>
-				{tabs.map(({ id, title, content }) => (
-					<Tab eventKey={id} key={id} title={title} className="tabs-items">
-						{content}
-					</Tab>
-				))}
-			</Tabs>
-		);
-	}
-}
-
-export default TabsRmes;
+export const TabsRmes = ({ tabs }) => {
+	return (
+		<Tabs defaultActiveKey={1} id="informationToManage" justified>
+			{tabs.map(({ id, title, content }) => (
+				<Tab eventKey={id} key={id} title={title} className="tabs-items">
+					{content}
+				</Tab>
+			))}
+		</Tabs>
+	);
+};
