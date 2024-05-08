66cf1d8e25555d58a862c2a80a4ee8e3fc457ae4
Emmanuel <demey.emmanuel@gmail.com>
2024-05-08 20:23:54 +0200

    feat: rewrite concept compare control component

--- app/src/js/applications/concepts/compare/controls.js
+++ app/src/js/applications/concepts/compare/controls.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { ReturnButton, ActionToolbar } from '@inseefr/wilco';
 
-class Controls extends Component {
-	render() {
-		const location = this.props.history.location.pathname;
-		const nextLocation = location.replace('/compare', '');
-		return (
-			<ActionToolbar>
-				<ReturnButton action={nextLocation} col={3} />
-			</ActionToolbar>
-		);
-	}
-}
+const Controls = () => {
+	const location = useLocation();
+	const nextLocation = location.pathname.replace('/compare', '');
+	return (
+		<ActionToolbar>
+			<ReturnButton action={nextLocation} col={3} />
+		</ActionToolbar>
+	);
+};
 
-export default withRouter(Controls);
+export default Controls;
