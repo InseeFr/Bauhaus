cd4010afad5f1aff84b0d27f24cd3d2de1847b90
Vasseur Pierre <pierre.vasseur@insee.fr>
2024-05-17 18:08:12 +0200

    add lg2 in vizu

--- app/src/js/applications/operations/msd/pages/sims-visualisation/sims-block-codelist.js
+++ app/src/js/applications/operations/msd/pages/sims-visualisation/sims-block-codelist.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const SimsBlockCodeList = ({ codesLists, currentSection, multi = false }) => {
+const SimsBlockCodeList = ({
+	codesLists,
+	currentSection,
+	multi = false,
+	isSecondLang,
+}) => {
 	const codes = codesLists[currentSection.codeList].codes;
 
 	if (multi) {
@@ -9,20 +14,22 @@ const SimsBlockCodeList = ({ codesLists, currentSection, multi = false }) => {
 			: [currentSection.value];
 
 		// If the list of codes only contain one item, we do not display a list
-		if(value.length === 1){
-			return <>{codes.find(({code}) => code === value[0])?.labelLg1}</>
+		const foundCode = codes.find(({ code }) => code === value[0]) ?? '';
+		if (value.length === 1) {
+			return <>{isSecondLang ? foundCode.labelLg2 : foundCode.labelLg1}</>;
 		}
 		return (
 			<ul>
 				{codes
 					.filter(({ code }) => value.includes(code))
 					.map((code, index) => (
-						<li key={index}>{code.labelLg1}</li>
+						<li key={index}>{isSecondLang ? code.labelLg2 : code.labelLg1}</li>
 					))}
 			</ul>
 		);
 	}
-	return <>{codes.find(code => code.code === currentSection.value).labelLg1}</>;
+	const foundCode = codes.find((code) => code.code === currentSection.value);
+	return <>{isSecondLang ? foundCode.labelLg2 : foundCode.labelLg1}</>;
 };
 
 export default SimsBlockCodeList;
