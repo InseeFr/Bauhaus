0872ecd65baf39641e3d0a35e7db647a3ac797dc
Emmanuel <demey.emmanuel@gmail.com>
2024-07-12 09:42:46 +0200

    feat: improve error message when downloading message

---
+++ src/js/new-architecture/utils/files.ts
@@ -0,0 +1,26 @@
+import FileSaver from 'file-saver';
+
+const CONTENT_DISPOSITION_FILENAME_REGEXP =
+	/filename[^;\n=]*="((['"]).*?\2|[^;\n]*)"/;
+
+export const saveFileFromHttpResponse = (response: Response) => {
+	const contentDisposition = response.headers.get('Content-Disposition');
+
+	if (contentDisposition === null) {
+		console.error(
+			'Unable to download the File due to a missing Content-Disposition header'
+		);
+		return Promise.reject();
+	}
+
+	const matches = contentDisposition.match(CONTENT_DISPOSITION_FILENAME_REGEXP);
+
+	if (matches === null) {
+		console.error('Unable to parse the Content-Disposition header');
+		return Promise.reject();
+	}
+
+	const fileName = matches[1];
+
+	return response.blob().then((blob) => FileSaver.saveAs(blob, fileName));
+};
