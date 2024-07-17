1b1eff9ce54f94d663a692fbfeedf2470dc3c9f2
Emmanuel <demey.emmanuel@gmail.com>
2024-07-17 10:21:01 +0200

    fix: review

--- src/js/applications/concepts/edition-creation/general.spec.js
+++ src/js/applications/concepts/edition-creation/general.spec.js
@@ -1,26 +1,18 @@
 import { render } from '@testing-library/react';
 import ConceptGeneral from './general';
 import { empty } from '../../../utils/concepts/general';
-import { Provider } from 'react-redux';
-import configureStore from '../../../store/configure-store';
 
-const store = configureStore({
-	disseminationStatus: {
-		results: [{ url: 'url', label: 'label' }],
-	},
-});
+jest.mock('../../../utils/dissemination-status/disseminationStatus');
 
 describe('concept-edition-creation-general', () => {
 	it('renders without crashing', () => {
 		render(
-			<Provider store={store}>
-				<ConceptGeneral
-					general={empty()}
-					stampList={[]}
-					handleChange={jest.fn()}
-					langs={{ lg1: 'fr', lg2: 'en' }}
-				/>
-			</Provider>
+			<ConceptGeneral
+				general={empty()}
+				stampList={[]}
+				handleChange={jest.fn()}
+				langs={{ lg1: 'fr', lg2: 'en' }}
+			/>
 		);
 	});
 });
--- src/js/applications/concepts/edition-creation/home.spec.js
+++ src/js/applications/concepts/edition-creation/home.spec.js
@@ -1,31 +1,23 @@
 import ConceptEditionCreation from './home';
 import { empty } from '../../../utils/concepts/general';
-import { Provider } from 'react-redux';
-import configureStore from '../../../store/configure-store';
 import { renderWithRouter } from '../../../new-architecture/tests-utils/render';
 
-const store = configureStore({
-	disseminationStatus: {
-		results: [{ url: 'url', label: 'label' }],
-	},
-});
+jest.mock('./general');
 
 describe('concept-edition-creation', () => {
 	it('renders without crashing', () => {
 		renderWithRouter(
-			<Provider store={store}>
-				<ConceptEditionCreation
-					id="id"
-					creation={true}
-					title="title"
-					general={empty()}
-					notes={{}}
-					conceptsWithLinks={[]}
-					stampList={[]}
-					save={jest.fn()}
-					langs={{ lg1: 'fr', lg2: 'en' }}
-				/>
-			</Provider>
+			<ConceptEditionCreation
+				id="id"
+				creation={true}
+				title="title"
+				general={empty()}
+				notes={{}}
+				conceptsWithLinks={[]}
+				stampList={[]}
+				save={jest.fn()}
+				langs={{ lg1: 'fr', lg2: 'en' }}
+			/>
 		);
 	});
 });
