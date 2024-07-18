44c4d8e490a642635dec4a7309f95bcace94a1e3
Emmanuel <demey.emmanuel@gmail.com>
2024-07-18 15:35:10 +0200

    fix: remove unused import

--- src/js/new-architecture/utils/hooks/series.ts
+++ src/js/new-architecture/utils/hooks/series.ts
@@ -1,8 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import operationSeries from '../../../remote-api/operations-api';
 import { Series } from '../../model/Series';
-import { transformModelToSelectOptions } from '../transformer';
-import { Options } from '../../model/SelectOption';
 
 export const useSeries = () => {
 	return useQuery({
