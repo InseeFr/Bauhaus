cacbe91db835c060d96b5727aaa3601e27941576
Emmanuel <demey.emmanuel@gmail.com>
2024-07-18 15:36:35 +0200

    feat: add model for series and operations

---
+++ src/js/new-architecture/model/Operation.ts
@@ -0,0 +1,10 @@
+/**
+ * Type used when fetching all Series
+ */
+export type Operation = {
+	id: string;
+	label: string;
+	altLabel: string;
+	iri: string;
+	seriesIri: string;
+};
---
+++ src/js/new-architecture/model/Series.ts
@@ -0,0 +1,9 @@
+/**
+ * Type used when fetching all Series
+ */
+export type Series = {
+	id: string;
+	label: string;
+	altLabel: string;
+	iri: string;
+};
