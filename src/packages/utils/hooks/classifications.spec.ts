feec9b6b40db509441a921d8c8e95d5e7e10b1d6
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2024-08-21 18:05:08 +0200

    feat: migrate 2 components to TypeScript (#892)
    * feat: migrate 2 compoents
    
    * fix: solve typescript issues
    
    * fix: remove fit
    
    * feat: update sonar coverage
    
    * feat: try delting sonar.sources
    
    * feat: try delting sonar.sources
    
    * feat: try delting sonar.sources

---
+++ src/packages/utils/hooks/classifications.spec.ts
@@ -0,0 +1,20 @@
+import { useClassifications } from './classifications';
+import { renderHook } from '@testing-library/react';
+
+jest.mock('@tanstack/react-query', () => ({
+	useQuery: jest.fn(),
+}));
+
+describe('useClassifications', () => {
+	it('should call useQuery', () => {
+		require('@tanstack/react-query').useQuery.mockReturnValue({
+			isLoading: true,
+			data: ['data'],
+		});
+
+		const { result } = renderHook(() => useClassifications());
+
+		expect(result.current.isLoading).toBe(true);
+		expect(result.current.data).toEqual(['data']);
+	});
+});
