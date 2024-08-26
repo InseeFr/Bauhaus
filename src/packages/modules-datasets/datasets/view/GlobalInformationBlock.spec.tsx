b41f0a0263c8d89609861e860c63b142e70d4c8e
Emmanuel <demey.emmanuel@gmail.com>
2024-08-26 20:20:05 +0100

    feat: add unit test

---
+++ src/packages/modules-datasets/datasets/view/GlobalInformationBlock.spec.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { GlobalInformationBlock } from './GlobalInformationBlock';
+import * as useThemesHook from '../useThemes';
+import * as useOrganizationsHook from '../../../utils/hooks/organizations';
+import * as useCodesListHook from '../../../utils/hooks/codeslist';
+import { Dataset } from '../../../model/Dataset';
+import { Organization } from '../../../model/organization';
+import { CodesList } from '../../../model/CodesList';
+
+jest.mock('../useThemes');
+jest.mock('../../../utils/hooks/organizations');
+jest.mock('../../../utils/hooks/codeslist');
+
+jest.mock('./wasGeneratedByBlock', () => ({
+	WasGeneratedByBlock: () => <div>Mocked WasGeneratedByBlock</div>,
+}));
+
+describe('GlobalInformationBlock', () => {
+	const mockDataset = {
+		catalogRecord: {
+			created: '2021-01-01',
+			updated: '2022-01-01',
+		},
+		issued: '2021-06-01',
+		accessRights: 'public',
+		accrualPeriodicity: 'annual',
+		confidentialityStatus: 'open',
+		creators: ['org1', 'org2'],
+		publisher: 'org3',
+		wasGeneratedIRIs: ['iri1', 'iri2'],
+		themes: ['theme1', 'theme2'],
+		keywords: {
+			lg1: ['keyword1', 'keyword2'],
+			lg2: ['keyword3', 'keyword4'],
+		},
+	} as unknown as Dataset;
+
+	beforeEach(() => {
+		jest.spyOn(useThemesHook, 'useThemes').mockReturnValue({
+			data: [
+				{ value: 'theme1', label: 'Theme 1' },
+				{ value: 'theme2', label: 'Theme 2' },
+			],
+		} as any);
+
+		jest.spyOn(useOrganizationsHook, 'useOrganizations').mockReturnValue({
+			data: [
+				{ id: 'org1', label: 'Organization 1' },
+				{ id: 'org2', label: 'Organization 2' },
+				{ id: 'org3', label: 'Organization 3' },
+			],
+		} as any);
+
+		jest
+			.spyOn(useCodesListHook, 'useCodesList')
+			.mockImplementation((notation) => {
+				switch (notation) {
+					case 'CL_ACCESS_RIGHTS':
+						return {
+							codes: [
+								{ iri: 'CL_ACCESS_RIGHTS', labelLg1: 'CL_ACCESS_RIGHTS' },
+							],
+						} as CodesList;
+					case 'CL_FREQ':
+						return {
+							codes: [{ iri: 'CL_FREQ', labelLg1: 'CL_FREQ' }],
+						} as CodesList;
+					case 'CL_CONF_STATUS':
+						return {
+							codes: [{ iri: 'CL_CONF_STATUS', labelLg1: 'CL_CONF_STATUS' }],
+						} as CodesList;
+					default:
+						return {} as CodesList;
+				}
+			});
+	});
+
+	it('should render the GlobalInformationBlock with correct data', () => {
+		render(<GlobalInformationBlock dataset={mockDataset} />);
+
+		screen.getByText(/Creation Date/i);
+		screen.getByText(/01\/01\/2021/i);
+		screen.getByText(/Modification Date/i);
+		screen.getByText(/01\/01\/2022/i);
+		screen.getByText(/Date of the first diffusion of the data/i);
+		screen.getByText(/06\/01\/2021/i);
+	});
+
+	it('should return null if organizations data is not available', () => {
+		jest
+			.spyOn(useOrganizationsHook, 'useOrganizations')
+			.mockReturnValue({ data: undefined } as any);
+
+		const { container } = render(
+			<GlobalInformationBlock dataset={mockDataset} />
+		);
+
+		expect(container.firstChild).toBeNull();
+	});
+});
