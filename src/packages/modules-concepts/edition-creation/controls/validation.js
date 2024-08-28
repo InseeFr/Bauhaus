109f4ed4873c9fd4fb5606a6eeb2891891ced866
Vasseur Pierre <pierre.vasseur@insee.fr>
2024-08-28 18:34:35 +0200

    unfinished

--- src/packages/modules-concepts/edition-creation/controls/validation.js
+++ src/packages/modules-concepts/edition-creation/controls/validation.js
@@ -1,5 +1,84 @@
 import { htmlIsEmpty, htmlLength } from '../../../utils/html-utils';
 import D, { D1 } from '../../../deprecated-locales';
+import { z } from 'zod';
+import { formatValidation } from '../../../utils/validation';
+
+const Concept = (oldLabelLg1, conceptsWithLinks, maxLengthScopeNote) =>
+	z.object({
+		prefLabelLg1: z
+			.string({ required_error: D.mandatoryProperty(D1.labelTitle) })
+			.trim()
+			.min(1, { message: D.mandatoryProperty(D1.labelTitle) })
+			.refine(
+				(value) =>
+					value === oldLabelLg1 ||
+					!conceptsWithLinks.map((concept) => concept.label).includes(value),
+
+				{ message: D.duplicatedLabel }
+			),
+		creator: z
+			.string({ required_error: D.mandatoryProperty(D.creatorTitle) })
+			.min(1, { message: D.mandatoryProperty(D.creatorTitle) }),
+
+		scopeNoteLg1: z
+			.string()
+			.refine((value) => htmlLength(value) <= maxLengthScopeNote, {
+				message: D.tooLongScopeNote(maxLengthScopeNote),
+			}),
+		scopeNoteLg2: z
+			.string()
+			.refine((value) => htmlLength(value) <= maxLengthScopeNote, {
+				message: D.tooLongScopeNote(maxLengthScopeNote),
+			}),
+		definitionLg1: z.string().refine((value) => !htmlIsEmpty(value), {
+			message: D.emptyDefinitionLg1,
+		}),
+	});
+
+const disseminationStatusEnum = z.enum([
+	'Privé',
+	'Public générique',
+	'Public spécifique',
+]);
+
+const ScopeNoteAndDisseminationStatus = z.discriminatedUnion(
+	'disseminationStatus',
+	[
+		z.object({
+			disseminationStatus: disseminationStatusEnum.extract(['Privé']),
+			scopeNoteLg1: z.string().refine((value) => !htmlIsEmpty(value), {
+				message: D.emptyScopeNoteLg1,
+			}),
+		}),
+		z.object({
+			disseminationStatus: disseminationStatusEnum.extract([
+				'Public générique',
+				'Public spécifique',
+			]),
+			scopeNoteLg1: z
+				.string()
+				.refine((value) => !htmlIsEmpty(value), {
+					message: D.emptyScopeNoteLg1,
+				})
+				.optional(),
+		}),
+	]
+);
+
+// disseminationStatus: z.string({ required_error: D.mandatoryProperty(D.disseminationStatusTitle) }).min(1, { message: D.mandatoryProperty(D.disseminationStatusTitle) })
+
+export const validate = (
+	general,
+	notes,
+	oldLabelLg1,
+	conceptsWithLinks,
+	maxLengthScopeNote
+) =>
+	formatValidation(
+		Concept(oldLabelLg1, conceptsWithLinks, maxLengthScopeNote).extend(
+			ScopeNoteAndDisseminationStatus
+		)
+	)({ ...general, ...notes });
 
 export const scndWithoutFirst = (first, second) => {
 	return !htmlIsEmpty(second) && htmlIsEmpty(first);
@@ -12,59 +91,61 @@ export const checkPrefLabelLg1Existing = (
 ) => {
 	return (
 		prefLabelLg1 !== initialPrefLabelFr &&
-		concepts.map((concept) => concept.label).indexOf(prefLabelLg1) !== -1
+		concepts.map((concept) => concept.label).includes(prefLabelLg1)
 	);
 };
 
-const validate = (
-	oldGeneral,
-	newGeneral,
+export const validatee = (
+	general,
 	notes,
+	oldLabelLg1,
 	conceptsWithLinks,
 	maxLengthScopeNote
 ) => {
 	const errorMessage = [];
 	const fields = {};
 
-	const isPrefLabelLg1Existing = checkPrefLabelLg1Existing(
-		conceptsWithLinks,
-		newGeneral.prefLabelLg1,
-		oldGeneral.prefLabelLg1
-	);
+	// const isPrefLabelLg1Existing = checkPrefLabelLg1Existing(
+	// 	conceptsWithLinks,
+	// 	general.prefLabelLg1,
+	// 	oldLabelLg1
+	// );
 
-	if (!newGeneral.prefLabelLg1) {
-		errorMessage.push(D.mandatoryProperty(D1.labelTitle));
-		fields.prefLabelLg1 = D.mandatoryProperty(D1.labelTitle);
-	}
-	if (isPrefLabelLg1Existing) {
-		errorMessage.push(D.duplicatedLabel);
-		fields.prefLabelLg1 = D.duplicatedLabel;
-	}
+	// if (!general.prefLabelLg1) {
+	// 	errorMessage.push(D.mandatoryProperty(D1.labelTitle));
+	// 	fields.prefLabelLg1 = D.mandatoryProperty(D1.labelTitle);
+	// }
 
-	if (!newGeneral.creator) {
-		errorMessage.push(D.mandatoryProperty(D.creatorTitle));
-		fields.creator = D.mandatoryProperty(D.creatorTitle);
-	}
-	if (!newGeneral.disseminationStatus) {
-		errorMessage.push(D.mandatoryProperty(D.disseminationStatusTitle));
-		fields.disseminationStatus = D.mandatoryProperty(
-			D.disseminationStatusTitle
-		);
-	}
+	// if (isPrefLabelLg1Existing) {
+	// 	errorMessage.push(D.duplicatedLabel);
+	// 	fields.prefLabelLg1 = D.duplicatedLabel;
+	// }
 
-	if (htmlIsEmpty(notes.definitionLg1)) {
-		errorMessage.push(D.emptyDefinitionLg1);
-		fields.definitionLg1 = D.emptyDefinitionLg1;
-	}
+	// if (!general.creator) {
+	// 	errorMessage.push(D.mandatoryProperty(D.creatorTitle));
+	// 	fields.creator = D.mandatoryProperty(D.creatorTitle);
+	// }
 
-	const isStatusPublicAndEmptyScopeNote =
-		newGeneral.disseminationStatus.includes('Public') &&
-		htmlIsEmpty(notes.scopeNoteLg1);
+	// if (!general.disseminationStatus) {
+	// 	errorMessage.push(D.mandatoryProperty(D.disseminationStatusTitle));
+	// 	fields.disseminationStatus = D.mandatoryProperty(
+	// 		D.disseminationStatusTitle
+	// 	);
+	// }
 
-	if (isStatusPublicAndEmptyScopeNote) {
-		errorMessage.push(D.emptyScopeNoteLg1);
-		fields.scopeNoteLg1 = D.emptyScopeNoteLg1;
-	}
+	// if (htmlIsEmpty(notes.definitionLg1)) {
+	// 	errorMessage.push(D.emptyDefinitionLg1);
+	// 	fields.definitionLg1 = D.emptyDefinitionLg1;
+	// }
+
+	// const isStatusPublicAndEmptyScopeNote =
+	// 	general.disseminationStatus.includes('Public') &&
+	// 	htmlIsEmpty(notes.scopeNoteLg1);
+
+	// if (isStatusPublicAndEmptyScopeNote) {
+	// 	errorMessage.push(D.emptyScopeNoteLg1);
+	// 	fields.scopeNoteLg1 = D.emptyScopeNoteLg1;
+	// }
 
 	const hasScopeNoteLg2NotLg1 = scndWithoutFirst(
 		notes.scopeNoteLg1,
@@ -76,14 +157,15 @@ const validate = (
 		fields.scopeNoteLg1 = D.hasScopeNoteLg2NotLg1;
 	}
 
-	if (htmlLength(notes.scopeNoteLg1) > maxLengthScopeNote) {
-		errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
-		fields.scopeNoteLg1 = D.tooLongScopeNote(maxLengthScopeNote);
-	}
-	if (htmlLength(notes.scopeNoteLg2) > maxLengthScopeNote) {
-		errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
-		fields.scopeNoteLg2 = D.tooLongScopeNote(maxLengthScopeNote);
-	}
+	// if (htmlLength(notes.scopeNoteLg1) > maxLengthScopeNote) {
+	// 	errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
+	// 	fields.scopeNoteLg1 = D.tooLongScopeNote(maxLengthScopeNote);
+	// }
+
+	// if (htmlLength(notes.scopeNoteLg2) > maxLengthScopeNote) {
+	// 	errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
+	// 	fields.scopeNoteLg2 = D.tooLongScopeNote(maxLengthScopeNote);
+	// }
 
 	const hasEditorialNoteLg2NotLg1 = scndWithoutFirst(
 		notes.editorialNoteLg1,
@@ -107,5 +189,3 @@ const validate = (
 
 	return { errorMessage, fields };
 };
-
-export default validate;
--- src/packages/modules-concepts/edition-creation/controls/validation.spec.js
+++ src/packages/modules-concepts/edition-creation/controls/validation.spec.js
@@ -1,9 +1,7 @@
-import validator, {
-	scndWithoutFirst,
-	checkPrefLabelLg1Existing,
-} from './validation';
+// import validator, {
+import { scndWithoutFirst, checkPrefLabelLg1Existing } from './validation';
 import * as HTMLUtils from '../../../utils/html-utils';
-import D, { D1 } from '../../../deprecated-locales';
+// import D, { D1 } from '../../../deprecated-locales';
 
 jest.mock('../../../utils/html-utils', () => ({
 	htmlIsEmpty: jest.fn(),
@@ -65,120 +63,120 @@ describe('checkPrefLabelLg1Existing', () => {
 	});
 });
 
-describe('validator', () => {
-	it('should return errors for missing mandatory fields and duplicated label', () => {
-		HTMLUtils.htmlIsEmpty.mockReturnValue(true);
-		HTMLUtils.htmlLength.mockReturnValue(0);
+// describe('validator', () => {
+// 	it('should return errors for missing mandatory fields and duplicated label', () => {
+// 		HTMLUtils.htmlIsEmpty.mockReturnValue(true);
+// 		HTMLUtils.htmlLength.mockReturnValue(0);
 
-		const oldGeneral = { prefLabelLg1: 'oldLabel' };
-		const newGeneral = {
-			prefLabelLg1: '',
-			creator: '',
-			disseminationStatus: 'Public',
-		};
-		const notes = {
-			definitionLg1: '',
-			scopeNoteLg1: '',
-			scopeNoteLg2: '',
-			editorialNoteLg1: '',
-			editorialNoteLg2: '',
-			changeNoteLg1: '',
-			changeNoteLg2: '',
-		};
-		const conceptsWithLinks = [{ label: 'duplicatedLabel' }];
-		const maxLengthScopeNote = 100;
+// 		const oldGeneral = { prefLabelLg1: 'oldLabel' };
+// 		const newGeneral = {
+// 			prefLabelLg1: '',
+// 			creator: '',
+// 			disseminationStatus: 'Public',
+// 		};
+// 		const notes = {
+// 			definitionLg1: '',
+// 			scopeNoteLg1: '',
+// 			scopeNoteLg2: '',
+// 			editorialNoteLg1: '',
+// 			editorialNoteLg2: '',
+// 			changeNoteLg1: '',
+// 			changeNoteLg2: '',
+// 		};
+// 		const conceptsWithLinks = [{ label: 'duplicatedLabel' }];
+// 		const maxLengthScopeNote = 100;
 
-		const result = validator(
-			oldGeneral,
-			newGeneral,
-			notes,
-			conceptsWithLinks,
-			maxLengthScopeNote
-		);
+// 		const result = validator(
+// 			oldGeneral,
+// 			newGeneral,
+// 			notes,
+// 			conceptsWithLinks,
+// 			maxLengthScopeNote
+// 		);
 
-		expect(result.errorMessage).toContain(D.mandatoryProperty(D1.labelTitle));
-		expect(result.errorMessage).toContain(D.mandatoryProperty(D.creatorTitle));
-		expect(result.errorMessage).toContain(D.emptyDefinitionLg1);
-		expect(result.errorMessage).toContain(D.emptyScopeNoteLg1);
+// 		expect(result.errorMessage).toContain(D.mandatoryProperty(D1.labelTitle));
+// 		expect(result.errorMessage).toContain(D.mandatoryProperty(D.creatorTitle));
+// 		expect(result.errorMessage).toContain(D.emptyDefinitionLg1);
+// 		expect(result.errorMessage).toContain(D.emptyScopeNoteLg1);
 
-		expect(result.fields.prefLabelLg1).toBe(D.mandatoryProperty(D1.labelTitle));
-		expect(result.fields.creator).toBe(D.mandatoryProperty(D.creatorTitle));
-		expect(result.fields.definitionLg1).toBe(D.emptyDefinitionLg1);
-		expect(result.fields.scopeNoteLg1).toBe(D.emptyScopeNoteLg1);
-	});
+// 		expect(result.fields.prefLabelLg1).toBe(D.mandatoryProperty(D1.labelTitle));
+// 		expect(result.fields.creator).toBe(D.mandatoryProperty(D.creatorTitle));
+// 		expect(result.fields.definitionLg1).toBe(D.emptyDefinitionLg1);
+// 		expect(result.fields.scopeNoteLg1).toBe(D.emptyScopeNoteLg1);
+// 	});
 
-	it('should return error for too long scope notes', () => {
-		HTMLUtils.htmlIsEmpty.mockReturnValue(false);
-		HTMLUtils.htmlLength.mockImplementation((html) => html.length);
+// 	it('should return error for too long scope notes', () => {
+// 		HTMLUtils.htmlIsEmpty.mockReturnValue(false);
+// 		HTMLUtils.htmlLength.mockImplementation((html) => html.length);
 
-		const oldGeneral = { prefLabelLg1: 'oldLabel' };
-		const newGeneral = {
-			prefLabelLg1: 'newLabel',
-			creator: 'creator',
-			disseminationStatus: 'Public',
-		};
-		const notes = {
-			definitionLg1: 'definition',
-			scopeNoteLg1: 'a'.repeat(101),
-			scopeNoteLg2: 'a'.repeat(101),
-			editorialNoteLg1: '',
-			editorialNoteLg2: '',
-			changeNoteLg1: '',
-			changeNoteLg2: '',
-		};
-		const conceptsWithLinks = [{ label: 'existingLabel' }];
-		const maxLengthScopeNote = 100;
+// 		const oldGeneral = { prefLabelLg1: 'oldLabel' };
+// 		const newGeneral = {
+// 			prefLabelLg1: 'newLabel',
+// 			creator: 'creator',
+// 			disseminationStatus: 'Public',
+// 		};
+// 		const notes = {
+// 			definitionLg1: 'definition',
+// 			scopeNoteLg1: 'a'.repeat(101),
+// 			scopeNoteLg2: 'a'.repeat(101),
+// 			editorialNoteLg1: '',
+// 			editorialNoteLg2: '',
+// 			changeNoteLg1: '',
+// 			changeNoteLg2: '',
+// 		};
+// 		const conceptsWithLinks = [{ label: 'existingLabel' }];
+// 		const maxLengthScopeNote = 100;
 
-		const result = validator(
-			oldGeneral,
-			newGeneral,
-			notes,
-			conceptsWithLinks,
-			maxLengthScopeNote
-		);
+// 		const result = validator(
+// 			oldGeneral,
+// 			newGeneral,
+// 			notes,
+// 			conceptsWithLinks,
+// 			maxLengthScopeNote
+// 		);
 
-		expect(result.errorMessage).toContain(
-			D.tooLongScopeNote(maxLengthScopeNote)
-		);
-		expect(result.fields.scopeNoteLg1).toBe(
-			D.tooLongScopeNote(maxLengthScopeNote)
-		);
-		expect(result.fields.scopeNoteLg2).toBe(
-			D.tooLongScopeNote(maxLengthScopeNote)
-		);
-	});
+// 		expect(result.errorMessage).toContain(
+// 			D.tooLongScopeNote(maxLengthScopeNote)
+// 		);
+// 		expect(result.fields.scopeNoteLg1).toBe(
+// 			D.tooLongScopeNote(maxLengthScopeNote)
+// 		);
+// 		expect(result.fields.scopeNoteLg2).toBe(
+// 			D.tooLongScopeNote(maxLengthScopeNote)
+// 		);
+// 	});
 
-	it('should pass validation for valid input', () => {
-		HTMLUtils.htmlIsEmpty.mockReturnValue(false);
-		HTMLUtils.htmlLength.mockImplementation((html) => html.length);
+// 	it('should pass validation for valid input', () => {
+// 		HTMLUtils.htmlIsEmpty.mockReturnValue(false);
+// 		HTMLUtils.htmlLength.mockImplementation((html) => html.length);
 
-		const oldGeneral = { prefLabelLg1: 'oldLabel' };
-		const newGeneral = {
-			prefLabelLg1: 'newLabel',
-			creator: 'creator',
-			disseminationStatus: 'Public',
-		};
-		const notes = {
-			definitionLg1: 'definition',
-			scopeNoteLg1: 'scopeNote1',
-			scopeNoteLg2: 'scopeNote2',
-			editorialNoteLg1: 'editorialNote1',
-			editorialNoteLg2: 'editorialNote2',
-			changeNoteLg1: 'changeNote1',
-			changeNoteLg2: 'changeNote2',
-		};
-		const conceptsWithLinks = [{ label: 'existingLabel' }];
-		const maxLengthScopeNote = 100;
+// 		const oldGeneral = { prefLabelLg1: 'oldLabel' };
+// 		const newGeneral = {
+// 			prefLabelLg1: 'newLabel',
+// 			creator: 'creator',
+// 			disseminationStatus: 'Public',
+// 		};
+// 		const notes = {
+// 			definitionLg1: 'definition',
+// 			scopeNoteLg1: 'scopeNote1',
+// 			scopeNoteLg2: 'scopeNote2',
+// 			editorialNoteLg1: 'editorialNote1',
+// 			editorialNoteLg2: 'editorialNote2',
+// 			changeNoteLg1: 'changeNote1',
+// 			changeNoteLg2: 'changeNote2',
+// 		};
+// 		const conceptsWithLinks = [{ label: 'existingLabel' }];
+// 		const maxLengthScopeNote = 100;
 
-		const result = validator(
-			oldGeneral,
-			newGeneral,
-			notes,
-			conceptsWithLinks,
-			maxLengthScopeNote
-		);
+// 		const result = validator(
+// 			oldGeneral,
+// 			newGeneral,
+// 			notes,
+// 			conceptsWithLinks,
+// 			maxLengthScopeNote
+// 		);
 
-		expect(result.errorMessage).toHaveLength(0);
-		expect(result.fields).toEqual({});
-	});
-});
+// 		expect(result.errorMessage).toHaveLength(0);
+// 		expect(result.fields).toEqual({});
+// 	});
+// });
