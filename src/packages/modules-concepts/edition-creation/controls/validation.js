4996a6cbe894e02d1661d3b27a503f6b591a5c6c
Vasseur Pierre <pierre.vasseur@insee.fr>
2024-08-29 17:16:05 +0200

    fix validation

--- src/packages/modules-concepts/edition-creation/controls/validation.js
+++ src/packages/modules-concepts/edition-creation/controls/validation.js
@@ -19,7 +19,11 @@ const Concept = (oldLabelLg1, conceptsWithLinks, maxLengthScopeNote) =>
 		creator: z
 			.string({ required_error: D.mandatoryProperty(D.creatorTitle) })
 			.min(1, { message: D.mandatoryProperty(D.creatorTitle) }),
-
+		disseminationStatus: z
+			.string({
+				required_error: D.mandatoryProperty(D.disseminationStatusTitle),
+			})
+			.min(1, { message: D.mandatoryProperty(D.disseminationStatusTitle) }),
 		scopeNoteLg1: z
 			.string()
 			.refine((value) => htmlLength(value) <= maxLengthScopeNote, {
@@ -35,37 +39,11 @@ const Concept = (oldLabelLg1, conceptsWithLinks, maxLengthScopeNote) =>
 		}),
 	});
 
-const disseminationStatusEnum = z.enum([
-	'Privé',
-	'Public générique',
-	'Public spécifique',
-]);
-
-const ScopeNoteAndDisseminationStatus = z.discriminatedUnion(
-	'disseminationStatus',
-	[
-		z.object({
-			disseminationStatus: disseminationStatusEnum.extract(['Privé']),
-			scopeNoteLg1: z.string().refine((value) => !htmlIsEmpty(value), {
-				message: D.emptyScopeNoteLg1,
-			}),
-		}),
-		z.object({
-			disseminationStatus: disseminationStatusEnum.extract([
-				'Public générique',
-				'Public spécifique',
-			]),
-			scopeNoteLg1: z
-				.string()
-				.refine((value) => !htmlIsEmpty(value), {
-					message: D.emptyScopeNoteLg1,
-				})
-				.optional(),
-		}),
-	]
-);
-
-// disseminationStatus: z.string({ required_error: D.mandatoryProperty(D.disseminationStatusTitle) }).min(1, { message: D.mandatoryProperty(D.disseminationStatusTitle) })
+const MandatoryScopeNote = {
+	scopeNoteLg1: z.string().refine((value) => !htmlIsEmpty(value), {
+		message: D.emptyScopeNoteLg1,
+	}),
+};
 
 export const validate = (
 	general,
@@ -73,119 +51,15 @@ export const validate = (
 	oldLabelLg1,
 	conceptsWithLinks,
 	maxLengthScopeNote
-) =>
-	formatValidation(
-		Concept(oldLabelLg1, conceptsWithLinks, maxLengthScopeNote).extend(
-			ScopeNoteAndDisseminationStatus
-		)
-	)({ ...general, ...notes });
-
-export const scndWithoutFirst = (first, second) => {
-	return !htmlIsEmpty(second) && htmlIsEmpty(first);
-};
-
-export const checkPrefLabelLg1Existing = (
-	concepts,
-	prefLabelLg1,
-	initialPrefLabelFr
 ) => {
-	return (
-		prefLabelLg1 !== initialPrefLabelFr &&
-		concepts.map((concept) => concept.label).includes(prefLabelLg1)
-	);
-};
+	const ConceptToValidate = general.disseminationStatus.includes('Public')
+		? Concept(oldLabelLg1, conceptsWithLinks, maxLengthScopeNote).extend(
+				MandatoryScopeNote
+		  )
+		: Concept(oldLabelLg1, conceptsWithLinks, maxLengthScopeNote);
 
-export const validatee = (
-	general,
-	notes,
-	oldLabelLg1,
-	conceptsWithLinks,
-	maxLengthScopeNote
-) => {
-	const errorMessage = [];
-	const fields = {};
-
-	// const isPrefLabelLg1Existing = checkPrefLabelLg1Existing(
-	// 	conceptsWithLinks,
-	// 	general.prefLabelLg1,
-	// 	oldLabelLg1
-	// );
-
-	// if (!general.prefLabelLg1) {
-	// 	errorMessage.push(D.mandatoryProperty(D1.labelTitle));
-	// 	fields.prefLabelLg1 = D.mandatoryProperty(D1.labelTitle);
-	// }
-
-	// if (isPrefLabelLg1Existing) {
-	// 	errorMessage.push(D.duplicatedLabel);
-	// 	fields.prefLabelLg1 = D.duplicatedLabel;
-	// }
-
-	// if (!general.creator) {
-	// 	errorMessage.push(D.mandatoryProperty(D.creatorTitle));
-	// 	fields.creator = D.mandatoryProperty(D.creatorTitle);
-	// }
-
-	// if (!general.disseminationStatus) {
-	// 	errorMessage.push(D.mandatoryProperty(D.disseminationStatusTitle));
-	// 	fields.disseminationStatus = D.mandatoryProperty(
-	// 		D.disseminationStatusTitle
-	// 	);
-	// }
-
-	// if (htmlIsEmpty(notes.definitionLg1)) {
-	// 	errorMessage.push(D.emptyDefinitionLg1);
-	// 	fields.definitionLg1 = D.emptyDefinitionLg1;
-	// }
-
-	// const isStatusPublicAndEmptyScopeNote =
-	// 	general.disseminationStatus.includes('Public') &&
-	// 	htmlIsEmpty(notes.scopeNoteLg1);
-
-	// if (isStatusPublicAndEmptyScopeNote) {
-	// 	errorMessage.push(D.emptyScopeNoteLg1);
-	// 	fields.scopeNoteLg1 = D.emptyScopeNoteLg1;
-	// }
-
-	const hasScopeNoteLg2NotLg1 = scndWithoutFirst(
-		notes.scopeNoteLg1,
-		notes.scopeNoteLg2
-	);
-
-	if (hasScopeNoteLg2NotLg1) {
-		errorMessage.push(D.hasScopeNoteLg2NotLg1);
-		fields.scopeNoteLg1 = D.hasScopeNoteLg2NotLg1;
-	}
-
-	// if (htmlLength(notes.scopeNoteLg1) > maxLengthScopeNote) {
-	// 	errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
-	// 	fields.scopeNoteLg1 = D.tooLongScopeNote(maxLengthScopeNote);
-	// }
-
-	// if (htmlLength(notes.scopeNoteLg2) > maxLengthScopeNote) {
-	// 	errorMessage.push(D.tooLongScopeNote(maxLengthScopeNote));
-	// 	fields.scopeNoteLg2 = D.tooLongScopeNote(maxLengthScopeNote);
-	// }
-
-	const hasEditorialNoteLg2NotLg1 = scndWithoutFirst(
-		notes.editorialNoteLg1,
-		notes.editorialNoteLg2
-	);
-
-	if (hasEditorialNoteLg2NotLg1) {
-		errorMessage.push(D.hasEditorialNoteLg2NotLg1);
-		fields.editorialNoteLg1 = D.hasEditorialNoteLg2NotLg1;
-	}
-
-	const hasChangeNoteLg2NotLg1 = scndWithoutFirst(
-		notes.changeNoteLg1,
-		notes.changeNoteLg2
-	);
-
-	if (hasChangeNoteLg2NotLg1) {
-		errorMessage.push(D.hasChangeNoteLg2NotLg1);
-		fields.changeNoteLg1 = D.hasChangeNoteLg2NotLg1;
-	}
-
-	return { errorMessage, fields };
+	return formatValidation(ConceptToValidate)({
+		...general,
+		...notes,
+	});
 };
