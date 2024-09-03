732a0b4e32f7d9be4bf0fc1d75b1190ee6672789
Emmanuel <demey.emmanuel@gmail.com>
2024-09-03 09:29:40 +0100

    fix: remove duplicated code returnes by Sonar

--- src/packages/modules-structures/edition/validation.tsx
+++ src/packages/modules-structures/edition/validation.tsx
@@ -2,19 +2,17 @@ import D, { D1, D2 } from '../../deprecated-locales';
 import { z } from 'zod';
 import { formatValidation } from '../../utils/validation';
 
+export const generateMandatoryAndNotEmptyField = (property: string)  => {
+		return z
+			.string({ required_error: D.mandatoryProperty(property) })
+			.trim()
+			.min(1, { message: D.mandatoryProperty(property) })
+}
+
 export const StructureZod = z.object({
-	identifiant: z
-		.string({ required_error: D.mandatoryProperty(D.idTitle) })
-		.trim()
-		.min(1, { message: D.mandatoryProperty(D.idTitle) }),
-	labelLg1: z
-		.string({ required_error: D.mandatoryProperty(D1.labelTitle) })
-		.trim()
-		.min(1, { message: D.mandatoryProperty(D1.labelTitle) }),
-	labelLg2: z
-		.string({ required_error: D.mandatoryProperty(D2.labelTitle) })
-		.trim()
-		.min(1, { message: D.mandatoryProperty(D2.labelTitle) }),
+	identifiant: generateMandatoryAndNotEmptyField(D.idTitle),
+	labelLg1: generateMandatoryAndNotEmptyField(D1.labelTitle),
+	labelLg2: generateMandatoryAndNotEmptyField(D2.labelTitle),
 });
 
 export const validate = formatValidation(StructureZod);
