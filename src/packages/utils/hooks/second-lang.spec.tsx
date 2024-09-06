4161d7c5b4a2f5237b053a35937943d3d0773efb
Emmanuel <demey.emmanuel@gmail.com>
2024-09-06 11:34:52 +0100

    feat: add unit test

---
+++ src/packages/utils/hooks/second-lang.spec.tsx
@@ -0,0 +1,25 @@
+import { renderHook, act } from '@testing-library/react';
+import { useSecondLang } from './second-lang';
+import { AppContextProvider } from '../../application/app-context';
+
+describe('useSecondLang', () => {
+	it('returns the correct initial value and toggle function', () => {
+		const wrapper = ({ children }: { children: React.ReactNode }) => (
+			<AppContextProvider lg1="English" lg2="French">
+				{children}
+			</AppContextProvider>
+		);
+
+		const { result } = renderHook(() => useSecondLang(), { wrapper });
+
+		const [value, toggle] = result.current;
+		expect(value).toBe(false);
+
+		act(() => {
+			toggle();
+		});
+
+		const [newValue] = result.current;
+		expect(newValue).toBe(true);
+	});
+});
