9023454fed20d91612c500f738edf25bd22c07d9
Pierre Vasseur <132562606+PierreVasseur@users.noreply.github.com>
2024-09-20 11:48:01 +0200

    Fix : set same icon for publish button in Concepts and Collections (#966)
    * set same button icon
    
    * solve issue creation page for series and indicators
    
    * cancelbutton instead of returnbutton in structures edition

--- src/packages/modules-concepts/collections/menu.jsx
+++ src/packages/modules-concepts/collections/menu.jsx
@@ -1,10 +1,11 @@
-import D from '../../deprecated-locales/build-dictionary';
 import check from '../../auth/auth';
 import { FeminineButton } from '../../components';
 import { usePermission } from '../../redux/hooks/usePermission';
 import { VerticalMenu } from '../../components/vertical-menu';
-import { ExportButton } from '../../components/buttons/buttons-with-icons';
-import { Button } from '../../components/buttons/button';
+import {
+	ExportButton,
+	PublishButton,
+} from '../../components/buttons/buttons-with-icons';
 
 export const Menu = () => {
 	const { authType, roles } = usePermission();
@@ -18,14 +19,10 @@ export const Menu = () => {
 			{adminOrContributor && <FeminineButton action="/collection/create" />}
 			<ExportButton action="/collections/export" wrapper={false} />
 			{adminOrCreator && (
-				<Button
-					label={
-						<>
-							<span className="glyphicon glyphicon-ok" aria-hidden="true" />
-							<span> {D.btnValid}</span>
-						</>
-					}
+				<PublishButton
 					action="/collections/validation"
+					col={8}
+					offset={2}
 					wrapper={false}
 				/>
 			)}
