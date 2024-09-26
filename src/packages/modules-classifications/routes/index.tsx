bc988e7c2c96b720ceaffc7827f0cadb1f7f6812
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-09-26 17:39:33 +0200

    review import in classifications router

--- src/packages/modules-classifications/routes/index.tsx
+++ src/packages/modules-classifications/routes/index.tsx
@@ -3,34 +3,27 @@ import { RouteObject } from 'react-router-dom';
 export const routes: RouteObject[] = [
 	{
 		path: 'families',
-		lazy: () => import('../../modules-classifications/families/home-container'),
+		lazy: () => import('../families/home-container'),
 	},
 	{
 		path: 'family/:id',
-		lazy: () =>
-			import(
-				'../../modules-classifications/families/visualization/home-container'
-			),
+		lazy: () => import('../families/visualization/home-container'),
 	},
 	{
 		path: 'series',
-		lazy: () => import('../../modules-classifications/series/home-container'),
+		lazy: () => import('../series/home-container'),
 	},
 	{
 		path: 'series/:id',
-		lazy: () =>
-			import(
-				'../../modules-classifications/series/visualization/home-container'
-			),
+		lazy: () => import('../series/visualization/home-container'),
 	},
 	{
 		path: '',
-		lazy: () => import('../../modules-classifications/home-container'),
+		lazy: () => import('../home-container'),
 	},
 	{
 		path: 'classification/:id',
-		lazy: () =>
-			import('../../modules-classifications/visualization/home-container'),
+		lazy: () => import('../visualization/home-container'),
 	},
 	{
 		path: 'classification/:id/modify',
@@ -38,50 +31,38 @@ export const routes: RouteObject[] = [
 	},
 	{
 		path: 'classification/:id/items',
-		lazy: () =>
-			import(
-				'../../modules-classifications/visualization/items/home-container'
-			),
+		lazy: () => import('../visualization/items/home-container'),
 	},
 	{
 		path: 'classification/:id/tree',
-		lazy: () =>
-			import('../../modules-classifications/visualization/tree/home-container'),
+		lazy: () => import('../visualization/tree/home-container'),
 	},
 	{
 		path: 'classification/:classificationId/level/:levelId',
-		lazy: () => import('../../modules-classifications/level/home-container'),
+		lazy: () => import('../level/home-container'),
 	},
 	{
 		path: 'classification/:classificationId/item/:itemId',
-		lazy: () => import('../../modules-classifications/item/home-container'),
+		lazy: () => import('../item/home-container'),
 	},
 	{
 		path: 'classification/:classificationId/item/:itemId/modify',
-		lazy: () => import('../../modules-classifications/item/edition'),
+		lazy: () => import('../item/edition'),
 	},
 	{
 		path: 'classification/:classificationId/item/:itemId/compare',
-		lazy: () =>
-			import('../../modules-classifications/item/compare/home-container'),
+		lazy: () => import('../item/compare/home-container'),
 	},
 	{
 		path: 'correspondences',
-		lazy: () =>
-			import('../../modules-classifications/correspondences/home-container'),
+		lazy: () => import('../correspondences/home-container'),
 	},
 	{
 		path: 'correspondence/:id',
-		lazy: () =>
-			import(
-				'../../modules-classifications/correspondences/visualization/home-container'
-			),
+		lazy: () => import('../correspondences/visualization/home-container'),
 	},
 	{
 		path: 'correspondence/:correspondenceId/association/:associationId',
-		lazy: () =>
-			import(
-				'../../modules-classifications/correspondences/association/home-container'
-			),
+		lazy: () => import('../correspondences/association/home-container'),
 	},
 ];
