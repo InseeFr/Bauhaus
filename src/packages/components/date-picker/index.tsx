487630dd36aab42a0cd28313d88cecbfc981232c
Emmanuel <demey.emmanuel@gmail.com>
2024-10-23 09:20:10 +0100

    fix: solve issue with z-index and datepicker

--- src/packages/components/date-picker/index.tsx
+++ src/packages/components/date-picker/index.tsx
@@ -1,13 +1,15 @@
 import { Calendar } from 'primereact/calendar';
+import './date-picker.scss';
 
 type DatePickerTypes = {
 	value: string;
-	onChange: (value: any) => void;
+	onChange: (value: string | undefined) => void;
 };
 export const DatePicker = ({ value, onChange }: Readonly<DatePickerTypes>) => {
+	const date = value ? new Date(value) : undefined;
 	return (
 		<Calendar
-			value={new Date(value)}
+			value={date}
 			onChange={(e) => {
 				onChange(e.value?.toISOString());
 			}}
