c2c195fa282357726213f60fc7c06b68eeaab59b
Pierre Vasseur <132562606+PierreVasseur@users.noreply.github.com>
2024-11-06 08:53:52 +0100

    delete script (#1057)

--- scripts/getVersion.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'node:fs';
-import path from 'node:path';
-
-const packageJson = JSON.parse(
-	fs.readFileSync(path.resolve(__dirname, '..', 'package.json'), 'utf8'),
-);
-console.log(packageJson.version);
