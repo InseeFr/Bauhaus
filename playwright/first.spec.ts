d0c8cdcc6f7540ebd1f9e7fa00db6194a3a82331
Emmanuel <demey.emmanuel@gmail.com>
2024-11-07 16:31:29 +0000

    more work

--- playwright/first.spec.ts
+++ playwright/first.spec.ts
@@ -1,6 +1,20 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Should display an error is the backend is not on', async ({ page }) => {
 	await page.goto('/');
-	await page.getByText('Error');
+	await expect(page.getByText('Metadata management application')).toBeVisible();
+});
+
+test('test', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Opérations operations' }).click();
+	await page.getByRole('link', { name: 'Familles' }).click();
+	await page.getByRole('link', { name: 'Nouvelle' }).click();
+	await page.getByLabel('Intitulé*').click();
+	await page.getByLabel('Intitulé*').click();
+	await page.getByLabel('Intitulé*').fill('test');
+	await page.getByLabel('Intitulé*').press('Tab');
+	await page.getByLabel('Title*').fill('test');
+	await page.getByRole('button', { name: 'Sauvegarder' }).click();
+	await expect(page.locator('h2')).toContainText('test');
 });
