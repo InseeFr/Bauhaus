5feea69e5c6528b3d2e99a588b410398e75db37c
Emmanuel <demey.emmanuel@gmail.com>
2024-11-07 19:25:45 +0000

    feat: make test failed

--- playwright/first.spec.ts
+++ playwright/first.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Should display an error is the backend is not on', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByText('Metadata management application')).toBeVisible();
+	await expect(page.getByText('Test management application')).toBeVisible();
 });
