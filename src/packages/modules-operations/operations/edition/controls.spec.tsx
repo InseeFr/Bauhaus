71acc9bc8c2527ceece38cae1707b6aa9cba0c6a
Emmanuel <demey.emmanuel@gmail.com>
2024-11-29 11:33:25 +0000

    prettier

--- src/packages/modules-operations/operations/edition/controls.spec.tsx
+++ src/packages/modules-operations/operations/edition/controls.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, it, vi } from 'vitest';
+
 import { Controls } from './controls';
 
 vi.mock('../../../components/action-toolbar', () => ({
--- src/packages/modules-operations/operations/edition/fields/year.spec.tsx
+++ src/packages/modules-operations/operations/edition/fields/year.spec.tsx
@@ -1,6 +1,7 @@
-import { describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { describe, it, vi } from 'vitest';
+
 import { YearInput } from './year';
 
 describe('YearInput Component', () => {
