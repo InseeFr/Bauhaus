6b8d3635c39de2e699261ef252431b7e0e638768
Emmanuel <demey.emmanuel@gmail.com>
2024-12-11 10:28:40 +0000

    feat: add toolbar

--- src/packages/components/rich-editor/react-md-editor.tsx
+++ src/packages/components/rich-editor/react-md-editor.tsx
@@ -1,10 +1,19 @@
-import Editor from '@uiw/react-md-editor';
+import Editor, {
+	bold,
+	italic,
+	unorderedListCommand,
+} from '@uiw/react-md-editor';
 
 export const MDEditor = ({
 	text,
 	handleChange,
 }: Readonly<{ text: string; handleChange: (value?: string) => void }>) => {
 	return (
-		<Editor value={text} onChange={handleChange} commands={[]} preview="edit" />
+		<Editor
+			value={text}
+			onChange={handleChange}
+			commands={[italic, bold, unorderedListCommand]}
+			preview="edit"
+		/>
 	);
 };
