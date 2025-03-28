6d1de724489813d340abbe2ab0893c00100fd4f2
Emmanuel <demey.emmanuel@gmail.com>
2025-03-28 15:00:27 +0100

    fix: remove console.log

--- src/dev-tools/load.tsx
+++ src/dev-tools/load.tsx
@@ -1,5 +1,4 @@
 function load(store: unknown, callback: VoidFunction) {
-	console.log(import.meta.env.VITE_DEV_TOOLS_ENABLED);
 	if (import.meta.env.VITE_DEV_TOOLS_ENABLED === 'true') {
 		import('./dev-tools').then((mod) => mod.install(store)).finally(callback);
 	} else {
