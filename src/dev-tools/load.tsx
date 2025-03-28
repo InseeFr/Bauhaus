d43db0eeea5d69f52ee8edc3744dd15601cf9b1a
Emmanuel <demey.emmanuel@gmail.com>
2025-03-28 14:59:20 +0100

    feat: start migrating react-select

--- src/dev-tools/load.tsx
+++ src/dev-tools/load.tsx
@@ -1,4 +1,5 @@
 function load(store: unknown, callback: VoidFunction) {
+	console.log(import.meta.env.VITE_DEV_TOOLS_ENABLED);
 	if (import.meta.env.VITE_DEV_TOOLS_ENABLED === 'true') {
 		import('./dev-tools').then((mod) => mod.install(store)).finally(callback);
 	} else {
