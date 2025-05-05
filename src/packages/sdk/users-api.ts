86d670cb69c3e7d130a3e227357dcbcaad2cd8b0
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2025-05-05 10:35:02 +0200

    Revert "feat: implement new security layer for families"

--- src/packages/sdk/users-api.ts
+++ src/packages/sdk/users-api.ts
@@ -1,19 +1,6 @@
 import { buildApi } from './build-api';
 
-export type MODULE = 'FAMILY';
-export type PRIVILEGE = 'READ' | 'CREATE' | 'UPDATE' | 'PUBLISH' | 'DELETE';
-export type STRATEGY = 'ALL' | 'STAMP' | 'NONE';
-
 const apiConfig = {
 	getStamp: () => ['stamp'],
-	getInfo: () => ['info'],
 };
-export const UsersApi = buildApi('users', apiConfig) as {
-	getStamp: () => Promise<string>;
-	getInfo: () => Promise<
-		{
-			application: MODULE;
-			privileges: { privilege: PRIVILEGE; strategy: STRATEGY }[];
-		}[]
-	>;
-};
+export const UsersApi = buildApi('users', apiConfig) as any;
