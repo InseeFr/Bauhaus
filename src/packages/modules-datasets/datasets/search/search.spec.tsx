187edeeb94f8467e41126675e8b8596195e390f9
Pierre Vasseur <pierre.vasseur@insee.fr>
2025-05-15 17:43:45 +0200

    fix: tests failure

--- src/packages/modules-datasets/datasets/search/search.spec.tsx
+++ src/packages/modules-datasets/datasets/search/search.spec.tsx
@@ -1,6 +1,13 @@
 import { renderWithRouter } from '../../../tests-utils/render';
 import { AdvancedSearchForm } from './search';
 
+vi.mock('@tanstack/react-query', () => ({
+	useQuery: vi.fn().mockReturnValue({
+		isLoading: true,
+		data: ['data'],
+	}),
+}));
+
 describe('advanced search component', () => {
 	it('renders without crashing', () => {
 		renderWithRouter(
