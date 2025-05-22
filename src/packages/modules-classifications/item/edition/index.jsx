01de985f6b61c14f91e30e813035b043bdcfd310
Emmanuel <demey.emmanuel@gmail.com>
2025-05-22 16:01:47 +0200

    fix: missin gimport

--- src/packages/modules-classifications/item/edition/index.jsx
+++ src/packages/modules-classifications/item/edition/index.jsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { ClientSideError, ErrorBloc } from '@components/errors-bloc';
