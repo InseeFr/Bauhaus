796fe8008d161786a06e850be01ea8b1ccb3266e
Emmanuel <demey.emmanuel@gmail.com>
2025-05-22 16:24:01 +0200

    fix: remove unused code

--- src/packages/tests/render.tsx
+++ src/packages/tests/render.tsx
@@ -1,20 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
-import { PropsWithChildren, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { MODULE, PRIVILEGE, STRATEGY } from '@sdk/users-api';
+import { MODULE, PRIVILEGE, STRATEGY } from '@utils/hooks/users';
 
 import { AppContextProvider } from '../application/app-context';
 
-export const renderWithReactQuery = ({
-	children,
-}: PropsWithChildren<never>) => {
-	const queryClient = new QueryClient();
-	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-	);
-};
 export const mockReactQuery = (data: unknown) => {
 	vi.doMock('@tanstack/react-query', async () => {
 		const actual = await vi.importActual<
