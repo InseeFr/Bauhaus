e21f17d9e990a119252bf8afb9e865f0dfcb4788
Emmanuel <demey.emmanuel@gmail.com>
2025-06-06 16:51:00 +0200

    feat: hide adminitrator menu for non-administrator

--- src/packages/modules-concepts/menu/concepts.spec.tsx
+++ src/packages/modules-concepts/menu/concepts.spec.tsx
@@ -1,8 +1,56 @@
+import { screen } from '@testing-library/dom';
+import { Provider } from 'react-redux';
+
+import { ADMIN } from '../../auth/roles';
+import D from '../../deprecated-locales';
+import configureStore from '../../redux/configure-store';
 import { renderWithRouter } from '../../tests/render';
 import MenuConcepts from './index';
 
 describe('menu-concepts', () => {
-	it('renders without crashing', () => {
-		renderWithRouter(<MenuConcepts />);
+	it('should display the administrion menu', () => {
+		const store = configureStore({
+			app: {
+				auth: {
+					type: 'type',
+					user: {
+						roles: [ADMIN],
+					},
+				},
+			},
+		});
+
+		renderWithRouter(
+			<Provider store={store}>
+				<MenuConcepts />
+			</Provider>,
+			['/concepts'],
+		);
+		const links = screen.getAllByRole('link');
+
+		expect(links).toHaveLength(5);
+		expect(links[3].textContent).toBe(D.administrationTitle);
+	});
+
+	it('should not display the administrion menu', () => {
+		const store = configureStore({
+			app: {
+				auth: {
+					type: 'type',
+					user: {
+						roles: [],
+					},
+				},
+			},
+		});
+
+		renderWithRouter(
+			<Provider store={store}>
+				<MenuConcepts />
+			</Provider>,
+			['/concepts'],
+		);
+		const links = screen.getAllByRole('link');
+		expect(links).toHaveLength(4);
 	});
 });
