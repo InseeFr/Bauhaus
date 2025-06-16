9ad84fe71c3b818ec7c3c91af41125e51d4e7f62
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2025-06-16 10:05:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig/*, loadEnv*/ } from 'vite';
 import { viteEnvs } from 'vite-envs';
 // import csp from 'vite-plugin-csp-guard';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
+	//const env = loadEnv(mode, process.cwd(), '');
 
 	return {
 		optimizeDeps: {
