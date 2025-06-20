53bacb920923e8fb2a3d7917cf434d9985e50b52
Emmanuel <demey.emmanuel@gmail.com>
2025-06-20 14:29:57 +0200

    fix: solve CSP ?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized';
-import { defineConfig/*, loadEnv*/ } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { viteEnvs } from 'vite-envs';
-// import csp from 'vite-plugin-csp-guard';
+import csp from 'vite-plugin-csp-guard';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig((/*{ mode }*/) => {
-	//const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
 		optimizeDeps: {
@@ -29,20 +29,18 @@ export default defineConfig((/*{ mode }*/) => {
 		},
 		plugins: [
 			react(),
-//			csp({
-//				dev: {
-//					run: true,
-//				},
-//				policy: {
-//					'style-src-elem': ["'unsafe-inline'", 'https://fonts.googleapis.com'],
-//					'script-src-elem': ["'self'", 'https://ajax.googleapis.com/'],
-//					'font-src': ["'self'", 'https://fonts.gstatic.com/'],
-//					'connect-src': [env.VITE_API_BASE_HOST + '/', 'ws://localhost:3000'],
-//				},
-//				build: {
-//					sri: true,
-//				},
-//			}),
+			csp({
+				dev: {
+					run: true,
+				},
+				policy: {
+					'style-src-elem': ["'unsafe-inline'", 'https://fonts.googleapis.com'],
+					'script-src': ["'self'", 'https://ajax.googleapis.com'],
+					'script-src-elem': ["'self'", 'https://ajax.googleapis.com/'],
+					'font-src': ["'self'", 'https://fonts.gstatic.com/'],
+					'connect-src': [env.VITE_API_BASE_HOST + '/', 'ws://localhost:3000'],
+				},
+			}),
 			tsconfigPaths(),
 			viteEnvs({
 				declarationFile: '.env',
