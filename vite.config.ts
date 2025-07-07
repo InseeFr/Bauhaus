fa4889ea487db57329694161fe0a814a2fbb6db2
Emmanuel <demey.emmanuel@gmail.com>
2025-07-07 10:49:16 +0200

    fix: make modern-compiler scss preprocessor a constabt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig /*, loadEnv*/ } from 'vite';
 import { viteEnvs } from 'vite-envs';
-import csp from 'vite-plugin-csp-guard';
+// import csp from 'vite-plugin-csp-guard';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig((/*{ mode }*/) => {
+	//const env = loadEnv(mode, process.cwd(), '');
 
 	return {
 		optimizeDeps: {
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
 		css: {
 			preprocessorOptions: {
 				scss: {
-					api: 'modern-compiler',
+					api: 'modern-compiler' as const,
 				},
 			},
 		},
@@ -29,6 +29,20 @@ export default defineConfig(({ mode }) => {
 		},
 		plugins: [
 			react(),
+			//			csp({
+			//				dev: {
+			//					run: true,
+			//				},
+			//				policy: {
+			//					'style-src-elem': ["'unsafe-inline'", 'https://fonts.googleapis.com'],
+			//					'script-src-elem': ["'self'", 'https://ajax.googleapis.com/'],
+			//					'font-src': ["'self'", 'https://fonts.gstatic.com/'],
+			//					'connect-src': [env.VITE_API_BASE_HOST + '/', 'ws://localhost:3000'],
+			//				},
+			//				build: {
+			//					sri: true,
+			//				},
+			//			}),
 			tsconfigPaths(),
 			viteEnvs({
 				declarationFile: '.env',
@@ -47,23 +61,6 @@ export default defineConfig(({ mode }) => {
 						VITE_NAME: packageJson.name,
 						VITE_VERSION: packageJson.version,
 					};
-				},
-			}),
-			csp({
-				dev: {
-					run: true,
-				},
-				policy: {
-					'default-src': ["'self'", env.VITE_OIDC_ISSUER + '/'],
-					'style-src-elem': ["'unsafe-inline'", 'https://fonts.googleapis.com'],
-					'script-src': ["'self'", 'https://ajax.googleapis.com'],
-					'script-src-elem': ["'self'", "'unsafe-inline'", 'https://ajax.googleapis.com/'],
-					'font-src': ["'self'", 'https://fonts.gstatic.com/'],
-					'connect-src': [
-						env.VITE_API_BASE_HOST + '/',
-						'ws://localhost:3000',
-						env.VITE_OIDC_ISSUER + '/',
-					],
 				},
 			}),
 		],
