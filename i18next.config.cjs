4ded11309f998b39473485fddc93f7eaaef9eaf9
Pierre Vasseur <pierre.vasseur@insee.fr>
2026-05-05 11:25:28 +0200

    chore: update i18next config file

--- i18next.config.cjs
+++ i18next.config.cjs
@@ -1,15 +1,18 @@
+const excludeGlobs = [
+	'!src/packages/**/*.spec.{ts,tsx,js,jsx}',
+	'!src/packages/**/*.test.{ts,tsx,js,jsx}',
+	'!src/packages/i18n/locales/**',
+	'!**/node_modules/**',
+];
+
+const migratedModules = ['modules-ddi', 'modules-structures', 'modules-codelists', 'modules-datasets'];
+
 module.exports = {
 	locales: ['fr', 'en'],
-	extract: {
-		input: [
-			'src/packages/modules-ddi/**/*.{ts,tsx,js,jsx}',
-			'!src/packages/**/*.spec.{ts,tsx,js,jsx}',
-			'!src/packages/**/*.test.{ts,tsx,js,jsx}',
-			'!src/packages/i18n/locales/**',
-			'!**/node_modules/**',
-		],
-		output: 'src/packages/modules-ddi/i18n/locales/{{language}}.json',
-	},
+	extract: migratedModules.map((module) => ({
+		input: [`src/packages/${module}/**/*.{ts,tsx,js,jsx}`, ...excludeGlobs],
+		output: `src/packages/${module}/i18n/locales/{{language}}.json`,
+	})),
 	lint: {
 		input: ['src/packages/**/*.{ts,tsx,js,jsx}'],
 		ignore: [
