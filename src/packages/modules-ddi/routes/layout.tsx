dd5d47a71d782ca0be071377ec933a6275f4137a
Pierre Vasseur <pierre.vasseur@insee.fr>
2026-08-31 19:04:57 +0200

    refactor: use propre i18n instance for ddi

--- src/packages/modules-ddi/routes/layout.tsx
+++ src/packages/modules-ddi/routes/layout.tsx
@@ -3,12 +3,12 @@ import { I18nextProvider } from "react-i18next";
 
 import { useTheme } from "../../utils/hooks/useTheme";
 import { Menu } from "../menu";
-import i18n from "../i18n";
+import ddiI18n from "../i18n";
 
 export const Component = () => {
   useTheme("ddi");
   return (
-    <I18nextProvider i18n={i18n}>
+    <I18nextProvider i18n={ddiI18n}>
       <Menu />
       <div className="container">
         <Outlet />
