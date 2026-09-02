e7e74e89ce52b926ea70a5fb7b33a93d4a566577
Pierre Vasseur <pierre.vasseur@insee.fr>
2026-09-02 18:42:18 +0200

    chore: format

--- src/packages/modules-operations/pages/sims/components/MSDLayout.tsx
+++ src/packages/modules-operations/pages/sims/components/MSDLayout.tsx
@@ -58,11 +58,7 @@ export const MSDLayout = ({
         </nav>
       </section>
       {status === Status.CONTENT && (
-        <button
-          type="button"
-          className="msd-panel-trigger-left"
-          onClick={changeStatusToBoth}
-        >
+        <button type="button" className="msd-panel-trigger-left" onClick={changeStatusToBoth}>
           {t("sims.helpSummary")}
           <span className="glyphicon glyphicon-chevron-right" />
         </button>
@@ -70,40 +66,26 @@ export const MSDLayout = ({
       {status === Status.BOTH && (
         <div className="msd-panel-trigger-middle">
           <div>
-            <button
-              type="button"
-              onClick={changeStatusToContent}
-              title="open content"
-            >
+            <button type="button" onClick={changeStatusToContent} title="open content">
               <span className="glyphicon glyphicon-chevron-left" />
             </button>
           </div>
           <div>
-            <button
-              type="button"
-              onClick={changeStatusToSummary}
-              title="open summary"
-            >
+            <button type="button" onClick={changeStatusToSummary} title="open summary">
               <span className="glyphicon glyphicon-chevron-right" />
             </button>
           </div>
         </div>
       )}
       {status === Status.SUMMARY && (
-        <button
-          type="button"
-          className="msd-panel-trigger-right"
-          onClick={changeStatusToBoth}
-        >
+        <button type="button" className="msd-panel-trigger-right" onClick={changeStatusToBoth}>
           <span className="glyphicon glyphicon-chevron-left" />
           {t("sims.helpContent")}
         </button>
       )}
       <section
         style={styleContent}
-        className={
-          status === Status.CONTENT ? "msd-content-alone" : "msd-content"
-        }
+        className={status === Status.CONTENT ? "msd-content-alone" : "msd-content"}
       >
         {children}
       </section>
