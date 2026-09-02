f777f08c3155e48cf12b2af30c40c64207099b87
Pierre Vasseur <pierre.vasseur@insee.fr>
2026-09-02 18:41:03 +0200

    fix: rename import

--- src/packages/modules-operations/pages/sims/components/MSDLayout.tsx
+++ src/packages/modules-operations/pages/sims/components/MSDLayout.tsx
@@ -5,7 +5,7 @@ import { MetadataStructure } from "@model/Sims";
 
 import { Status, useLayout } from "../../../hooks/useLayout";
 import { Outline } from "./Outline";
-import "./MSDComponent.css";
+import "./MSDLayout.css";
 import "../../../../components/panel-trigger/panel-trigger.css";
 
 interface MSDComponentTypes {
@@ -58,7 +58,11 @@ export const MSDLayout = ({
         </nav>
       </section>
       {status === Status.CONTENT && (
-        <button type="button" className="msd-panel-trigger-left" onClick={changeStatusToBoth}>
+        <button
+          type="button"
+          className="msd-panel-trigger-left"
+          onClick={changeStatusToBoth}
+        >
           {t("sims.helpSummary")}
           <span className="glyphicon glyphicon-chevron-right" />
         </button>
@@ -66,26 +70,40 @@ export const MSDLayout = ({
       {status === Status.BOTH && (
         <div className="msd-panel-trigger-middle">
           <div>
-            <button type="button" onClick={changeStatusToContent} title="open content">
+            <button
+              type="button"
+              onClick={changeStatusToContent}
+              title="open content"
+            >
               <span className="glyphicon glyphicon-chevron-left" />
             </button>
           </div>
           <div>
-            <button type="button" onClick={changeStatusToSummary} title="open summary">
+            <button
+              type="button"
+              onClick={changeStatusToSummary}
+              title="open summary"
+            >
               <span className="glyphicon glyphicon-chevron-right" />
             </button>
           </div>
         </div>
       )}
       {status === Status.SUMMARY && (
-        <button type="button" className="msd-panel-trigger-right" onClick={changeStatusToBoth}>
+        <button
+          type="button"
+          className="msd-panel-trigger-right"
+          onClick={changeStatusToBoth}
+        >
           <span className="glyphicon glyphicon-chevron-left" />
           {t("sims.helpContent")}
         </button>
       )}
       <section
         style={styleContent}
-        className={status === Status.CONTENT ? "msd-content-alone" : "msd-content"}
+        className={
+          status === Status.CONTENT ? "msd-content-alone" : "msd-content"
+        }
       >
         {children}
       </section>
